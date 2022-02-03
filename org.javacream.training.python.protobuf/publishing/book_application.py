d39e8ff7a3edb37e252bfd4194be0cd3951b815e
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-02-03 08:59:53 +0100

    add channel project

---
+++ org.javacream.training.python.protobuf/publishing/book_application.py
@@ -0,0 +1,38 @@
+from email.message import Message
+from re import M
+import sys
+sys.path.append('..')
+
+from book_pb2 import Book
+from google.protobuf.json_format import MessageToJson
+
+book = Book()
+book.isbn = 'Isbn1'
+book.title = 'Title1'
+book.price = 19.99
+book.pages = 200
+book.available = True
+book.tags.append("sports")
+book.tags.append("politics")
+print(book)
+
+with open("book.bin", "wb") as f:
+    print("write as binary")
+    bytesAsString = book.SerializeToString()
+    f.write(bytesAsString)
+
+
+with open("book.bin", "rb") as f:
+    print("read values")
+    book_read = Book().FromString(f.read())
+
+
+print(book_read)
+
+
+print("Is Available?: " + str(book_read.available))
+print("Isbn?: " + str(book_read.isbn))
+print("Tags?: " + str(book_read.tags))
+
+json_book = MessageToJson(book)
+print(json_book)
