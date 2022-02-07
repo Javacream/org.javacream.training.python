8cb943cdc2f9ab13e505ef440764b17c8d91d7e2
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-02-07 16:17:33 +0100

    list omprehension

--- basics/src/6-Komplexe Datenverarbeitung/people.py
+++ basics/src/6-Komplexe Datenverarbeitung/people.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 counter = 0
 def create_person(lastname, firstname):
     global counter
@@ -12,9 +15,20 @@ s = {p1, p2, p3, p1, p2}
 l = [p1, p2, p3, p1, p2]
 d = {p1[0]:p1, p2[0]:p2, p3[0]:p3}
 
-print(s)
-print(l)
-print(d)
+#print(s)
+#print(l)
+#print(d)
 
+sawitzkis = []
+for person in s:
+    if "Sawitzki" == person[1]:
+        sawitzkis.append(person)
 
+print (sawitzkis)
+
+#Comprehension
+
+#sawitzkis2 = [person for person in l]  //sawitzki2 ist eine Liste von Personen-Tuple
+#sawitzkis2 = [person[1] for person in l]  //sawitzki2 ist eine Liste von Personen-Nachnamen, der erste Teil definiert "was will ich haben?"
+sawitzkis2 = [person for person in l if "Sawitzki" == person[1] ]
 
