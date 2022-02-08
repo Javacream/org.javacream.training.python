a42185857c4fde7cd5dcc44ecfae48d8a3274fe5
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-02-08 20:29:22 +0100

    add simple rest server using Flask

---
+++ rest/server/simple_rest_server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import datetime 
+
+app = Flask(__name__)
+@app.route('/echo/<message>', methods=['GET'])
+def echo(message):
+    print(message)
+    return message
+
+@app.route('/echo', methods=['POST'])
+def say_hello():
+    print("saying hello")
+    return "Hello " + datetime.datetime.now() 
+
+
+app.run(port=5000)
