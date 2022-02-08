c8706b353f5529616b4862db416bb2f40f491b92
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-02-08 20:12:14 +0100

    add simple rest client

---
+++ rest/client/book_restclient.py
@@ -0,0 +1,32 @@
+import aiohttp
+import asyncio
+import json
+
+async def main():
+
+    async with aiohttp.ClientSession() as session:
+
+        url = 'http://h2908727.stratoserver.net:8080/api/books'
+        async with session.get(url) as resp:
+            books_json_string = await resp.text()
+            books_list = json.loads(books_json_string)
+            book_dict = books_list[0]
+            print(book_dict['title'])
+
+
+        url = 'http://h2908727.stratoserver.net:8080/api/books/ISBN1'
+        async with session.get(url) as resp:
+            books_json_string = await resp.text()
+            book_dict = json.loads(books_json_string)
+            print(book_dict['title'])
+
+        url = 'http://h2908727.stratoserver.net:8080/api/books/Python'
+        async with session.post(url) as resp:
+            isbn = await resp.text()
+            print(isbn)
+            async with session.delete('http://h2908727.stratoserver.net:8080/api/books/' + isbn) as resp:
+                pass
+
+#Windows Workaround
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+asyncio.run(main())
