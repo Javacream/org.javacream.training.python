7ec793fe5395e0869c945239e7991fcb3f9b71a5
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-02-09 14:26:49 +0100

    patch coroutines for python 3.6

--- advanced/coroutines.py
+++ advanced/coroutines.py
@@ -8,12 +8,13 @@ async def say_after(delay, what):
 async def main():
     print(f"started at {time.strftime('%X')}")
 
-    future = say_after(1, 'hello')
+    future = say_after(2, 'hello')
     print(dir(future))
     print(future)
+    await say_after(1, 'world')
     await future
-    await say_after(2, 'world')
 
     print(f"finished at {time.strftime('%X')}")
-    
-asyncio.run(main())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+#asyncio.run(main())
