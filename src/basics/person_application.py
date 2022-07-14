6b88f0a100d56c39a51db45f62d9dd9502b2c4ea
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-14 13:59:41 +0200

    basic solution

--- src/basics/person_application.py
+++ src/basics/person_application.py
@@ -1,12 +1,14 @@
 class Person:
-    def __init__(self, lastname, firstname):
+    def __init__(self, lastname, firstname, height, gender):
         self.lastname = lastname
         self.firstname = firstname
+        self.height = height
+        self.gender = gender
 
     def say_hello(self):
-        return "hello, my name is " + self.lastname
-
-person1 = Person("Sawitzki", "Rainer")
-person2 = Person("Meier", "Hannah")
+        return "Hello, my name is " + self.firstname + " " + self.lastname
+ 
+person1 = Person("Sawitzki", "Rainer", 183, 'm')
+person2 = Person("Meier", "Hannah", 199, 'd')
 
 print(person1.say_hello())
