759ebf2172af5d218a0a7116b524d1b859fd179b
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-14 14:32:29 +0200

    remove extended solution

--- src/basics/person_application.py
+++ src/basics/person_application.py
@@ -1,10 +1,4 @@
-from enum import Enum
-
 class Person:
-    class Gender(Enum):
-        MALE=1
-        FEMALE=2
-        DIVERSE=3
     def __init__(self, lastname, firstname, height, gender):
         self.lastname = lastname
         self.firstname = firstname
@@ -12,11 +6,9 @@ class Person:
         self.gender = gender
 
     def say_hello(self):
-        #so genannte String Interpolation
         return f"Hello, my name is {self.firstname} {self.lastname}"
 
-#Besser, aber der Zugriff über den Index ist nicht sprechend 
-person1 = Person("Sawitzki", "Rainer", 183, Person.Gender.MALE)
-person2 = Person("Meier", "Hannah", 199, Person.Gender.DIVERSE)
+person1 = Person("Sawitzki", "Rainer", 183, 'm')
+person2 = Person("Meier", "Hannah", 199, 'd')
 
 print(person1.say_hello())
