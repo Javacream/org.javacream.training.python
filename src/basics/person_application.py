92889512faa927a11c337bd5b213ec5d48885e73
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-14 14:08:44 +0200

    finish extended solution

--- src/basics/person_application.py
+++ src/basics/person_application.py
@@ -1,10 +1,10 @@
 from enum import Enum
-class Gender(Enum):
-    MALE=1
-    FEMALE=2
-    DIVERSE=3
 
 class Person:
+    class Gender(Enum):
+        MALE=1
+        FEMALE=2
+        DIVERSE=3
     def __init__(self, lastname, firstname, height, gender):
         self.lastname = lastname
         self.firstname = firstname
@@ -16,7 +16,7 @@ class Person:
         return f"Hello, my name is {self.firstname} {self.lastname}"
 
 #Besser, aber der Zugriff über den Index ist nicht sprechend 
-person1 = Person("Sawitzki", "Rainer", 183, Gender.MALE)
-person2 = Person("Meier", "Hannah", 199, Gender.DIVERSE)
+person1 = Person("Sawitzki", "Rainer", 183, Person.Gender.MALE)
+person2 = Person("Meier", "Hannah", 199, Person.Gender.DIVERSE)
 
 print(person1.say_hello())
