5514e3d9b2dbd1ae88bbabd60b28cfcfe6ad4c76
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-14 14:00:56 +0200

    start with solution extended

--- src/basics/person_application.py
+++ src/basics/person_application.py
@@ -6,8 +6,10 @@ class Person:
         self.gender = gender
 
     def say_hello(self):
+        #hm, nicht schön, diese Verkettung der Zeichenfolge...
         return "Hello, my name is " + self.firstname + " " + self.lastname
- 
+
+#Hm, Gender als Freitext? Ich dachte, es gibt nur 'm', 'f', 'd' 
 person1 = Person("Sawitzki", "Rainer", 183, 'm')
 person2 = Person("Meier", "Hannah", 199, 'd')
 
