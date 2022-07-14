6cfc17c2fcac266f8e8bb5140deac16bf252e9d1
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-14 14:05:58 +0200

    extended solution 1

--- src/basics/person_application.py
+++ src/basics/person_application.py
@@ -1,4 +1,5 @@
 class Person:
+    Gender = ('m', 'f', 'd')
     def __init__(self, lastname, firstname, height, gender):
         self.lastname = lastname
         self.firstname = firstname
@@ -6,11 +7,11 @@ class Person:
         self.gender = gender
 
     def say_hello(self):
-        #hm, nicht schön, diese Verkettung der Zeichenfolge...
-        return "Hello, my name is " + self.firstname + " " + self.lastname
+        #so genannte String Interpolation
+        return f"Hello, my name is {self.firstname} {self.lastname}"
 
-#Hm, Gender als Freitext? Ich dachte, es gibt nur 'm', 'f', 'd' 
-person1 = Person("Sawitzki", "Rainer", 183, 'm')
-person2 = Person("Meier", "Hannah", 199, 'd')
+#Besser, aber der Zugriff über den Index ist nicht sprechend 
+person1 = Person("Sawitzki", "Rainer", 183, Person.Gender[0])
+person2 = Person("Meier", "Hannah", 199, Person.Gender[2])
 
 print(person1.say_hello())
