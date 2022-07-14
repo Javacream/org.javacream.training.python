8dc23f2c9cd3e417f29516ea2149348bc65f5ee1
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-14 10:24:06 +0200

    scratch

---
+++ src/basics/scratch.py
@@ -0,0 +1,27 @@
+# Zeilenkommentar
+
+# Deklaration einer Variablen
+
+# versuch einer Deklaration, so in Python nicht möglich, keine statische Typisierung
+# string message
+
+# sinnlos
+#message
+
+#Variablen-Deklaration
+# <name> = Literal
+message = 'Hello World'
+number = 42
+
+print(message)
+
+message = "Hugo"
+number = 43
+# sowas ist in Python definitiv möglich, message war vorher eine Zeichenkette und ist im Anschluss eine Ganzzahl
+# Python ist dynamisch
+message = 42
+
+print(message)
+
+
+
