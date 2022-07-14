b116d2eef5a1bf0ff11d550763749ccf9af73b80
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-14 13:52:35 +0200

    start person_application

--- src/basics/person_application.py
+++ src/basics/person_application.py
@@ -1,11 +1,12 @@
-#person_lastname = "Sawitzki"
-#person_firstname = "Rainer"
 class Person:
     def __init__(self, lastname, firstname):
         self.lastname = lastname
         self.firstname = firstname
 
+    def say_hello(self):
+        return "hello, my name is " + self.lastname
+
 person1 = Person("Sawitzki", "Rainer")
 person2 = Person("Meier", "Hannah")
 
-print("finished")
+print(person1.say_hello())
