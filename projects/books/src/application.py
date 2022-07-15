e6d30f50a2932d2d9dcba60a83f3d6009f1697db
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-15 10:27:04 +0200

    add calls to IsbnGenerator and create Book

--- projects/books/src/application.py
+++ projects/books/src/application.py
@@ -1,7 +1,13 @@
 
 from javacream.storeservice import StoreService
+from javacream.isbngenerator import IsbnGenerator
+from javacream.booksservice import Book
 
 
 if __name__ == '__main__':
     store_service = StoreService()
     print (store_service.get_stock("this", "that"))
+    isbn_generator = IsbnGenerator()
+    print (isbn_generator.next_isbn())
+    book = Book(isbn_generator.next_isbn(), "Python", 200, 29.99)
+    print(book)    
