6ddf4cbd8f8e6b9f870fc1008e752d3731a810ca
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-15 10:59:27 +0200

    implement StoreService using Dictionary

--- projects/books/src/javacream/storeservice.py
+++ projects/books/src/javacream/storeservice.py
@@ -1,3 +1,29 @@
 class StoreService(object):
+    def __init__(self):
+        self.store = {}
+    
     def get_stock(self, category, item):
-        return 42
+        category_dict = self.store.get(category, None)
+        if not category_dict:
+            return 0
+        else:
+            stock = category_dict.get(item, None)
+            if stock:
+                return stock
+            else:
+                return 0 
+    def set_stock(self, category, item, stock):
+        category_dict = self.store.get(category, None)
+        if not category_dict:
+            category_dict = {item:stock}
+            self.store[category] = category_dict
+        else:
+            item_dict = category_dict[item]
+            if not item_dict:
+                item_dict ={item:stock}    
+            else:
+                item_dict[item] = stock
+    def get_categories(self):
+        return self.store.keys()
+    def get_items_for(self, category):
+        return self.store.get(category, {}).keys()
