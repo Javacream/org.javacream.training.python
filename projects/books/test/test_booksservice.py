672da9d27181dd975d6b30817be6e5373624fa58
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-15 14:03:19 +0200

    introduce decorators

--- projects/books/test/test_booksservice.py
+++ projects/books/test/test_booksservice.py
@@ -4,11 +4,12 @@ sys.path.append('../src')
 from unittest2.case import TestCase
 #from javacream.isbngenerator import IsbnGenerator
 from javacream.context import context
+import testutils
 class BooksServiceTest(TestCase):
 
 
     def setUp(self):
-        self.books_service = context.books_service
+        self.books_service = testutils.decorate(context.books_service, testutils.trace)
     
     def test_sequence(self):
         isbn = self.books_service.create("Demo", 200, 19.99)
