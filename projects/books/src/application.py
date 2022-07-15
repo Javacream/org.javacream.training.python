5957c5301390690fd66c9f6cd63cc5b9c876f82f
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-15 10:59:04 +0200

    implement ToDo

--- projects/books/src/application.py
+++ projects/books/src/application.py
@@ -11,3 +11,11 @@ if __name__ == '__main__':
     print (isbn_generator.next_isbn())
     book = Book(isbn_generator.next_isbn(), "Python", 200, 29.99)
     print(book)    
+
+    store_service.set_stock("books", "Isbn1", 42)
+    store_service.set_stock("dvd", "egal", 4711)
+    print (store_service.get_stock("books", "Isbn1"))
+    print(store_service.get_categories())
+    print(store_service.get_items_for("books"))
+    print(store_service.get_items_for("unknown"))
+
