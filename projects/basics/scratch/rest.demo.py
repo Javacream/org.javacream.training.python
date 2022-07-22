f83d3db6475c9adf8596acb31a3dfaf16519ff22
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-22 11:14:06 +0200

    extend rest scratch

--- projects/basics/scratch/rest.demo.py
+++ projects/basics/scratch/rest.demo.py
@@ -3,6 +3,14 @@ import requests
 endpoint_url = "http://h2908727.stratoserver.net:8080/api/books"
 
 response = requests.get(endpoint_url)
+#todo: response-Http-Status, 200 OK
 print(response)#der Beginn der Antwort vom Server
+#todo: JSON-Decoder von Pythone wirft eine Exception, falls eine Umwandlung in Python list/dict nicht möglich ist
 result = response.json()
 print(result)
+print(type(result))
+print(type(result[0]))
+#todo: Result ist list/dict, damit ist das Auslesen der Daten auf die dict-Syntax beschränktprint(result[0]['title'])
+print(result[0]['title'])
+#durchaus sinnvoll: Umwandlung des Dictionaries in ein Objekt -> book.title, kann auch generisch erfolgen
+#diskussionswürdig: Umwandlung in eine Instanz der Klasse "Book" 
