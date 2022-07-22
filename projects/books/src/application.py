d1da81a188d4f1417c62b1379eaa1b9c1e9b420e
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-22 15:38:03 +0200

    introduce argparse

--- projects/books/src/application.py
+++ projects/books/src/application.py
@@ -1,23 +1,18 @@
-#from javacream.storeservice import StoreService
-#from javacream.isbngenerator import IsbnGenerator
+import argparse
 from javacream.booksservice import Book
 from javacream.context import context
 
 if __name__ == '__main__':
-    store_service = context.store_service
-    print (store_service.get_stock("this", "that"))
-    isbn_generator = context.isbngenerator
-    print (isbn_generator.next_isbn())
-    book = Book(isbn_generator.next_isbn(), "Python", 29.99)
-    print(book)    
+    parser = argparse.ArgumentParser(description="books script")
+    parser.add_argument('env', choices=['local', 'remote'], metavar="Environment", type=str, nargs=1, help="name of environment")
+    parser.parse_args()
 
-    store_service.set_stock("books", "Isbn1", 42)
-    store_service.set_stock("books", "Isbn2", 41)
-    store_service.set_stock("dvd", "egal", 4711)
-    print (store_service.get_stock("books", "Isbn1"))
-    print(store_service.get_categories())
-    print(store_service.get_items_for("books"))
-    print(store_service.get_items_for("unknown"))
+    #Idee
+    #Migrationsskript: 
+    # Bücher aus dem WebService lesen, availability ist aber immer 'false'
+    # Pro Buch ein get_stock im StoreService
+    # Ausgabe der Bücher mit der tatsächlichen Availability 
+
 
     
 
