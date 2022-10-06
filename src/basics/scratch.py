de9028b781d21cee840a79a26970a8e82b9dc457
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-10-06 11:33:47 +0200

    control structures, if

--- src/basics/scratch.py
+++ src/basics/scratch.py
@@ -1,4 +1,21 @@
-n1 = '9'
-n2 = 4.2
-sum = n1 + str(n2)
-print(sum)
+# Kontroll-Strukturen
+
+# Keywords if elif else, Einrückungen syntaktisch notwenig
+
+n1 = 2
+n2 = 2
+
+if n1 > n2:
+    print('n1 greater than n2')
+elif n1 < n2:
+    print('n2 less n2')
+else:
+    print('n2 equal n2')
+
+# switch-Anweisung existiert, heißt in Python aber "match"
+
+command = "c1"
+#match gibt es erst seit python 3.10
+match command:
+    case 'c1':
+        print ("detected command c1")
