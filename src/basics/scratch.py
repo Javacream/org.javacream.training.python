31ffb44e89a98afed8d0dc9a3ed6d34a761251fd
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-10-06 14:59:21 +0200

    scratch

--- src/basics/scratch.py
+++ src/basics/scratch.py
@@ -1,20 +1,14 @@
 myList = ['A', 'B', 'C']
-myList2 = myList
+print(myList)
+def changeList():
+    #global myList
+    myList = [1,2,3]
+    myList[2] = "ALSO CHANGED"
+    print(myList)
+    return myList
 
-myList2[1] = 'CHANGED'
-print(myList2[1])
+changeList()
+print(myList)
+myList = changeList()
+print(myList)
 
-#TODO
-#Was wird ausgegeben bei print(myList[1])?
-#Schreiben Sie eine Funktion mit einem Parameter, dem Sie eine Liste übergeben und ändern in der Funktion einen Wert der Liste
-
-def changeList(l):
-    l[2] = "ALSO CHANGED"
-
-print(myList[2])
-changeList(myList)
-print(myList[2])
-
-#Todo: Zeichnen Sie ein Stack-Heap-Diagramm und machen Sie sich daran das Verhalten des Programms klar
-#Frage: Arbeitet Python mit Call By Value oder einem Call by Reference? 
-#Recherche: Wie kann dieses Verhalten geändert werden? Hinweis: Gar nicht...
