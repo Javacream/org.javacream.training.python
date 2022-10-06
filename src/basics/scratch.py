b9e240aa5d41a69e37b3aa3034ebf8739beb5a53
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-10-06 11:18:29 +0200

    casting

--- src/basics/scratch.py
+++ src/basics/scratch.py
@@ -1,15 +1,4 @@
-# Kommentar in Python, ab '#' wird alles vom Interpreter überlesen
-
-# Variablen-Deklarationen
-# Zuweisung an eine neue Variable deklariert diese
-# ein Schlüsselwort zur Deklaration ist nicht notwendig, gar nicht vorhanden
-# Variable-Name = Wert-Zuweisung
-# Wert in Python über Literale
-message = "Hello"
-number = 9
-print(message)
-message = number
-print(message)
-message = True
-
-print(message)
+n1 = '9'
+n2 = 4.2
+sum = n1 + str(n2)
+print(sum)
