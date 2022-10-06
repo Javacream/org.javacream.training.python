c887eac619fd0a843fcc8cfc8f6114898a7d32db
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-10-06 15:01:41 +0200

    play around with strings

--- src/basics/scratch.py
+++ src/basics/scratch.py
@@ -1,14 +1,6 @@
-myList = ['A', 'B', 'C']
-print(myList)
-def changeList():
-    #global myList
-    myList = [1,2,3]
-    myList[2] = "ALSO CHANGED"
-    print(myList)
-    return myList
-
-changeList()
-print(myList)
-myList = changeList()
-print(myList)
-
+message = "HeLlO"
+#print(message.lower())
+message.lower()
+print(message)
+lowercaseMessage = message.lower()
+print(lowercaseMessage)
