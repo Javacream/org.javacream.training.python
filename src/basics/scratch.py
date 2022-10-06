a5b09480b0b617a0df29e6987089666868340148
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-10-06 13:12:54 +0200

    set

--- src/basics/scratch.py
+++ src/basics/scratch.py
@@ -1,18 +1,6 @@
-myList = ['Hugo', 'Emil', 'Hugo']
+mySet = {'Hugo', 'Emil', 'Hugo'}
 
-for name in myList:
+for name in mySet:
     print(name)
 
-hugo = myList[0]
-print(hugo)
-
-
-myList[0] = "Fritz" #Fehler, schreibender Zugriff it nicht möglich
-print(myList[0])
-
-#print(myList[3]) # Fehler
-
-#myList[3] = 'fritz' #Fehler -> List-Methode ermöglicht dies
-#print(myList[3])
-
-myList = ('Sawitzki', 183, 'm', True)
+mySet = ('Sawitzki', 183, 'm', True)
