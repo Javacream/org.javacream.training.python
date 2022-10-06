67350504e0f47ee25a6ea58cd2440eb51acb404d
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-10-06 11:14:42 +0200

    variables

---
+++ src/basics/scratch.py
@@ -0,0 +1,15 @@
+# Kommentar in Python, ab '#' wird alles vom Interpreter überlesen
+
+# Variablen-Deklarationen
+# Zuweisung an eine neue Variable deklariert diese
+# ein Schlüsselwort zur Deklaration ist nicht notwendig, gar nicht vorhanden
+# Variable-Name = Wert-Zuweisung
+# Wert in Python über Literale
+message = "Hello"
+number = 9
+print(message)
+message = number
+print(message)
+message = True
+
+print(message)
