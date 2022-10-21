d0553fe1d969f467f6eda7f4b4f6fa53eb4bac21
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-10-21 15:05:11 +0200

    add logging, argparse and decorators

--- src/books/application_context.py
+++ src/books/application_context.py
@@ -1,10 +1,20 @@
+import logging
+
 from books.booksservice import RestClientBooksService
 from books.config import Configuration
+from books.decorators import decorate, trace
 from books.isbngenerator import IsbnGenerator
 from books.storeservice import DatabaseStoreService
+
+file = logging.FileHandler('./books.log')
+console = logging.StreamHandler() # Standard: Konsole
+file.setLevel(logging.DEBUG)
+console.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, handlers=[console, file])
 
 configuration = Configuration()
 isbngenerator = IsbnGenerator(configuration.get_isbngenerator_configuration()['prefix'], configuration.get_isbngenerator_configuration()['suffix'])
 store_service = DatabaseStoreService(configuration.get_database_configuration())
 books_service = RestClientBooksService(configuration.get_configuration('endpoint')['url'], store_service)
+#books_service = decorate(books_service, trace)
 
--- src/books/books_application.py
+++ src/books/books_application.py
@@ -1,3 +1,5 @@
+import argparse
+import re
 import sys
 
 sys.path.append('/home/rainersawitzki/git/org.javacream.training.python/src')
@@ -5,6 +7,11 @@ sys.path.append('/home/rainersawitzki/git/org.javacream.training.python/src')
 from books.application_context import books_service
 
 if __name__=='__main__':
+    parser = argparse.ArgumentParser(description="books script")
+    parser.add_argument('--env', choices=['local', 'remote'], help="name of environment", type=str, nargs=1, required=True )
+    parser.add_argument('--hugo', help="just for fun", type=str)
+    args= parser.parse_args()
+    print(args)
     book = books_service.create("Python")
     print(book)
     books_service.create("Java")
