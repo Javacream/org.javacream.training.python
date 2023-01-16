b40d5980038d33a97b17148ebc2f6973ec34c3f7
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-01-16 15:15:15 +0100

    play with classes

--- src/advanced/oop/scratch_oop.py
+++ src/advanced/oop/scratch_oop.py
@@ -4,24 +4,26 @@
 
 #class Person(lastname, firstname)
 
+def my_func(x):
+    print(x.lastname)
+
 class Person:
     def __init__(self, lastname, firstname):
         self.lastname = lastname
         self.firstname = firstname
+        self.say_hello_as_attribute = my_func
+    def say_hello(self):
+        print(self.lastname)
 
 p = Person("Sawitzki", "Rainer")
+p.say_hello_as_attribute(p)
+p.say_hello()
 
-X = Person
-
-p2 = X("A", "B")
-print(p2.lastname)
+Person.say_hello(p)
+print(dir(Person))
 
 class Object:
     pass
-
 o = Object()
-Person.__init__(o, "47", "11")
-print(o.lastname)
-print(dir(o))
-print(dir(Person))
-    
+o.lastname = "Hugo"
+Person.say_hello(o)
