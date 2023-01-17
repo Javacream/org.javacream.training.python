65755d412274c89630457c5ce66ee664106343ac
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-01-17 16:09:24 +0100

    finish

--- src/advanced/typing/types_example.py
+++ src/advanced/typing/types_example.py
@@ -1,7 +1,10 @@
-from typing import NewType
-
-def func1(p1:str) -> str:
-    return p1
+def func1(p1:str) -> int|str:
+    if True:
+        return p1
+    else:
+        return 42
 
 print(func1("Hugo"))
 print(func1(42))
+
+result: int|str = func1("")
