f1378d3f3783f5a1e9f9b95f5c09729facc29c04
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-02 15:28:43 +0200

    implement simple_app

--- python_programming/simple_app.py
+++ python_programming/simple_app.py
@@ -7,6 +7,27 @@ while userInput != "exit":
         number2 = int(number2AsString)
         sum = number1 + number2
         print(f"das ergebnis der addition von {number1} und {number2} ist {sum}")
+    elif userInput == '-':
+        number1AsString = input("Bitte die erste Zahl eingeben: ")
+        number2AsString = input("Bitte die zweite Zahl eingeben: ")
+        number1 = int(number1AsString)
+        number2 = int(number2AsString)
+        diff = number1 - number2
+        print(f"das ergebnis der subtraktion von {number1} und {number2} ist {diff}")
+    elif userInput == '*':
+        number1AsString = input("Bitte die erste Zahl eingeben: ")
+        number2AsString = input("Bitte die zweite Zahl eingeben: ")
+        number1 = int(number1AsString)
+        number2 = int(number2AsString)
+        product  = number1 * number2
+        print(f"das ergebnis der multiplikation von {number1} und {number2} ist {product}")
+    elif userInput == '/':
+        number1AsString = input("Bitte die erste Zahl eingeben: ")
+        number2AsString = input("Bitte die zweite Zahl eingeben: ")
+        number1 = int(number1AsString)
+        number2 = int(number2AsString)
+        quotient  = number1 / number2
+        print(f"das ergebnis der division von {number1} und {number2} ist {quotient}")
     else:
         print(f"unbekannte eingabe: {userInput}")
     userInput = input("Bitte Kommado eingeben, exit zum beenden oder '+': ")
