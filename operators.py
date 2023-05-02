b40bec09a9387f656b37d1bcea2cd1d8993111a8
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-02 13:38:36 +0200

    introduce booleans

--- operators.py
+++ operators.py
@@ -1,39 +1,20 @@
-number1 = 20
-number2 = 22
-result = number1 + number2
-print(result) # 42: Das ist eine Lieblingszahl von EntwicklerInnen, Stichwprt Douglas Adams, Deep Thougth, Answer to all
+number1 = 9
+number2 = 9
+number3 = 42
 
-result = 2*21
-print(result)
-print(84/2)
+result = number1 == number2
+print(result) # Logische Wert True, True ist das Literal für den logischen Zustand "Wahr", ein sogenanntes "Boolean"
+print(number1 == number3)#False = "Falsch"
 
-result = "Hello " + "World!"
-print(result)
+print(True)#True
+print(not True)#False
 
-#result = "Hello " + number1# error, mischen von Typen ist nicht zulässig
-result = 2*10+22 # Punkt vor Strich-Regel
-print(result)
+print (True and False) # False
+print (True or False) # True
 
-result = 2*(10 + 11)#Klammerung 
-print(result)
-
-print(1 + 1/3) # ergebnis ist eine Kommazahl
-
-print((1 + 1/3)*3)
+print (True and True) # True
+print (True or True) # True
 
 
-#result = 2*(3-5/2)+47 - 3/2 + 7*5 # syntaktisch OK, aber unwartbarer Code
-
-# Kryptisch
-z1 = 12
-z2 = .19
-z3 = z1*(1+z2)
-
-# selbstdokumentierender Code
-netto = 12
-mehrwertSteuerInProzent = .19
-aufschlag = 1 + mehrwertSteuerInProzent
-brutto = netto * aufschlag
-print(brutto)
-
-
+print (False and False) # False
+print (False or False) # False
