de14bfbe3012923e3f8c1cf05db987a11120801a
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-02 14:09:27 +0200

    introduce controll structures

---
+++ control.structures.py
@@ -0,0 +1,13 @@
+number1 = 333
+while number1 < 10:
+    print(number1)
+    number1 = number1 + 2	
+print(f"while loop ist beendet mit dem Wert {number1} für number1") #f"..."", ein Formatted String mit Zugriff auf die Variablen
+ #print(number1)
+
+number2 = 42
+
+if number1 > number2:
+    print("number1 is greater than number2")
+else:
+    print("number1 is smaller or equals than number2")
