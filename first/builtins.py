6b2ff550354bc27a43e23f9571ea7b7606568b8c
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-02 11:45:30 +0200

    introduce print

---
+++ first/builtins.py
@@ -0,0 +1,9 @@
+message = "Hello World" # das steht zwar in der hello.world.py bereits drin, ist hier aber komplett unbekannt
+
+print #Ausgabe auf der Konsole <built-in function print
+
+#print message# error, missing parentheses
+print(message)
+print("Hello Moon")
+#print("Hello Moon)
+#print(hugo)# error: hugo ist als Variable nicht bekannt, weil nirgendwo im Programm eine Zuweisung erfolgte
