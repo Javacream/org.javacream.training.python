75e9fc06eb24651250caa6f4e766f103979fefe9
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-02 15:09:23 +0200

    start simple app

---
+++ python_programming/scratch.py
@@ -0,0 +1,14 @@
+#input = input()
+#print(input)
+#message = [""]
+#typeOfMessage = type(message)
+#print(typeOfMessage)#<class 'str'> -> Eine Zeichenkette, <class 'int'> eine Ganzzahl
+
+#print(type(input()))#sehr veschachtelt, schlecht lesbar -> in der Praxis so bitte nicht
+#print (2 * int("21hugo"))
+
+inputString = input()
+while inputString != "exit":
+    number1 = input()
+    number2=input()
+    inputString = input()
