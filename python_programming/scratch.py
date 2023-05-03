e6f5ec7587d4d5ae8a625882f04d1c6333780119
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-03 13:56:18 +0200

    references and e.g. strings

--- python_programming/scratch.py
+++ python_programming/scratch.py
@@ -1,13 +1,10 @@
-def fn(list):
-    list[0] = "Fritz"
-    print(list[0])
+def fn(string):
+    string = "Fritz"
+    print(string)
 
 def main():
-    names = ["Hugo", "Emil"]
-    fn(names)
-    print(names[0]) # Was ist die Ausgabe: Hugo oder Fritz?
+    name = "Hugo"
+    fn(name)
+    print(name) # Was ist die Ausgabe: Hugo oder Fritz?
 
-    names2 = names # Zuweisung einer Referenez, KEINE KOPIE EINES OBJEKTS
-    names2[1] = "Edgar"
-    print(names[1])
 main()
