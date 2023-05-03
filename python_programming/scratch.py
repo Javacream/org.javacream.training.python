19e7f179c5b5448ef8dcb0162846f62be4884029
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-03 15:08:33 +0200

    string methods

--- python_programming/scratch.py
+++ python_programming/scratch.py
@@ -1,10 +1,7 @@
-def fn(string):
-    string = "Fritz"
-    print(string)
-
 def main():
     name = "Hugo"
-    fn(name)
-    print(name) # Was ist die Ausgabe: Hugo oder Fritz?
-
+    print(len(name))
+    upperName = name.upper()
+    print(name)
+    print(upperName)
 main()
