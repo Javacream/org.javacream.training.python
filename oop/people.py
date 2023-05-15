278f7deb4657cb38c65966e56c9eaa265cdef1c1
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-15 15:26:32 +0200

    add company

--- oop/people.py
+++ oop/people.py
@@ -4,16 +4,26 @@ class Person:
         self.firstname = firstname
     def sayHello(self):
         helloMessage = f"Hello, my name is {self.firstname} {self.lastname}"
+        if hasattr(self, "address") and self.address:
+            helloMessage = f"{helloMessage}, i live in {self.address.city} at {self.address.street}"
         return helloMessage
 class Address:
     def __init__(self, city, street):
         self.city = city
         self.street = street
 
+class Company:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
 def test():
     p1 = Person("Sawitzki", "Rainer")
     a1 = Address("München", "Marienplatz")
     p1.address = a1
+
+    c1 = Company("Javacream", a1)
+    print(p1.sayHello())
+    print(c1)    
     print("test done!")
 
 test()
