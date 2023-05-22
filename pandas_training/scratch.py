16b2b0a9a778fcc79b5c29572b915d2fb5bcd910
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-22 16:03:46 +0200

    introduce data exploration

--- pandas_training/scratch.py
+++ pandas_training/scratch.py
@@ -2,24 +2,12 @@ import os
 import numpy as np
 import pandas as pd
 
-# series1 = pd.Series(data=[188, 166, 177, 185], index=['a', 'b', 'h', 'e'])
-# series2 = pd.Series(data=['Hugo', 'Emil', 'Fritz', 'Fridolin'], index=['a', 'b', 'h', 'e'])
-
-data1 = [188, 166, 177, 185]
-data2 = ['Hugo', 'Emil', 'Frida', 'Helga']
-data3 = ['m', 'd', 'f', 'f']
-
-
-dataDict = {"height": data1, "name": data2, "gender": data3}
-
-dataFrame= pd.DataFrame(dataDict, index=dataDict['name'])
-
-dataFrame["weight"] = [81, 54, 66, 64]
-
-dataFrame["numberOfEyes"] = 2
-
-calculated = dataFrame.height < 180
-print(dataFrame.loc[calculated])
-# Typisches Idiom zur Selektion von Daten
-print(dataFrame.loc[dataFrame.height < 180])
-
+df = pd.read_csv("./titanic.csv")
+df.index = df['PassengerId']
+#print(df.shape)
+#print(df.head())
+#print(df.head())
+#print(df.index[0:3])
+#print(df.columns)
+#print(df.info())
+#print(df.describe())
