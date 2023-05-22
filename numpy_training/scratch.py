61f1274e307cd53405ec260056695cce62be3e70
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-22 10:23:21 +0200

    ply with shape and dtype

--- numpy_training/scratch.py
+++ numpy_training/scratch.py
@@ -1,4 +1,16 @@
 import numpy as np
 
-npArray = np.array([0,1,2,3])
-print(type(npArray))
+
+list1 = [0,0.3,2,3]
+list2 = [9,8,7,6]
+
+np1dArray = np.array(list1)
+np2dArray = np.array([list1, list2])
+
+print(np2dArray.shape)
+print(np1dArray.shape)
+
+npVector = np1dArray
+npMatrice = np2dArray
+
+print(np1dArray.dtype)
