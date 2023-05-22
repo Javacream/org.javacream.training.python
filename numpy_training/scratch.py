695eccc4a4560fc089dd2f745619386a53cb726f
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-22 11:50:37 +0200

    scratch numpy math

--- numpy_training/scratch.py
+++ numpy_training/scratch.py
@@ -5,11 +5,28 @@ list1 = [0,1,2,3]
 list2 = [9,8,7,6]
 list3 = [5,2,1,6]
 
-corePythonArray = [list1, list2, list3]
 
-def flattenCorePython(list):
-    result = []
-    [result.append(sublist) for sublist in list]
-    return result
 
-print(flattenCorePython(corePythonArray))
+
+npArray = [list1, list2, list3]
+npArrayRandom = np.random(3,4)
+npSmallArrayRandom = np.random(1,2)
+
+# Basis-Mathematische Operationen
+
+npArray + 1
+npArray - 1
+npArray * 1
+npArray / 1
+npArray ** 2
+npArray % 2
+
+npArray + npArrayRandom
+npArray * npArrayRandom
+npArray + npSmallArrayRandom
+
+np.sqrt(npArray)
+
+# Matrizen-Operationen (Advanced Math)
+#z.B. Matrix mit Vektor, Matrix mit Matrix, Determinante Bestimmen
+
