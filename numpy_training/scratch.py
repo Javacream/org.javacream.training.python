2b0faad1c0d0675121a1f5fff281bed09286d974
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-22 11:36:39 +0200

    flatten with core python

--- numpy_training/scratch.py
+++ numpy_training/scratch.py
@@ -5,19 +5,11 @@ list1 = [0,1,2,3]
 list2 = [9,8,7,6]
 list3 = [5,2,1,6]
 
-np2dArray = np.array([list1, list2, list3])
+corePythonArray = [list1, list2, list3]
 
-#print(np.zeros(shape = [2,3]))
-#print(np.ones(shape = [2,3]))
-#print(np.identity(3))
+def flattenCorePython(list):
+    result = []
+    [result.append(sublist) for sublist in list]
+    return result
 
-# Reshaping
-print(np2dArray.shape)
-
-print(np2dArray.flatten())
-
-
-reshaped = np.reshape(a=np2dArray, newshape=(4,3), order="F")
-print(reshaped)
-
-print(np.flipud(np2dArray))
+print(flattenCorePython(corePythonArray))
