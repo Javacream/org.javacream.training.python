238a35d536ab6083fe034198d974b5a0344d7b9c
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-23 11:42:21 +0200

    introduce Cabin category

--- data_analysis/titanic.py
+++ data_analysis/titanic.py
@@ -15,19 +15,36 @@ def main():
         #print(dataFrame['Ticket'].describe())
         #print(dataFrame['Cabin'].describe())
         #print(dataFrame['Cabin'].head(15))
-        #pass
         #print(dataFrame['Pclass'].describe())
-        print(dataFrame['Pclass'].head(15))
+        #print(dataFrame['Embarked'].describe())
+        #print(dataFrame['Cabin'].unique())
+        pass
     def cleanup(dataFrame):
         # PassengerId ist redundant, ist der eh vorhandene Index
         del dataFrame['PassengerId']
         del dataFrame['Ticket']
         categorizedSurvival = pd.Categorical(dataFrame["Survived"])
         categorizedSurvival = categorizedSurvival.rename_categories(['died', 'survived'])
-        print(categorizedSurvival.describe())
         dataFrame["Survived"] = categorizedSurvival
-        print(dataFrame.info())
-    
+        categorizedSex = pd.Categorical(dataFrame["Survived"])
+        categorizedSex = categorizedSex.rename_categories({'male': 'Male', 'female': 'Female'})
+        dataFrame["Sex"] = categorizedSex
+        categorizedEmbarked = pd.Categorical(dataFrame["Embarked"])
+        categorizedEmbarked = categorizedEmbarked.rename_categories(new_categories= {'S': 'Southhampton', 'C': 'Cherbourg', 'Q':'Queenstown'})
+        dataFrame["Embarked"] = categorizedEmbarked
+        categorizedPclass = pd.Categorical(dataFrame["Pclass"])
+        categorizedPclass = categorizedPclass.rename_categories(new_categories= {0: 'First', 1: 'Second', 2:'Third'})
+        dataFrame["Pclass"] = categorizedPclass
+
+        cabinDataAsString = dataFrame["Cabin"].astype('str')
+        corePythonCabinList = [cabin[0] for cabin in cabinDataAsString]
+        numpyCabinArray = np.array(corePythonCabinList) 
+        categorizedCabin = pd.Categorical(numpyCabinArray)
+        dataFrame["Cabin"] = categorizedCabin
+        print(dataFrame['Cabin'].unique())
+
+        #print(dataFrame.info())
+
     titanicData = pd.read_csv("./titanic.csv")
     explore(titanicData)
     cleanup(titanicData)
