b72cb31a481081c23f6eaa00eea15173c10e7978
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-23 13:07:37 +0200

    set nan values

--- data_analysis/titanic.py
+++ data_analysis/titanic.py
@@ -4,23 +4,8 @@ import pandas as pd
 
 def main():
     def explore(dataFrame):
-        #print(dataFrame.shape)
-        #print(dataFrame.dtypes)
-        #print(dataFrame.head())
-        #print(dataFrame.info())
-        #print(dataFrame.describe()) #-> in der explore Phase zu früh!
-        #categoricalColumnNames = dataFrame.dtypes[dataFrame.dtypes == 'object'].index
-        #print(dataFrame[categoricalColumnNames].describe())
-        #print(dataFrame['Ticket'].head(15))
-        #print(dataFrame['Ticket'].describe())
-        #print(dataFrame['Cabin'].describe())
-        #print(dataFrame['Cabin'].head(15))
-        #print(dataFrame['Pclass'].describe())
-        #print(dataFrame['Embarked'].describe())
-        #print(dataFrame['Cabin'].unique())
         pass
     def cleanup(dataFrame):
-        # PassengerId ist redundant, ist der eh vorhandene Index
         del dataFrame['PassengerId']
         del dataFrame['Ticket']
         dataFrame.loc[dataFrame["Age"].isnull(),"Age"] = 42.2
@@ -44,7 +29,7 @@ def main():
         numpyCabinArray = np.array(corePythonCabinList) 
         categorizedCabin = pd.Categorical(numpyCabinArray)
         dataFrame["Cabin"] = categorizedCabin
-        print(dataFrame["Embarked"].describe())
+
     titanicData = pd.read_csv("./titanic.csv")
     explore(titanicData)
     cleanup(titanicData)
