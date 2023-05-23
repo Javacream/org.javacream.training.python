9722cce0b9019a5c5abf4d3f3dc1d4dffc2f508f
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-23 15:53:05 +0200

    introduce scatter

--- plots/pandas_scratch.py
+++ plots/pandas_scratch.py
@@ -8,6 +8,7 @@ dataFrame = pd.read_csv('./diamonds.csv')
 #print(dataFrame[dataFrame['carat'] < 0.25])
 #dataFrame.hist(column="carat", color="red", bins=20, range=(0, 4))
 #dataFrame.boxplot(column="carat", by="clarity")
-dataFrame["carat"].plot(kind="density")
+#dataFrame["carat"].plot(kind="density")
+dataFrame.plot(kind="scatter", x="carat", y="price")
 plt.savefig('scratch.png')
 
