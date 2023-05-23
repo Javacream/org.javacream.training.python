42339c2936ba7a2bf2e88b2c112c3e5ac49e3c59
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-23 15:46:49 +0200

    introduce boxplot frouped by categorical variable

--- plots/pandas_scratch.py
+++ plots/pandas_scratch.py
@@ -7,6 +7,6 @@ dataFrame = pd.read_csv('./diamonds.csv')
 
 #print(dataFrame[dataFrame['carat'] < 0.25])
 #dataFrame.hist(column="carat", color="red", bins=20, range=(0, 4))
-dataFrame.boxplot(column="carat")
+dataFrame.boxplot(column="carat", by="clarity")
 plt.savefig('scratch.png')
 
