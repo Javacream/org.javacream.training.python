d0e57503a4553692ae44fe10f1f6caf7db759a8c
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-23 10:52:00 +0200

    start introduce categories

--- data_analysis/titanic.py
+++ data_analysis/titanic.py
@@ -6,12 +6,30 @@ def main():
     def explore(dataFrame):
         #print(dataFrame.shape)
         #print(dataFrame.dtypes)
-        print(dataFrame.head())
+        #print(dataFrame.head())
         #print(dataFrame.info())
         #print(dataFrame.describe()) #-> in der explore Phase zu früh!
-        categoricalColumnNames = dataFrame.dtypes[dataFrame.dtypes == 'object'].index
-        print(dataFrame[categoricalColumnNames].describe())
+        #categoricalColumnNames = dataFrame.dtypes[dataFrame.dtypes == 'object'].index
+        #print(dataFrame[categoricalColumnNames].describe())
+        #print(dataFrame['Ticket'].head(15))
+        #print(dataFrame['Ticket'].describe())
+        #print(dataFrame['Cabin'].describe())
+        #print(dataFrame['Cabin'].head(15))
+        #pass
+        #print(dataFrame['Pclass'].describe())
+        print(dataFrame['Pclass'].head(15))
+    def cleanup(dataFrame):
+        # PassengerId ist redundant, ist der eh vorhandene Index
+        del dataFrame['PassengerId']
+        del dataFrame['Ticket']
+        categorizedSurvival = pd.Categorical(dataFrame["Survived"])
+        categorizedSurvival = categorizedSurvival.rename_categories(['died', 'survived'])
+        print(categorizedSurvival.describe())
+        dataFrame["Survived"] = categorizedSurvival
+        print(dataFrame.info())
+    
     titanicData = pd.read_csv("./titanic.csv")
     explore(titanicData)
+    cleanup(titanicData)
 main()
 
