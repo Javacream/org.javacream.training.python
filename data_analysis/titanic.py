4bf3a24dadd5c956fc2fade38b26b610cd3938c4
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-23 14:26:24 +0200

    finsih concat & merge

--- data_analysis/titanic.py
+++ data_analysis/titanic.py
@@ -29,13 +29,19 @@ def main():
         numpyCabinArray = np.array(corePythonCabinList) 
         categorizedCabin = pd.Categorical(numpyCabinArray)
         dataFrame["Cabin"] = categorizedCabin
-        print(dataFrame.info())
-    titanicData = pd.read_csv("./titanic.csv")
-    incompleteTitanicData = pd.read_csv("./titanic_part2_incomplete.csv")
-    incompleteTitanicData["Survived"] = 0
-    complete = pd.concat([titanicData, incompleteTitanicData])
-    print(complete["Survived"].describe())
-    explore(titanicData)
-    cleanup(complete)
+        print(dataFrame.isnull().values.sum())
+
+    def createDataFrame():
+        titanicData = pd.read_csv("./titanic.csv")
+        incompleteTitanicData = pd.read_csv("./titanic_part2_incomplete.csv")
+        #incompleteTitanicData["Survived"] = 0
+        incompleteTitanicSurvicedData = pd.read_csv("./titanic_part2_survival.csv")
+        titanicData2 = pd.merge(incompleteTitanicData, incompleteTitanicSurvicedData, how="inner", on="PassengerId")
+        complete = pd.concat([titanicData, titanicData2])
+        return complete
+    
+    dataFrame = createDataFrame()
+    explore(dataFrame)
+    cleanup(dataFrame)
 main()
 
