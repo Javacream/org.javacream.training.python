7de1b4529bc33c42f6aedfb994eb9a92c5c5acd6
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-23 15:49:53 +0200

    introduce density

--- plots/pandas_scratch.py
+++ plots/pandas_scratch.py
@@ -7,6 +7,7 @@ dataFrame = pd.read_csv('./diamonds.csv')
 
 #print(dataFrame[dataFrame['carat'] < 0.25])
 #dataFrame.hist(column="carat", color="red", bins=20, range=(0, 4))
-dataFrame.boxplot(column="carat", by="clarity")
+#dataFrame.boxplot(column="carat", by="clarity")
+dataFrame["carat"].plot(kind="density")
 plt.savefig('scratch.png')
 
