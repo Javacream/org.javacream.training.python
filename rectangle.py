fdcf46df40671a9653eead170b47f2f02153710c
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-09-04 14:03:46 +0200

    introduce str

--- rectangle.py
+++ rectangle.py
@@ -4,8 +4,9 @@ rectangle_width = 10
 rectangle_height = 5
 
 rectangle_area = rectangle_height*rectangle_width
-
-print(rectangle_area)
+rectangle_area_string = str(rectangle_area)
+result = "Area: " + rectangle_area_string
+print(result)
 
 rectangle_perimeter = 2 * (rectangle_height + rectangle_width)
 print(rectangle_perimeter)
