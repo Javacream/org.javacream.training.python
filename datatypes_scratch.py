d0697eb26a9f53134bf4e00324e96e5cd1129abf
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-09-04 13:57:08 +0200

    play with types

---
+++ datatypes_scratch.py
@@ -0,0 +1,34 @@
+variable = 1 # 1 ist ein Zahlen-Literal
+variable = 1.1 # 1 ist ein Zahlen-Literal
+variable = "1" # ein String-Literal
+variable = '1' # ein String-Literal
+variable = True # Boolean Literal
+variable = False # Boolean Literal
+
+# wrong = a # falsch, a ist eine Variable
+# wrong = "a' # Anführungszeichen immer pärchenweise
+
+# Berechnungen
+
+number1 = 20
+number2 = 22
+
+result = number1 + number2
+result = number1 - number2
+print(result)
+
+s1 = "20"
+s2 = "22"
+result = s1 + s2
+# result = s1 - s2 falsch, Zeichenketten können nicht subtrahiert / multipliziert / dividiert werden
+print(result)
+
+result = 42 * "Hu" # Ergebnis Huhu
+# result = "Hu" / 2 # falsch
+# result = number1 + s1 + number2 # falsch
+print(result)
+
+
+
+
+
