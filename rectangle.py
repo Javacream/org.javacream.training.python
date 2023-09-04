b6a3ebba7bf5a0bf534c05fe1a11dfb49b44701b
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-09-04 14:04:57 +0200

    shortened

--- rectangle.py
+++ rectangle.py
@@ -4,9 +4,8 @@ rectangle_width = 10
 rectangle_height = 5
 
 rectangle_area = rectangle_height*rectangle_width
-rectangle_area_string = str(rectangle_area)
-result = "Area: " + rectangle_area_string
-print(result)
+
+print("Area: " + str(rectangle_area))
 
 rectangle_perimeter = 2 * (rectangle_height + rectangle_width)
 print(rectangle_perimeter)
