abc85c1b164eb38c78e4c71383122f0ce6a74f7a
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-09-05 11:28:12 +0200

    varibles only in functions

--- functions_parameters_variables.py
+++ functions_parameters_variables.py
@@ -1,17 +1,19 @@
-name = "Hugo"
+
 
 def fn1():
-    global name
-    name = "Emil"
     var_fn1 = 42
     print(var_fn1)
-    print(name)
+   # print(name)
 def fn2():
     var_fn2 = 4711    
     print(var_fn2)
-    # print(var_fn1)  # Laufzeitfehler, var_fn1 ist hier nicht bekannt!
-    print(name)
+   # print(var_fn1)  # Laufzeitfehler, var_fn1 ist hier nicht bekannt!
+   # print(name)
 
-fn1()
-fn2()
 
+def main():
+    name = "Hugo"
+    fn1()
+    fn2()
+
+main()
