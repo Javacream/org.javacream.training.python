2b68546c35a0c9dac1ae99d261be0ff2e733d520
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-09-05 11:41:22 +0200

    functions and params

--- functions_parameters_variables.py
+++ functions_parameters_variables.py
@@ -10,10 +10,16 @@ def fn2():
    # print(var_fn1)  # Laufzeitfehler, var_fn1 ist hier nicht bekannt!
    # print(name)
 
+def fn3(param):
+    print(param)
 
 def main():
     name = "Hugo"
     fn1()
     fn2()
+    # fn3() # fehlender Übergabeparameter
+    # param = 42 fn3(), nur konzeptuell, syntaktisch nicht korekt und überflüssig!
+    fn3(42) # korrekt, implizit wird hier vor dem Eintritt in die function fn3 param=42 gesetzt
+    fn3(name) # korrekt, implizit wird hier vor dem Eintritt in die function fn3 param=name gesetzt
 
 main()
