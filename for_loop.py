cafd0b7d9ff7a183e5bd5110244cbb54499983ec
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-09-05 14:56:30 +0200

    for loop with range

---
+++ for_loop.py
@@ -0,0 +1,10 @@
+def loop_with_range():
+    # interval = range(5)
+    # interval = range(-1, 3)
+    # interval = range(5, 1) # sinnlos
+    # interval = range(1, 5, 2)
+    interval = range(5, 1, -1) 
+    for element in interval:
+        print(element)
+
+loop_with_range()
