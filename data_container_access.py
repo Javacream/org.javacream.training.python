193b11046c607ef6edc8e4aee3251702413d1456
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-09-05 15:31:43 +0200

    access elements in container

---
+++ data_container_access.py
@@ -0,0 +1,16 @@
+def access_range():
+    interval = range(5, 1, -1) 
+    element = interval[0]
+    print(element)
+def access_string():
+    name = "Hugo"
+    element = name[0]
+    print(element)
+def access_tuple():
+    seasons = ("Spring", "Summer", "Autumn", "Winter")
+    element = seasons[0]
+    print(element)
+
+access_range()
+access_string()
+access_tuple()
