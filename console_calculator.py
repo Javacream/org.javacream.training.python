71fb6f18e42b8f3a84fe68bf9e0af7d04e52541e
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-09-05 14:15:53 +0200

    refactor calculator

--- console_calculator.py
+++ console_calculator.py
@@ -1,11 +1,5 @@
 # simple console based calculator
 
-def main_loop():
-    input_string = input("enter exit to stop: ")
-    if (input_string == "exit"):
-        return False
-    else:
-        return True
 def select_operation():
     return input("enter operation: ")
 def input_number1():
@@ -27,26 +21,29 @@ def times(number1, number2):
 def divided_by(number1, number2):
     return number1 / number2
 
+def execute(operation):
+    if (operation == "+" or operation == "-" or operation == "*" or operation == "/"):
+        number1 = input_number1()
+        number2 = input_number2()
+        if (operation == "+"):
+            result = plus(number1, number2)
+        if (operation == "-"):
+            result = minus(number1, number2)
+        if (operation == "*"):
+            result = times(number1, number2)
+        if (operation == "/"):
+            result = divided_by(number1, number2)
+        print (str(number1) + operation + str(number2) + "=" + str(result))
+    else:
+        print("Unknown operation: " + operation)
+
 
 def main():
     #pass # placeholder for sequence
-    go_on = main_loop()
-    while(go_on):
+    while(True):
         operation = select_operation()
-        if (operation == "+" or operation == "-" or operation == "*" or operation == "/"):
-            number1 = input_number1()
-            number2 = input_number2()
-            if (operation == "+"):
-                result = plus(number1, number2)
-            if (operation == "-"):
-                result = minus(number1, number2)
-            if (operation == "*"):
-                result = times(number1, number2)
-            if (operation == "/"):
-                result = divided_by(number1, number2)
-            print (str(number1) + operation + str(number2) + "=" + str(result))
-        else:
-            print("Unknown operation: " + operation)
-        go_on = main_loop()
+        if (operation == 'exit'):
+            break
+        execute(operation)
 
 main()
