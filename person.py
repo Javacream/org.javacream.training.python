847c3f31e384de4451ff74d9aced78e9dad2f17e
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-09-07 11:56:13 +0200

    start with __str__

--- person.py
+++ person.py
@@ -1,3 +1,10 @@
+class Address:
+    def __init__(self, postal_code, city, street):
+        self.postal_code = postal_code
+        self.city = city
+        self.street = street
+    def __str__(self):
+        return "Address: city=" + self.city + ", street=" + self.street # besser: format()
 class Person:
     def __init__(self, lastname, firstname, height, weight, address): # Constructor
         self.lastname = lastname
@@ -8,14 +15,9 @@ class Person:
     def say_hello(self):
         return "Hi, my name is " + self.lastname + ", i live in " + self.address.city
 
-class Address:
-    def __init__(self, postal_code, city, street):
-        self.postal_code = postal_code
-        self.city = city
-        self.street = street
 
 a = Address(81371, 'München', "Marienplatz")
-
 p1 = Person("Sawitzki", "Rainer", 183, 81, a)
 p2 = Person("Mustermann", "Andrea", 176, 66, a)
-print(p1.say_hello())
+
+print(a.__str__())
