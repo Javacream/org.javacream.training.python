02aedabf3b73b71723e6a76ec5eda399d5eeaa50
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-09-07 10:25:08 +0200

    add say_hello method

--- person.py
+++ person.py
@@ -4,12 +4,19 @@ class Person:
         self.firstname = firstname
         self.height = height
         self.weight = weight
+    # say_hello als Methode ist richtig
+    def say_hello(self):
+        return "Hi, my name is " + self.lastname
 
+# say_hello als top-level funktion ist strange, aber würde funktionieren
+def say_hello(person):
+    return "Hi, my name is " + person.lastname
 
 p1 = Person("Sawitzki", "Rainer", 183, 81)
 print(p1.lastname)
 p2 = Person("Mustermann", "Andrea", 176, 66)
 print(p2.lastname)
-
+print(say_hello(p1))
+print(p1.say_hello())
 
 
