f3389557c6b8bb799e8aca60c4804e40b72d5f1f
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-09-07 09:40:37 +0200

    start introduce classes

---
+++ person.py
@@ -0,0 +1,16 @@
+class Person:
+    pass
+
+def person_is_a_type():
+    s = str("Hugo")
+    print(type(s))
+
+    my_set = set()
+    print(type(my_set))
+
+    my_dict = dict()
+    print(type(my_dict))
+
+    p = Person()
+    print(type(p))
+
