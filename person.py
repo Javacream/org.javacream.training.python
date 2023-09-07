d7da08ffb806a278bc248ca823a996cfecf94b6e
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-09-07 12:00:44 +0200

    implement __str__

--- person.py
+++ person.py
@@ -4,7 +4,8 @@ class Address:
         self.city = city
         self.street = street
     def __str__(self):
-        return "Address: city=" + self.city + ", street=" + self.street # besser: format()
+        str_template = "Address: postalCode={}, city={}, street={}"
+        return str_template.format(self.postal_code, self.city, self.street)
 class Person:
     def __init__(self, lastname, firstname, height, weight, address): # Constructor
         self.lastname = lastname
@@ -14,10 +15,15 @@ class Person:
         self.address = address
     def say_hello(self):
         return "Hi, my name is " + self.lastname + ", i live in " + self.address.city
+    def __str__(self):
+        str_template = "Person: lastname={}, firstname={}, height={}, weight={}"
+        return str_template.format(self.lastname, self.firstname, self.height, self.weight)
 
 
 a = Address(81371, 'München', "Marienplatz")
 p1 = Person("Sawitzki", "Rainer", 183, 81, a)
 p2 = Person("Mustermann", "Andrea", 176, 66, a)
 
-print(a.__str__())
+print(a)
+print(p1)
+print(p2)
