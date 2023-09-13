f4dc1f81f16dc53bf85ca01ae8b5eb780d071de1
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-13 13:26:45 +0200

    play with numpy: arrays part 1

---
+++ collections_revisited.py
@@ -0,0 +1,12 @@
+list1 = [1,2,3,4]
+list2 = [4,7,1,3]
+
+list3 = list1 + list2
+print(list3) # die Addition führt zu einer Summen-Liste
+
+# Elementweises Addieren ist auch sinnvoll
+# Ergebnis der Addition: [5, 9, 4, 7]
+
+# list4 = list1 + 2 # Laufzeitfehler
+#print(list4)
+# Sinnvoll: [3,4,5,6]
---
+++ numpy_scratch.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+list2d = [[1,2,3], [4,5,7]] # Ein Core Python Array
+array2d = np.array(list2d)
+
+array = np.arange(0, 9)
+# Andere Funktionen zum Erzeugen, z.B.
+#array = np.zeros((2,3)) # ones, empty, ...
+reshaped_array = array.reshape(3,3)
+re_reshaped_array = reshaped_array.reshape(1,9)
+# print(array, reshaped_array, re_reshaped_array)
+print(array2d.shape)
+# Diemnsionen in numPy werden as Achsen, axis bezeichnet
+
+list3d = [[[1,9],[2,9]], [[4,9],[5,9]]] # Ein Core Python Array
+print(np.array(list3d).shape)
+
+print(array[0])
+
