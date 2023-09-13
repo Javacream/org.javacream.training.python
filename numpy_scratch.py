f4fbe2db2a90f2f780fa12e8285b19acbd2a855b
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-13 14:09:26 +0200

    play

--- numpy_scratch.py
+++ numpy_scratch.py
@@ -4,10 +4,21 @@ a1 = np.array([[1,1], [2,3]])
 a2 = np.array([[4,5], [6,7]])
 
 result = np.concatenate((a1, a2), axis=1) # keine Objektorientierte-Programmierung=OOP -Formulierung
-print(result)
-print(result.shape)
+#print(result)
+#print(result.shape)
 
 # append: 
-print(np.append(a1, a2))
+#print(np.append(a1, a2))
 
 # vstack und hstack
+
+
+# print(np.arange(0,9).reshape(3,3).shape)
+
+a = np.arange(0,8)
+print(a)
+reshaped = a.reshape(2,4)
+print(a)
+print(reshaped)
+a.shape = (4,2)
+print(a)
