cf76a56cae5be76212593e5722a63083c48cd47f
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-13 14:46:56 +0200

    play with conditions

--- numpy_scratch.py
+++ numpy_scratch.py
@@ -8,5 +8,5 @@ print(result)
 result = np.any(data > 10)
 print(result)
 
-result = np.all((data > 10) & (data < 100))  # NumPy verknüpft logische Ausdrücke mit & (UND-Verknüpfung) bzw. dem | (ODER)
+result = np.all((data > 10) & (data < 100))  # NumPy verknüpft logische Ausdrücke mit & (UND-Verknüpfung) bzw. dem | (ODER), ~ Negation
 print(result)
