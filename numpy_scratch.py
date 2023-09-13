dd787454e2375d73764543f6d1d255bb7eb39b6c
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-13 14:52:57 +0200

    play with where and access elements using array

--- numpy_scratch.py
+++ numpy_scratch.py
@@ -1,12 +1,8 @@
 import numpy as np
 
 data = np.linspace(1, 50, 11)
-print(data)
 
-result = np.all(data > 10)
-print(result)
-result = np.any(data > 10)
+result = np.where((data < 30) & (data > 10))
 print(result)
 
-result = np.all((data > 10) & (data < 100))  # NumPy verknüpft logische Ausdrücke mit & (UND-Verknüpfung) bzw. dem | (ODER), ~ Negation
-print(result)
+print(data[result])
