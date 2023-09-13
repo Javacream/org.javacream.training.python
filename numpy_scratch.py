c8393a47f50256448b239ac6ea005d053f95f67e
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-13 15:13:31 +0200

    play with broadcast = basic math operations

--- numpy_scratch.py
+++ numpy_scratch.py
@@ -1,8 +1,11 @@
 import numpy as np
 
-data = np.linspace(1, 50, 11)
+data1 = np.linspace(1, 50, 12).reshape(2,6)
+data2 = np.linspace(42, 64, 12).reshape(2,6)
+data3 = np.array([5])
+data4 = 9
+# Was ist das Ergebnis einer Addition dieser beiden Arrays?
 
-result = np.where((data < 30) & (data > 10))
-print(result)
-
-print(data[result])
+# print(data1 + data2) # Elementweises +, -, *, /, funktioniert, weil beide Arrays die gleiche Form = shape haben
+#print(data1 + data3)
+print(data1 + data4)
