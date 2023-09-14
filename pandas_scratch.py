b440b8fedad071d843f7d1306b767a28d05a906f
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-14 12:11:11 +0200

    introduce dtype, head() and tail()

--- pandas_scratch.py
+++ pandas_scratch.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv('books.csv', index_col='ISBN')
-df2 = pd.read_csv('books2.csv', index_col='ISBN')
 
-#print(df.index.union(df2.index)) # Indizes aus beiden DataFrames aber natürlich ohne Duplikate
-print(df.index.symmetric_difference(df2.index))
+print(df.dtypes)
+
+print(df.tail(1))
