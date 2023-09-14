674c9ef943a13f7b92aee661548eabf37e2c1297
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-14 12:04:47 +0200

    play with set-like operations

--- pandas_scratch.py
+++ pandas_scratch.py
@@ -1,14 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv('books.csv', index_col='ISBN')
+df2 = pd.read_csv('books2.csv', index_col='ISBN')
 
-#selection = [False]*df.index.size
-#selection[66] = True
-
-#print(df[selection])
-
-#selection = df['Price'] > 95
-#print(df[selection])
-
-result = df[df['Price'] > 95]
-print(result)
+#print(df.index.union(df2.index)) # Indizes aus beiden DataFrames aber natürlich ohne Duplikate
+print(df.index.symmetric_difference(df2.index))
