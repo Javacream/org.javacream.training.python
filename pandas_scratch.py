32d1c74fe46b616f036668c5dd02468d1d7369dd
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-14 11:39:35 +0200

    play with selection not using index

--- pandas_scratch.py
+++ pandas_scratch.py
@@ -2,12 +2,13 @@ import pandas as pd
 
 df = pd.read_csv('books.csv', index_col='ISBN')
 
-#x = df.loc
-#x[3:7, ['Title', 'Price']]
-#df.loc[3:7, ['Title', 'Price']]
+#selection = [False]*df.index.size
+#selection[66] = True
 
-#l = [1,2,3,4]
-#l[1:3] = 666
-#print(l)
-print(df.loc['ISBN-2', 'Price'])
-print(df.iloc[1:3])
+#print(df[selection])
+
+#selection = df['Price'] > 95
+#print(df[selection])
+
+result = df[df['Price'] > 95]
+print(result)
