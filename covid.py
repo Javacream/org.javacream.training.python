d4be5c2b63292ca0269e46ee168179aecb0dee15
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-15 10:20:43 +0200

    introduce plots

--- covid.py
+++ covid.py
@@ -1,42 +1,42 @@
 import pandas as pd
 
-def cleanup(df):
-    df.drop(['day', 'month', 'year'], axis = 1, inplace=True) # axis = 1: Spalten löschen, axis = 0: Zeilen nach Index-Wert löschen
-    # drop rows containing missing data
-    #df.dropna(axis = 0, inplace=True)
-    # determine values (default or calculation)
-    # Todo -> country_code xx, population = 1000000
-    df.loc[df['country_code'].isna(), 'country_code'] = 'xx'
-    df.loc[df['population'].isna(), 'population'] = 1000000
-    # Imputation= extrapolation Zeile vorher Zeile Nachher
-    #df.fillna(method='ffill', inplace = True)
-    #print(df.head(10))
-
-def explore(df):
-    print(df.head())
-    print(df.dtypes)
-    print(df.size)
-    print(df.columns)
-    print(df.isna().sum())
-def analyse(df):
-    normalized_deaths = df['deaths'] / df['population']
-    df['normalized deaths'] = normalized_deaths
-    df['normalized cases'] = df['cases'] / df['population']
-    df.to_csv("covid_data.csv")
-    with open('means.txt', 'w') as file:
-        file.write("Deaths worldwide: " + str(df['deaths'].mean()) + "\n")
-        file.write("Cases worldwide: " + str(df['cases'].mean()) + "\n")
-        for continent in df['continent'].unique():
-            selection = df['continent'] == continent
-            file.write("Deaths " + continent + ": " + str(df[selection]['deaths'].mean()) + "\n")
-            file.write("Cases " + continent + ": " + str(df[selection]['cases'].mean()) + "\n")
-
 def main():
+    def cleanup():
+        df.drop(['day', 'month', 'year'], axis = 1, inplace=True) # axis = 1: Spalten löschen, axis = 0: Zeilen nach Index-Wert löschen
+        # drop rows containing missing data
+        #df.dropna(axis = 0, inplace=True)
+        # determine values (default or calculation)
+        # Todo -> country_code xx, population = 1000000
+        df.loc[df['country_code'].isna(), 'country_code'] = 'xx'
+        df.loc[df['population'].isna(), 'population'] = 1000000
+        # Imputation= extrapolation Zeile vorher Zeile Nachher
+        #df.fillna(method='ffill', inplace = True)
+        #print(df.head(10))
+
+    def explore():
+        print(df.head())
+        print(df.dtypes)
+        print(df.size)
+        print(df.columns)
+        print(df.isna().sum())
+    def analyse():
+        normalized_deaths = df['deaths'] / df['population']
+        df['normalized deaths'] = normalized_deaths
+        df['normalized cases'] = df['cases'] / df['population']
+        df.to_csv("covid_data.csv")
+        with open('means.txt', 'w') as file:
+            file.write("Deaths worldwide: " + str(df['deaths'].mean()) + "\n")
+            file.write("Cases worldwide: " + str(df['cases'].mean()) + "\n")
+            for continent in df['continent'].unique():
+                selection = df['continent'] == continent
+                file.write("Deaths " + continent + ": " + str(df[selection]['deaths'].mean()) + "\n")
+                file.write("Cases " + continent + ": " + str(df[selection]['cases'].mean()) + "\n")
+
     covid_data_file = ""#input("Enter convid data file: ")
     if (covid_data_file == ""):
         covid_data_file = "subset-covid-data.csv"
-    data_frame = pd.read_csv(covid_data_file)
-    explore(data_frame)
-    cleanup(data_frame)
-    analyse(data_frame)
+    df = pd.read_csv(covid_data_file)
+    explore()
+    cleanup()
+    analyse()
 main()
--- data_analysis/diamonds.py
+++ data_analysis/diamonds.py
@@ -1,15 +1,18 @@
 import pandas as pd
 
-def explore(df):
-    print(df.info())
-    print(df.head())
-#    print(df.dtypes)
-#    print(df.size)
-#    print(df.columns)
-    print(df.isna().sum())
-
 def main():
     df = pd.read_csv('data_analysis/diamonds.csv')
-    explore(df)
+
+    def explore():
+        print(df.info())
+        print(df.head())
+    #    print(df.dtypes)
+    #    print(df.size)
+    #    print(df.columns)
+        print(df.isna().sum())
+
+    def visualize():
+        pass
+    explore()
 
 main()
