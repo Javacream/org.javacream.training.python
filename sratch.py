fe81af65f9e5bca91f0d44777350543aa820ebb7
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-25 10:33:54 +0200

    introduce runtime type information

---
+++ sratch.py
@@ -0,0 +1,19 @@
+def main():
+    s1 = "Hello"
+    s2 = "World"
+    n1 = 9
+    n2 = 33
+
+    message = s1 + s2
+    print(message)
+
+    sum = n1 + n2
+    print(sum)
+
+    #result = s1 + n1
+    #print(result)
+
+    #diff = s1 - s2
+    diff = n1 -n2
+    print(diff)
+main()
