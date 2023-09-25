46019e6ddb2c25a2dca6c2a7abc51d4a5765c31d
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-25 10:51:53 +0200

    scratch with types

--- sratch.py
+++ sratch.py
@@ -1,19 +1,26 @@
 def main():
-    s1 = "Hello"
-    s2 = "World"
+    s1 = "3"
+    s2 =  "1.6" #"one point six"
     n1 = 9
     n2 = 33
 
     message = s1 + s2
-    print(message)
-
+#    print(message)
+#
     sum = n1 + n2
-    print(sum)
+#    print(sum)
 
     #result = s1 + n1
     #print(result)
 
     #diff = s1 - s2
-    diff = n1 -n2
+    diff = n1 - n2
+#    print(diff)
+
+    # type conversion using build in functions: str() , int(), float()
+
+    result = s1 + str(n1)
+    print(result)
+    diff = int(s1) - float(s2)
     print(diff)
 main()
