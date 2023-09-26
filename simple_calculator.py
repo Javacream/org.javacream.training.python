8ece2eb998d8efe13062d29ef96a99f3edbacd41
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-26 10:10:31 +0200

    implement simple_calculato using inner functions

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,27 +1,36 @@
-def plus(n1, n2):
-    return n1 + n2
-def minus(n1, n2):
-    return n1 - n2
-def times(n1, n2):
-    return n1 * n2
-def diviced_by(n1, n2):
-    return n1 / n2
+
 
 def main():
+    def plus(n1, n2):
+        return n1 + n2
+
+
+    def minus(n1, n2):
+        return n1 - n2
+
+
+    def times(n1, n2):
+        return n1 * n2
+
+
+    def diviced_by(n1, n2):
+        return n1 / n2
     operations = {'+': plus, '-': minus, '*': times, '/': diviced_by}
-    while(True):
+    operation_names = {'+', '-', '*', '/'} # later we will use the keys from our dictionary, but for now let's accept some code replication
+    while (True):
         should_continue = input("continue? (y)")
         if 'y' == should_continue:
             operation = input("operation? (+|-|*|/)")
-            # ToDo: use a set as valid operations
-            if operation != "+" and operation != "-" and operation != "*" and operation != "/":
+            if not operation in operation_names:
                 print("Unknow operation: " + operation)
             else:
                 number1 = int(input("enter first numer: "))
                 number2 = int(input("enter second numer: "))
                 result = operations[operation](number1, number2)
-                print (str(number1) + operation + str(number2) + "=" + str(result))
+                print(str(number1) + operation +
+                      str(number2) + "=" + str(result))
         else:
             break
+
 
 main()
