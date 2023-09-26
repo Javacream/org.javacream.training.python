8bb035543421e2e4b1058c6d363af93e2f7f3329
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-09-26 08:36:54 +0200

    finish scratch

--- collction_scratch.py
+++ collction_scratch.py
@@ -5,8 +5,11 @@ def main():
     my_set = {"John", "Georg", "Paul", "Ringo", "John"}
     my_list = ["Guitar", "Bass", "Drums", "Guitar"]
     my_dict = {"John": "Rhythm Guitar", "Paul": "Bass", "Ringo": "Drums", "George": "Lead Guitar"}
-    print_collection(my_dict)
-    if "Mandolin" in my_list:
+    my_string = "Beatles"
+    my_tuple = (1,2,3)
+    #my_tuple = tuple((1,2,3))
+    print_collection(my_tuple)
+    if 3 in my_tuple:
         print("band has a Mandolin")
 
 main()    
