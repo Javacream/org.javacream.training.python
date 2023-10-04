0a98a161cf84707c2cceb6a1355a70d1a8bf8531
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-10-04 08:05:05 +0200

    introduce methods and vsc assist limitations

---
+++ objects.scratch.py
@@ -0,0 +1,11 @@
+def scratch(p):
+    #p. -> no suggestion
+    pass
+
+def main():
+    name = "foo"
+    def convert():
+        upper_case = name.upper()
+        print(upper_case)
+    convert()
+main()
