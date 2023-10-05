8cb01aa4bde50e9a8201ed57bbbfdd7c0e1a0d7b
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-10-05 06:13:40 +0200

    cleanup

--- exception.scratch.py
+++
@@ -1,9 +0,0 @@
-print("Hello")
-#name = "Foo
-try:
-    my_input = input("Enter number: ")
-    number = int(my_input)
-    print (number - 9)
-except Exception as e:
-    print(e)
-print("World")
--- simple.file.applicarion.py
+++
@@ -1,4 +0,0 @@
-with open('./README.md', 'r') as f:
-    first_line = f.readline()
-    number = int(first_line)
-    print(number)
