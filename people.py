561da149e7f19f6c8953a4da6d21d176f9ecc483
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-10-05 09:17:35 +0200

    refactoring, introduce info_template as static

--- people.py
+++ people.py
@@ -1,4 +1,9 @@
+
+
 class Person:
+    # a static attribute, part of the class object
+    info_template = "Hello, my name is {} {}"
+
     # constructor
     def __init__(self, lastname, firstname, height):
         self.lastname = lastname
@@ -6,12 +11,11 @@ class Person:
         self.height = height
 
     def info(self):
-        info_template = "Hello, my name is {} {}"
-        return info_template.format(self.firstname, self.lastname)
+        return Person.info_template.format(self.firstname, self.lastname)
 
 def main():
-    p1 = Person("Goo", "Georg", 188)
-    p2 = Person("Foo", "Henry", 166)
+    p1 = Person("Goo", "Georg", "big")
+    p2 = Person("Foo", "Henry", "small")
     print(p1.info())
     print(p2.info())
 
