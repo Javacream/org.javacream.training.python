54d1ed3fc9830eb2c232594304958521338518a8
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-10-05 09:31:40 +0200

    start PeolpleManager

--- people.py
+++ people.py
@@ -13,10 +13,32 @@ class Person:
     def info(self):
         return Person.info_template.format(self.firstname, self.lastname)
 
+class PeopleManager:
+    def __init__(self):
+        self.__people__ = {}
+    
+    def create(self, lastname, firstname):
+        default_height = 167
+        counter = 0
+        new_person = Person(lastname, firstname, default_height)
+        self.__people__[counter] = new_person
+        counter = counter + 1
+        return counter
+
+    def find_by_id(self, id):
+        return self.__people__[id]
+
+    def delete_by_id(self, id):
+        self.__people__.pop(id)
+        return True
+
 def main():
-    p1 = Person("Goo", "Georg", "big")
-    p2 = Person("Foo", "Henry", "small")
-    print(p1.info())
-    print(p2.info())
+
+    people_manager = PeopleManager()
+    people_manager.create("Goo", "Georg")
+    people_manager.create("Foo", "Henry")
+
+    search_result = people_manager.find_by_id(0)
+    print(search_result.info())
 
 main()
