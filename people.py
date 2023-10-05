053650ec33664c9f05b993c1a035bd74455b7076
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-10-05 08:14:17 +0200

    introduce Person class

--- people.py
+++ people.py
@@ -1,16 +1,16 @@
 class Person:
-    pass
+    # constructor
+    def __init__(self, lastname, firstname):
+        self.lastname = lastname
+        self.firstname = firstname
+
+    def info(self):
+        return "Hello, my name is " + self.lastname
 
 def main():
-    def info_fn(obj):
-        return "Hello, my name is " + obj.name
-    p1 = Person()
-    p2 = Person()
+    p1 = Person("Goo", "Georg")
+    p2 = Person("Foo", "Henry")
     print(type(p1), type(p2))
-    p1.name = "Goo"
-    p2.name = "Foo"
-    p1.info = info_fn
-    p2.info = info_fn
     #print(p1.name, p2.name)
-    print(p1.info(p1), p2.info(p2))
+    print(p1.info(), p2.info())
 main()
