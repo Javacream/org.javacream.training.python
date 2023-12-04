596edb5543a309becc7e7a1de41b24d97781203a
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-04 14:36:32 +0100

    introduce main function, finish first contact

--- src/first.py
+++ src/first.py
@@ -1,5 +1,7 @@
-user_name = input("Please enter your name: ")
-welcome_message = "Hello"
-greeting = welcome_message + " " + user_name + "!"
-print(greeting)
+def main():
+    user_name = input("Please enter your name: ")
+    welcome_message = "Hello"
+    greeting = welcome_message + " " + user_name + "!"
+    print(greeting)
 
+main()
