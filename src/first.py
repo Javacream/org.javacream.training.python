d9744f1706d8245cefed3cb2df52c48d9ddff7c5
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-04 14:01:43 +0100

    introduce print

--- src/first.py
+++ src/first.py
@@ -1,11 +1,6 @@
-# Das folgende Programm definiert 4 Variable
-# Python-Konvention: Namen sollten sprechend sein ("selbst-dokumentierender Code")
-# Python-Konvention: Zur besseren Lesbarkeit bitte mit _ arbeiten, alternativ arbeiten mit Großbuchstaben
+welcome_message = "Hello"
 
+print(welcome_message)
 
-welcome_message = "Hello" # sprechende Namen statt Zellen-Angaben
-welcomeMessage = "Hello" # weniger gebräuchlich
-amount = 42
-goodbye_message = 'Goodbye'
-price = 4.2
+# pront(welcomemessage) -> Laufzeitfehler, Name Error
 
