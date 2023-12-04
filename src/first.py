0cb137b622f9169665130395afb6ed5dde437be3
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-04 11:27:28 +0100

    introduce self documenting variables

--- src/first.py
+++ src/first.py
@@ -1,15 +1,11 @@
 # Das folgende Programm definiert 4 Variable
+# Python-Konvention: Namen sollten sprechend sein ("selbst-dokumentierender Code")
+# Python-Konvention: Zur besseren Lesbarkeit bitte mit _ arbeiten, alternativ arbeiten mit Großbuchstaben
 
-# Das folgende ist korrekt:
 
-A1 = "Hello" # Zeichenkette in einem Paar doppelter Anführungszeichen
-B1 = 42 # Ganzzzahl ist einfach eine Zahl
-A2 = 'Goodbye' # oder Zeichenkette in einem Paar einfacher Anführungszeichen
-B2 = 4.2 # Kommzahl, das Komma ist in Python immer ein Punkt
+welcome_message = "Hello" # sprechende Namen statt Zellen-Angaben
+welcomeMessage = "Hello" # weniger gebräuchlich
+amount = 42
+goodbye_message = 'Goodbye'
+price = 4.2
 
-# Die folgenden Anweisungen sind aus verschiedenen Gründen nicht korrekt
-
-# A1 = "Hello # fehlendes schließendes Anführngszeichen
-# B1 = 4 2 # leerzeichen zwischen den Ziffern
-# A2 = "Goodbye' # Mischen von einafachen und doppelten Anführungszeichen
-# B2 = 4,2 # Ein Fehler, allerdings einer, den VSC nicht erkennt, es ist zwar eine korrekte Anweisungen, macht aber nicht das gewünschte
