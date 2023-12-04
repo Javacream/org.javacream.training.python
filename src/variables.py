630106e5f3f5efeb1d44d64e17a0e75f9b7e1a12
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-04 15:35:51 +0100

    introduce function scope

--- src/variables.py
+++ src/variables.py
@@ -1,7 +1,16 @@
+def greet(p1):
+    welcome = "Hello"
+    greeting = welcome + " " + p1 + "!"
+    print(greeting)
+    return_value = "OK"
+    return return_value
+
 def main():
-    var1 = "Hugo"
-    print(var1)
-    #var2 
-    #print(var2)
+    name = "Hugo"
+    greet_result = greet(name)
+    print(greet_result)
+
+def something(): # definiert, aber nie aufgerufen
+    print("doing something") 
 
 main()
