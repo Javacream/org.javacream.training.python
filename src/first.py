1c7656794e2260bae10b20a0d411815472c030e3
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-04 15:10:26 +0100

    introduce variables

--- src/first.py
+++
@@ -1,7 +0,0 @@
-def main():
-    user_name = input("Please enter your name: ")
-    welcome_message = "Hello"
-    greeting = welcome_message + " " + user_name + "!"
-    print(greeting)
-
-main()
---
+++ src/variables.py
@@ -0,0 +1,7 @@
+def main():
+    var1 = "Hugo"
+    print(var1)
+    #var2 
+    #print(var2)
+
+main()
