118d724295bab6e36791c86bac229d0eeb3616eb
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-05 09:48:02 +0100

    introduce types

---
+++ src/datatypes.py
@@ -0,0 +1,12 @@
+def main():
+    name = 'Hugo'
+    number = 42
+    price = 9.99
+    #kompakt
+    print(type(price))
+
+    # eventuell einfacher zu lesender ausführlicher Ausdruck
+    type_of_price = type(price)
+    print(type_of_price)
+
+main()
