fa6e762cd9b2095ccdc2cf41bc69e1783536126e
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-05 12:04:13 +0100

    multiline code

---
+++ src/multiline.py
@@ -0,0 +1,18 @@
+result = 1 < 2 \
+    and 3 > 4 \
+    or 3 == 5
+
+print ("a short string 1", "a short string 2")
+print ("a long                                        string 1", 
+       "a long                               string 2")
+
+result = (
+    1 < 2
+    and 3 > 4
+    or 3 == 5
+)
+#so nicht:
+
+#result = 1 < 2
+#    and 3 > 4
+#    or 3 == 5
