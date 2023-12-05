18c218cf64173dd0dc9786c030bc9ad86e59535e
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-05 15:45:47 +0100

    todo slide 55

--- src/calculator.py
+++ src/calculator.py
@@ -17,13 +17,25 @@ def main():
             try:
                 number1 = float(number1_as_string)
                 number2 = float(number2_as_string)
-
-                # sum
-                sum = number1 + number2
-
+                operation = input("enter operation (plus, minus, multiply, divideby)")
+                if (operation == "plus"):
+                    result = number1 + number2
+                    operation_message = "plus"
+                elif (operation == "minus"):
+                    result = number1 - number2
+                    operation_message = "minus"
+                elif (operation == "multiply"):
+                    result = number1 * number2
+                    operation_message = "times"
+                elif (operation == "divideby"):
+                    result = number1 / number2
+                    operation_message = "divided by"
+                else:
+                    print(f"invalid operation: {operation}")
+                    continue # dieses continue ist notwendig, sonst erfolgt eine sinnlose Ausgabe
                 # console output
                 #output_string = 'The sum of ' + number1_as_string + ' and ' + number2_as_string + ' is ' + str(sum)
-                output_string = f'the sum of {number1} and {number2} is {sum}'
+                output_string = f'{number1} {operation_message} {number2} is {result}'
                 print(output_string)
             except:
                 print(f'invalid number, you entered {number1_as_string}, {number2_as_string}')    
