0f16dd04ddb263afad627ec6c611e9a2f8592116
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-06 11:51:49 +0100

    explain oop

---
+++ oop_with_list_change.py
@@ -0,0 +1,12 @@
+def print_to_console(p):
+    print('In call: ' + str(p))
+    p[0] = 'Emil'
+    print('In call after change: ' + str(p))
+    return "OK"
+
+
+def main():
+    names = ['Hugo', 'Andrea']
+    print_to_console(names)
+    #print('After call: ' + str(names))
+main()
