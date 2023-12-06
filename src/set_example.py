41ddf678a4d4075b0176ea0377e8e55aee7796a8
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-06 09:53:13 +0100

    intrdouce set

--- src/set_example.py
+++ src/set_example.py
@@ -1,25 +1,10 @@
 def main():
     # Set-Literal, {element1, ...}
     names = {'Hugo', 'Andrea', 'Helga', 'Hugo'}
-    # Elementzugriff mit [index] nicht bei Set, ein Set hat keine innere Ordnung
-#    second_name = names[1]
-#    print(second_name)
-    #out_of_bound = names[42] # Laufzeitfehler
-    # negative Indizes beginnen von Hinten, nett, aber nicht essenziell
-#    x = names[-1]
-#    y = names[2]
-#    print(x, y)
 
     # Die Länge des Sets wird mit der build-in-Funktion len bestimmt
 
     print(len(names))
-
-    # Iteration mit while (untypisch for Collections)
-#    index = 0
-#    size = len(names)
-#    while index < size:
-#        print(names[index])
-#        index = index + 1
 
     # Iteration mit for (so ist es viel einfacher)
 
