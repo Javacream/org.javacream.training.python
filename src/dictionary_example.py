eebe52f93a71700c30838d51ee1065ba30089fe3
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-07 10:22:07 +0100

    introduce dict, part 4, iteration

--- src/dictionary_example.py
+++ src/dictionary_example.py
@@ -1,23 +1,29 @@
 def main():
     # dictionary literal
     postalCodes = {81377: 'München', 40004: 'Hamburg'}
-    postalCodes[40009] = 'Hamburg'
-    postalCodes[30000] = 'Barlin'
-    postalCodes[30000] = 'Berlin'
-    print(len(postalCodes))
 
-    plz = 81379
-    city = postalCodes.get(81379)
-    if city != None:
-        print(city)
-    else:
-        print(f'city for {plz} not found')    
+    # iteration über das dict iteriert über die keys
+    for key in postalCodes:
+        print(key)
+    
+    for key in postalCodes:
+        print(key, postalCodes[key]) # eher untypisch - items() -> weiter unten
 
-    postalCodes.setdefault(70000, 'Stuttgart') #Sawitzki: besser wäre der name set_or_update
-    print(len(postalCodes))
-    update_dict = {70001: 'Stuttgart', 81377: 'Augsburg'}
-    postalCodes.update(update_dict) #Parameter ist ein Dictionary, das die neuen key-value-Paare enthält, Sawitzki: besser merge
-    print(len(postalCodes))
-    print(postalCodes.get(81377))
+    # keys() liefert ein set der keys
+    for key in postalCodes.keys():
+        print(key)
+
+    # values() liefert ein set der values
+    for value in postalCodes.values():
+        print(value)
+
+    # items() liefert ein tuple der key-value-Paare
+    for key, value in postalCodes.items():
+        print(key, value)
+
+    # Nebenbemerkung
+    items = postalCodes.items()
+    x,y = items # items wird quasi auf die beiden Variablen x, y gesplitted
+
 
 main()
