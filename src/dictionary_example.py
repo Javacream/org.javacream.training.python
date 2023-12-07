3b061ba68bfeafd2c0cf8658562e12a471934cdc
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-07 09:50:37 +0100

    introduce dict, part2

--- src/dictionary_example.py
+++ src/dictionary_example.py
@@ -1,17 +1,9 @@
 def main():
     # dictionary literal
     postalCodes = {81377: 'München', 40004: 'Hamburg'}
-#    city = postalCodes[40009]
-#    print(city)
+    postalCodes[40009] = 'Hamburg'
+    postalCodes[30000] = 'Barlin'
+    postalCodes[30000] = 'Berlin'
+    print(len(postalCodes))
 
-    def own_dict(key):
-        if key == 81377:
-            return 'München'
-        elif key == 40004:
-            return 'Hamburg'
-        else:
-            raise KeyError()
-    
-    city = own_dict(40009)
-    print(city)
 main()
