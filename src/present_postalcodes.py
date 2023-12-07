4774b88258c6c6c8447457d9eea1296eec2fcf1c
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-07 12:10:14 +0100

    presentation step 2

--- src/present_postalcodes.py
+++ src/present_postalcodes.py
@@ -1,8 +1,8 @@
 def main():
 
 
+    postal_codes = dict()
     with open('zuordnung_plz_ort.csv', 'r') as pc_file:
-        postal_codes = dict()
         raw_data = pc_file.readlines()
         raw_data_without_header = raw_data[1:]
         for row in raw_data_without_header:
@@ -10,6 +10,13 @@ def main():
             postal_code = data[3]
             city = data[2]
             postal_codes[postal_code] = city # Hinzufügen des Wertes city unter dem key postal_code
-        
-
+    while True:    
+        user_input = input('enter a 5 digit number: ')
+        #if len(user_input) == 5 and user_input.isnumeric()
+        city = postal_codes.get(user_input)
+        if city != None:
+            print(f'city for postal code {user_input} is {city}')
+        else:
+            print(f'city for postal code {user_input} not found')
+              
 main()
