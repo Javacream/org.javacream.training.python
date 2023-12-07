78990a6fb253e2819aab021bd9b672a53e9871e9
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-07 07:26:19 +0100

    tear down, slide 78

--- src/calculator.py
+++ src/calculator.py
@@ -4,14 +4,12 @@ def main():
 
         # User input
         input_string = input("Input number1 or exit: ")
-        if input_string == 'exit':
+        if input_string.lower() == 'exit':
             break
         else:    
             number1_as_string = input_string
             number2_as_string = input("Input number2: ")
-
-            # Type conversion
-            try:
+            if number1_as_string.replace(".", "").isnumeric() and number2_as_string.replace(".", "").isnumeric():
                 number1 = float(number1_as_string)
                 number2 = float(number2_as_string)
                 while True:
@@ -36,8 +34,7 @@ def main():
                         print(f"invalid operation: {operation}")
                 output_string = f'{number1} {operation_message} {number2} is {result}'
                 print(output_string)
-            except:
+            else:
                 print(f'invalid number, you entered {number1_as_string}, {number2_as_string}')    
-                continue # in diesem Beispiel eigentlich unnötig, da wir uns eh am Ende der while-Schleife befinden
 
 main()
