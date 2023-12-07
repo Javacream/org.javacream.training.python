a52e9be4e6fb3d9b9ae47f933cc6aa28de925d4d
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-07 10:08:44 +0100

    introduce dict, part 3, dictionary methods

--- src/dictionary_example.py
+++ src/dictionary_example.py
@@ -6,4 +6,18 @@ def main():
     postalCodes[30000] = 'Berlin'
     print(len(postalCodes))
 
+    plz = 81379
+    city = postalCodes.get(81379)
+    if city != None:
+        print(city)
+    else:
+        print(f'city for {plz} not found')    
+
+    postalCodes.setdefault(70000, 'Stuttgart') #Sawitzki: besser wäre der name set_or_update
+    print(len(postalCodes))
+    update_dict = {70001: 'Stuttgart', 81377: 'Augsburg'}
+    postalCodes.update(update_dict) #Parameter ist ein Dictionary, das die neuen key-value-Paare enthält, Sawitzki: besser merge
+    print(len(postalCodes))
+    print(postalCodes.get(81377))
+
 main()
