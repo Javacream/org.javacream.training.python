83a62de293d8a0a568003912f1d07fb18c711d2b
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-13 15:59:59 +0100

    play with webservices

--- src/access_webservice.py
+++ src/access_webservice.py
@@ -5,5 +5,5 @@ def main():
     api_url = 'http://javacream.eu:8080/api/books'
     response = requests.get(api_url)
     json_result = response.json()
-    print(json_result)
+    print(json_result[0]['isbn'])
 main()
