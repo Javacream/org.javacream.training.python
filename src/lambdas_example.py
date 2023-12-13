167bdd595c7575a2f6ad510c6c4dbef78039aaed
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-13 11:41:45 +0100

    finnis lambda example

--- src/lambdas_example.py
+++ src/lambdas_example.py
@@ -6,11 +6,11 @@ names.sort(reverse=True)
 print(names)
 
 # Sortieren nach der Länge der Namen:
-# ...
+names.sort(key = lambda name: len(name))
 print(names)
 
 # Sortieren nach dem zweiten Buchstaben des Namen:
-# ...
+names.sort(key = lambda name: name[1])
 print(names)
 
 # ...
