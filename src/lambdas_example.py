4dd9fca343af9f96dc54f2bdecb9fc12744e0d19
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-13 11:47:14 +0100

    play with sort function

--- src/lambdas_example.py
+++ src/lambdas_example.py
@@ -13,4 +13,19 @@ print(names)
 names.sort(key = lambda name: name[1])
 print(names)
 
-# ...
+# Lösung ohne Lambdas
+
+def string_length(s):
+    return len(s)
+
+def second_char_of_string(s):
+    return s[1]
+
+names.sort(key = string_length) # KEINE RUNDEN KLAMMERN, WIR ÜBERGEBEN DIE REFERENZ AUF DIE FUNKTION
+print(names)
+
+names.sort(key = second_char_of_string) # KEINE RUNDEN KLAMMERN, WIR ÜBERGEBEN DIE REFERENZ AUF DIE FUNKTION
+print(names)
+
+names.sort(key = len) # KEINE RUNDEN KLAMMERN, WIR ÜBERGEBEN DIE REFERENZ AUF DIE FUNKTION
+print(names)
