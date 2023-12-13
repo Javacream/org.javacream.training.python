71201b57dbc3497162d67b57ca7772b3e1eaed3b
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-13 11:31:07 +0100

    start sort examples

---
+++ src/lambdas_example.py
@@ -0,0 +1,16 @@
+names = ['Hugo', 'Andrea', "Walther", "Emil"]
+
+names.sort()
+print(names)
+names.sort(reverse=True)
+print(names)
+
+# Sortieren nach der Länge der Namen:
+# ...
+print(names)
+
+# Sortieren nach dem zweiten Buchstaben des Namen:
+# ...
+print(names)
+
+# ...
