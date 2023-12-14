de9c3a5d7894067d889ea789518bfdd039044df2
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-12-14 11:06:21 +0100

    scratch with numpy and pandas

---
+++ numpy_scratch.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.arange(6)
+print(a)
+print(a.shape)
+a2 = a.reshape(3,2)
+print(a.shape)
+print(a2.shape)
+
--- src/access_webservice.py
+++
@@ -1,9 +0,0 @@
-import requests # Keine Standard-Bibliothek, installieren mit pip install requests
-
-def main():
-    #api_url = "https://jsonplaceholder.typicode.com/todos/1"
-    api_url = 'http://javacream.eu:8080/api/books'
-    response = requests.get(api_url)
-    json_result = response.json()
-    print(json_result[0]['isbn'])
-main()
