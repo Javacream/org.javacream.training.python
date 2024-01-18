49a6b46ccaefb7866a8811417266f76b3928e44d
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-01-18 08:50:36 +0100

    Einführung Einrückungen zur Ablaufsteuerung

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,19 +1,13 @@
 # Nach eingabe eines Namens und eines Gewichts wird der Body Mass index berechnet
 
-number1 = 4
-number2 = 8
-
-# Mathematische Berechnungen
-result = number1 + number2
-result = number1 - number2
-result = number1 * number2
-result = number1 / number2
 
 name = input("Bitte Namen eingeben: ")
-weight = input ("Bitte Körpergewicht eingeben: ")
-# weight = weight / name 
-weight = float(weight)
-weight = 2 * weight
-#print("Name: " + name + ", Gewicht: " + weight)# Laufzeitfehler
-#print("Name: " + name + ", Gewicht: " + str(weight)) # funktioniert, ist aber nicht sonderlich schön
-print(f"Name: {name}, Gewicht: {weight}") # Format-String
+weight = input ("Bitte Körpergewicht eingeben als Zahl: ")
+try:
+    weight = float(weight)
+    weight = 2 * weight
+    print(f"Name: {name}, Gewicht: {weight}") # Format-String
+except:
+    print(f"Fehler, das eingegebene Gewicht {weight} kann nicht als Zahl interpretiert werden")
+
+print("Fertig")
