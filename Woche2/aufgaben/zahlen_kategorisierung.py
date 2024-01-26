69784a94323ce0521345573eef775723b5407bc8
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-01-26 08:44:41 +0100

    einführung von sprechenden Variablen

--- Woche2/aufgaben/zahlen_kategorisierung.py
+++ Woche2/aufgaben/zahlen_kategorisierung.py
@@ -1,11 +1,19 @@
-counter = 1
-while (counter <= 30):
-    if (counter % 3 == 0) and (counter % 5 > 0):
-        print(f"{counter}, Fizz")
-    elif (counter % 3 > 0) and (counter % 5 == 0):
-        print(f"{counter}, Buzz")
-    elif (counter % 3 == 0) and (counter % 5 == 0):
-        print(f"{counter}, FizzBuzz")
+START_NUMBER = 1
+END_NUMBER = 30
+FIRST_DIVISOR = 4
+SECOND_DIVISOR = 5
+MESSAGE_FOR_FIRST_DIVISOR = "Fizz"
+MESSAGE_FOR_SECOND_DIVISOR = "Buzz"
+MESSAGE_FOR_FIRST_AND_SECOND_DIVISOR = "FizzBuzz"
+
+counter = START_NUMBER
+while (counter <= END_NUMBER):
+    if (counter % FIRST_DIVISOR == 0) and (counter % SECOND_DIVISOR > 0):
+        print(f"{counter}, {MESSAGE_FOR_FIRST_DIVISOR}")
+    elif (counter % FIRST_DIVISOR > 0) and (counter % SECOND_DIVISOR == 0):
+        print(f"{counter}, {MESSAGE_FOR_SECOND_DIVISOR}")
+    elif (counter % FIRST_DIVISOR == 0) and (counter % SECOND_DIVISOR == 0):
+        print(f"{counter}, {MESSAGE_FOR_FIRST_AND_SECOND_DIVISOR}")
     else:
         print(f"{counter}")
     counter += 1
