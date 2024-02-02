6242cc56de012fd070e8e7d966fc008228287da3
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-02-02 10:55:32 +0100

    Postalcode Übung

--- Woche3/aufgaben/postalcodes.py
+++ Woche3/aufgaben/postalcodes.py
@@ -1,8 +1,20 @@
 def read_data():
-    pass
-
+    with open("Woche3/aufgaben/postalcodes.txt") as file:
+        rows = file.readlines()
+        postalcodes = dict()
+        for row in rows:
+            row = row.replace("\n", "")
+            key_value = row.split(":")
+            postalcodes[key_value[0]] = key_value[1]
+        return postalcodes
 def query(postalcodes):
-    pass
+    while True:
+        user_input = input("Bitte PLZ eingegeben, 'x' für Programmende: ")
+        if user_input == 'x':
+            break
+        else:
+            city = postalcodes.get(user_input)
+            print(f"Stadt für PLZ {user_input} ist {city}")
 
 def main():
     postalcode_dictionary = read_data()
