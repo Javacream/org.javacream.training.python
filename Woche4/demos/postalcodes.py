429d3195736e10558553762b8d35977b10348d2d
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-02-06 08:24:51 +0100

    Beispiel: Reorganisation des Postalcode-Anwendung

--- Woche4/demos/postalcodes.py
+++ Woche4/demos/postalcodes.py
@@ -8,17 +8,17 @@ def main():
                 key_value = row.split(":")
                 postalcodes[key_value[0]] = key_value[1]
             return postalcodes
-    def query(postalcodes):
+    def query():
         while True:
             user_input = input("Bitte PLZ eingegeben, 'x' für Programmende: ")
             if user_input == 'x':
                 break
             else:
-                city = postalcodes.get(user_input)
+                city = postalcode_dictionary.get(user_input)
                 print(f"Stadt für PLZ {user_input} ist {city}")
 
 
     postalcode_dictionary = read_data()
-    query(postalcode_dictionary)
+    query()
 
 main()
