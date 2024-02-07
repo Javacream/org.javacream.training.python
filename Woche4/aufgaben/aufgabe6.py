2a89b60af68361f6210e485dfe0f5bd0393a532a
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-02-07 08:50:02 +0100

    beginne aufgabe6

---
+++ Woche4/aufgaben/aufgabe6.py
@@ -0,0 +1,6 @@
+import datetime
+def main():
+    actual_date = datetime.datetime.now()
+    print(actual_date) # Kontrolle der Darstellung, siehe https://www.w3schools.com/python/python_datetime.asp
+
+main()
