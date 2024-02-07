e361fb8ad153221157ae7582451a19d24d55ee1a
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-02-07 09:17:18 +0100

    scratch eigenes Modul

---
+++ Woche4/demos/data.py
@@ -0,0 +1,6 @@
+# Ein sehr einfaches Demo-Modul
+
+people = [{"id": 1, "name": "Sawitzki", "weight": 81}, {"id": 2, "name": "Mustermann", "weight": 66}, {"id": 3, "name": "Msterfrau", "weight": 56}]
+
+def demo():
+    return "Hello from Data!"
---
+++ Woche4/demos/use_own_module.py
@@ -0,0 +1,5 @@
+import data
+def main():
+    print(data.people)
+
+main()
