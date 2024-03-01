7b28d7d9e2f4d94b0c01a9b0619b47a709409c9a
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-03-01 09:20:19 +0100

    implmentierung und test fertigstellen

--- Woche7/body_mass_index_calculator/überarbeitet_refactoring/bmi_calculator.py
+++ Woche7/body_mass_index_calculator/überarbeitet_refactoring/bmi_calculator.py
@@ -1,8 +1,20 @@
 # Functional
 
 def calculate_bmi(height, weight):
-    pass
-
+    MIN_WEIGHT = 0.5
+    MAX_WEIGHT = 333
+    MIN_HEIGHT = 0.25
+    MAX_HEIGHT = 2.5
+    valid = False
+    if weight >= MIN_WEIGHT and weight <= MAX_WEIGHT:
+        if height >= MIN_HEIGHT and height <= MAX_HEIGHT:
+            bmi = weight / ( height * height ) 
+            valid = True
+    if valid:
+        return bmi
+    else:
+        #return -1 # Signalisiert, dass ein height oder weight nicht korrekt übergeben wurde
+        raise Exception(f"Ungültiger Bereich für Gewicht [{MIN_WEIGHT} - {MAX_WEIGHT}] oder Körpergröße [{MIN_HEIGHT} - {MAX_HEIGHT}]: {weight}, {height}")
 # OOP
 
 class BmiCalculator:
--- Woche7/body_mass_index_calculator/überarbeitet_refactoring/test_bmi_calculator.py
+++ Woche7/body_mass_index_calculator/überarbeitet_refactoring/test_bmi_calculator.py
@@ -3,7 +3,12 @@ import bmi_calculator
 
 class BmiCalculatorTests(unittest.TestCase):
     def test_bmi_calculator_is_available(self):
-        self.assertTrue(False)
+        self.assertIsNotNone(bmi_calculator.calculate_bmi)
 
-
+    def test_height_183_and_weight_75_calculates_bmi_22_3954(self):
+        HEIGHT = 1.83
+        WEIGHT = 75
+        EXPECTED_BMI = 22.3954
+        calculated_bmi = bmi_calculator.calculate_bmi(HEIGHT, WEIGHT)
+        self.assertAlmostEqual(EXPECTED_BMI, calculated_bmi, None, "Not Equal", 0.001)
 unittest.main()
