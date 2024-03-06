2e8582b39a3c80ad392834c669573af2585d5efd
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-03-06 06:31:51 +0100

    add simple server

---
+++ Woche8/demo/echo/echoclient.py
@@ -0,0 +1,24 @@
+import socket
+
+def start_echo_client(host, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+
+    while True:
+        message = input("Enter a message (or 'exit' to quit): ")
+        if message.lower() == 'exit':
+            break
+
+        client_socket.sendall(message.encode('utf-8'))
+
+        data = client_socket.recv(1024)
+        echoed_message = data.decode('utf-8')
+        print(f"Server echoed: {echoed_message}")
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    HOST = "127.0.0.1"  
+    PORT = 12345
+
+    start_echo_client(HOST, PORT)
