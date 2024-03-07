4dcd61c51d2e5ebadc80bb63da2fdc2cba9a8ff4
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-03-07 11:30:13 +0100

    Umstellen auf socketserver

--- Woche8/demo/chat/chatserver.py
+++ Woche8/demo/chat/chatserver.py
@@ -1,44 +1,32 @@
-import socket
-import multiprocessing
+import socketserver
 
-def start_echo_server(host, port):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-            server_socket.bind((host, port))
-            server_socket.listen()
-            print(f"Server listening on {host}:{port}")
-            while True:
-                client_socket, address = server_socket.accept()
-                print(f"{client_socket}")
-                #Sequentiell: chat(client_socket, address)
-                new_process = multiprocessing.Process(target=chat, args=(client_socket, address))
-                new_process.start()
-
-def chat(client_socket, address):
-    with client_socket:
-        print(f"Accepted connection from {client_socket}")
-        actions = {
-            "who_are_you": lambda name : f"I am the Chat server", 
-            "say_hello": lambda name : f"Hi {name}, nice to meet you", 
-        }
-        client_socket.sendall("What is your name?".encode('utf-8'))
-        data = client_socket.recv(1024)
+class ChatHandler(socketserver.BaseRequestHandler)
+def handle(self):
+    print(f"Accepted connection from {self.request}")
+    actions = {
+        "who_are_you": lambda name : f"I am the Chat server", 
+        "say_hello": lambda name : f"Hi {name}, nice to meet you", 
+    }
+    self.request.sendall("What is your name?".encode('utf-8'))
+    data = self.request.recv(1024)
+    if not data:
+        return  # Connection closed by the client
+    name = data.decode('utf-8')
+    self.request..sendall(f"Hi {name}, how can I help you?".encode('utf-8'))
+    while True:
+        data = self.request.recv(1024)
         if not data:
-            return  # Connection closed by the client
-        name = data.decode('utf-8')
-        client_socket.sendall(f"Hi {name}, how can I help you?".encode('utf-8'))
-        while True:
-            data = client_socket.recv(1024)
-            if not data:
-                break  # Connection closed by the client
-            action_command = data.decode('utf-8')
-            action = actions.get(action_command)
-            if action:
-                client_socket.sendall(action(name).encode('utf-8'))
-            else:
-                client_socket.sendall(f"Sorry, i did not understand {action_command}".encode('utf-8'))
+            break  # Connection closed by the client
+        action_command = data.decode('utf-8')
+        action = actions.get(action_command)
+        if action:
+            self.request.sendall(action(name).encode('utf-8'))
+        else:
+            self.request.sendall(f"Sorry, i did not understand {action_command}".encode('utf-8'))
 
 if __name__ == "__main__":
     HOST = "localhost"  
     PORT = 12346
 
-    start_echo_server(HOST, PORT)
+    with socketserver.TCPServer((HOST, PORT), ChatHandler) as server:
+        server.serve_forever()
