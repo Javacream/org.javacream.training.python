986d1a6ed91cf682e7c402400e308685b6f09429
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-06-17 10:50:56 +0200

    vars und literals

---
+++ basics/variable_and_operators.py
@@ -0,0 +1,8 @@
+# Variablen-Deklaration
+# Variablen-Name = Literal
+message = "Hello" # "Zeichenketten-Literal" -> intern ist message ein String, str
+number = 9 # Eine Ganzzahl
+value = 4.2 # Kommazahl
+state = True # Logischer Werte, True False
+# Hinweis: Weitere Literale wie Listen, Bereiche -> später
+
