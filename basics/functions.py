8481f2de72c8c393d605224084cdb86035cf1f39
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-06-17 15:44:03 +0200

    no function overloading

--- basics/functions.py
+++ basics/functions.py
@@ -1,11 +1,18 @@
-def greet(param):
-    print(f"Hello, {param}")
+#def greet(param):
+#    print(f"Hello, {param}")
+
+def greet(param, friendly):
+    if friendly:
+        print(f"Hi, {param}, nice to meet you!")
+    else:
+        print(f"Hello, {param}")
 
 def retrieve_name():
     return input("enter your name: ")
 
 def main():
     name = retrieve_name()
-    greet(name)
+#    greet(name) Funktionen können in Python nicht überladen werden!
+    greet(name, False)
 
 main()
