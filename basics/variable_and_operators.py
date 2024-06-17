d6f0d79b58641d3939207e2d0ab28b3e229adb8c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-06-17 11:00:24 +0200

    play with vars

--- basics/variable_and_operators.py
+++ basics/variable_and_operators.py
@@ -6,3 +6,15 @@ value = 4.2 # Kommazahl
 state = True # Logischer Werte, True False
 # Hinweis: Weitere Literale wie Listen, Bereiche -> später
 
+# print(number) # Verwendung von Variablen
+
+# Variable oder Konstante? -> Variable, Neuzuweisung ist möglich
+message = "World!"
+# print(message)
+
+# Typisierung, ist message immer ein str, prüft Python das?
+message = 42 # Diese Zuweisung ist möglich, Python ist "untyped"
+print(message)
+
+# Verwendung einer nicht-deklarierten Variable
+# print(hugo) # NameError: name 'hugo' is not defined
