56006db47b1b8bfea0fb2460c00362c4f5cd445e
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-06-17 11:44:43 +0200

    introduce builds

---
+++ basics/build_in_functions.py
@@ -0,0 +1,19 @@
+# Typumwandlung, "cast"
+# Von Zahl nach String gfeht, hier ist aber der fString viel besser
+
+number = 9
+# output = "Number is " + number
+number_as_string = str(number)
+output = "Number is " + number_as_string
+
+number = str(number)
+
+# Von String nach Zahl
+number_input1 = "20"
+number_input2 = "22"
+number_input1 = int(number_input1)
+number_input2 = int(number_input2)
+result = number_input1 + number_input2
+
+print(result)
+
