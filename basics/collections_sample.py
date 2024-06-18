656f7bdffff731217c0ec50725405d5b84d3765b
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-06-18 11:39:58 +0200

    play with set, string

---
+++ basics/collections_sample.py
@@ -0,0 +1,14 @@
+for number in range(1, 5, 3):
+    print(number)
+
+set1 = {1,2,3}
+set2 = {7,8}
+union = set1.union(set2)
+
+for element in union:
+    print(element)
+
+name = "Sawitzki"
+for character in name:
+    print(character)  
+print(len(name))   
