7bee86ab37f042e630b8487f40aba774b303476a
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-06-18 09:41:42 +0200

    introduce list

---
+++ basics/list_sample.py
@@ -0,0 +1,15 @@
+def main():
+    names = ["Hugo", "Emil", "Helga", "Andrea", "Emil"]
+    print(names[1]) # -> Emil
+    print(names[4]) # -> Emil
+    # print(names[42]) # -> out of range
+    print(names[-1]) #-> identisch zu names[4]
+    names.append("Franz")
+    print(len(names))
+    for name in names:
+        print(name)
+    index = 0
+    while (index < len(names)):
+        print(names[index])
+        index +=1   
+main()
