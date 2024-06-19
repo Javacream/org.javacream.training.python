85b674c02a4ac844fc02e0b3cd277c7c7343e389
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-06-19 15:45:34 +0200

    play with visualization

--- dataanalysis/pandas_sample.py
+++ dataanalysis/pandas_sample.py
@@ -1,6 +1,8 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 def main():
     df = pd.read_csv('./covid_data.csv')
     print(df.describe())
+    df.hist(column='normalized deaths', bins=100)
+    plt.savefig("convid.png")
 main()
