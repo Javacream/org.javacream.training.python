b15ad94a0d78b1324381136af7f87e60cce3c463
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-06-19 14:11:12 +0200

    introduce databases

--- todos/postal_codes/plz_query.py
+++ todos/postal_codes/plz_query.py
@@ -1,4 +1,5 @@
 import PostalCode as pc
+import mysql.connector
 def pipeline(data):
     #print(f"executing pipeline with data '{data}'") # huge console output!
     postal_codes = dict()
@@ -9,7 +10,15 @@ def pipeline(data):
 def write_result(result):
     #print(f"writing result: '{result}'")
     postal_code = input("Enter a 5-digit Postal Code: ")
-    print(f"city for postal code {postal_code} is {result[postal_code]}")
+    cnx = mysql.connector.connect(host='javacream.eu', port='3406', user='user', password='user', database='javacream')
+    cursor = cnx.cursor()
+    query = f"insert into MESSAGES values('Sawitzki: city for postal code {postal_code}is {result[postal_code]}')"
+    cursor.execute(query)
+    cnx.commit()
+    cursor.close()
+    cnx.close()    
+
+    print(f"Sawitzki: city for postal code {postal_code} is {result[postal_code]}")
 
 def main():
     data = pc.read_data()
