4fa4d4081a4b68ccf3a216274d97e1d74a5b7979
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-06-19 09:12:18 +0200

    play with slices

--- basics/scratch.py
+++ basics/scratch.py
@@ -1,16 +1,14 @@
-i = 9
-j = 9
-i = 42 # j bleibt natürlich auf 9
-
-i2 = 9
-j2 = i2 
-# j2 = 9
-i2 = 42 # j bleibt auf 9
-
-list1 = [9]
-list2 = [9] # das ist eine komplett neue Liste
-list1[0] = 42
-
-l3 = list1
-
-
+# Slicing
+names = ["A", "B", "C", "D", "E"]
+name = names[1] # Zugriff ein Element
+start_index = 1
+end_index = 3
+sub_list = names[start_index:end_index]
+print(sub_list)
+sub_list = names[0:3] #-> 0 ist der Standard!
+sub_list = names[:3]
+sub_list = names[1:len(names)] #-> len(liste) ist der Standard!
+sub_list = names[1:]
+names[-1] # -> E 
+names[0:len(names) - 1] 
+names[:-1] # -> E 
