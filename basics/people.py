cacc10ab808c9ad2e76b083a7726737fdc6cc771
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-06-19 09:59:11 +0200

    finish person and address

--- basics/people.py
+++ basics/people.py
@@ -5,9 +5,31 @@ class Person:
         self.weight = weight
         self.address = address
     def say_hello(self):
-        pass
+        return f"Hello, my name is {self.lastname}"
+    def __repr__(self):
+        return f"Person: lastname={self.lastname}, firstname={self.firstname}"
 
 class Address:
     def __init__(self, city, street):
         self.city = city
         self.street = street
+    def __repr__(self):
+        return f"Address: city={self.city}, street={self.street}"
+
+def main():
+    a1 = Address("München", "Marienplatz")
+    a2 = Address("Hamburg", "Reeperbahn")
+    p1 = Person("Sawitzki", "Rainer", 75, a1)
+    p2 = Person("Müller", "Hannah", 56, a2)
+    p3 = Person("Meier", "Andrea", 96, a2)
+    p1_as_list = list(["Sawitzki", "Rainer", 75, a1])
+ 
+    print(f"der nachname von p1 ist {p1.lastname}")
+    print(f"der nachname von p1_as_list ist {p1_as_list[0]}")
+
+    print(p1)
+    print(p1.say_hello())
+    print("people done")
+
+
+main()
