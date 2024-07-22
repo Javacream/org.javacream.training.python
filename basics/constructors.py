3cc402bf9c2e739d93a1a1d8240ccc791e7ceb11
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-07-22 14:20:22 +0200

    introduce address, todo slide 20

--- basics/constructors.py
+++ basics/constructors.py
@@ -1,22 +1,30 @@
 class Person:
-    def __init__ (self, lastname, firstname=""):
+    def __init__ (self, lastname, firstname):
         if lastname == None:
-            raise Exception("lastname is mandatory")
+            raise Exception("unable to create person, lastname is mandatory")
+        if firstname == None:
+            raise Exception("unable to create person, firstname is mandatory")
         self.lastname = lastname
         self.firstname = firstname
-    def __greet(self):
-        return f'Hello, i am {self.firstname} {self.lastname}'
     def greet(self):
-        return f'Hello, i am {self.firstname} {self.lastname}'
+        greeting =  f'Hello, i am {self.firstname} {self.lastname}'
+        if hasattr(self, 'address'):
+            greeting += f", i live in {self.address.city}"
+        return greeting    
 
-
+class Address:
+    def __init__(self, street, city):
+        if street == None:
+            raise Exception("unable to create address, street is mandatory")
+        if city == None:
+            raise Exception("unable to create address, city is mandatory")
+        self.city = city
+        self.street = street
+        
 def main():
     p1 = Person("Sawitzki", "Rainer")
-    p2 = Person("Meier", "Andrea")
-    # p3 = Person(None, None)
-    p3 = Person("Sawitzki")
+    a1 = Address("Marienplatz", "München")
     print(p1.greet())
-    print(p3.greet())
-    p1.weight = 75
-    print(p1.weight)    
+    p1.address = a1
+    print(p1.greet())
 main()
