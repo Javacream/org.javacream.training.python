75fc56a861a09c707367db607bc2b3e21b20f89a
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-07-22 13:36:54 +0200

    introdice greet method

--- basics/constructors.py
+++ basics/constructors.py
@@ -2,9 +2,12 @@ class Person:
     def __init__ (self, lastname, firstname):
         self.lastname = lastname
         self.firstname = firstname
+    def greet(self):
+        return f'Hello, i am {self.firstname} {self.lastname}'
 
 def main():
     p1 = Person("Sawitzki", "Rainer")
-    print(p1.lastname)
-    print(type(p1))
+    p2 = Person("Meier", "Andrea")
+    print(p1.greet())
+    print(p2.greet())
 main()
