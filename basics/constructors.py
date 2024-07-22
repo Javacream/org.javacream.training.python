079ab489ae208b29c692406a0e81b0d8e2de9a3a
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-07-22 12:04:39 +0200

    introduce self

--- basics/constructors.py
+++ basics/constructors.py
@@ -1,10 +1,10 @@
 class Person:
-    def __init__ (new_object, lastname, firstname):
-        new_object.lastname = lastname
-        new_object.firstname = firstname
+    def __init__ (self, lastname, firstname):
+        self.lastname = lastname
+        self.firstname = firstname
 
 def main():
     p1 = Person("Sawitzki", "Rainer")
     print(p1.lastname)
-
+    print(type(p1))
 main()
