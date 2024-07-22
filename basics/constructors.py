4b1a5cef0088bbfee1343ed5d203a862c52245d5
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-07-22 14:12:44 +0200

    play with class Person

--- basics/constructors.py
+++ basics/constructors.py
@@ -1,13 +1,22 @@
 class Person:
-    def __init__ (self, lastname, firstname):
+    def __init__ (self, lastname, firstname=""):
+        if lastname == None:
+            raise Exception("lastname is mandatory")
         self.lastname = lastname
         self.firstname = firstname
+    def __greet(self):
+        return f'Hello, i am {self.firstname} {self.lastname}'
     def greet(self):
         return f'Hello, i am {self.firstname} {self.lastname}'
+
 
 def main():
     p1 = Person("Sawitzki", "Rainer")
     p2 = Person("Meier", "Andrea")
+    # p3 = Person(None, None)
+    p3 = Person("Sawitzki")
     print(p1.greet())
-    print(p2.greet())
+    print(p3.greet())
+    p1.weight = 75
+    print(p1.weight)    
 main()
