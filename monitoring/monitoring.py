6f3d9069b67f271f872af45eb2970ecad4b2a6f5
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-07-23 11:07:55 +0200

    add Gauge and Counter class

--- monitoring/monitoring.py
+++ monitoring/monitoring.py
@@ -20,26 +20,28 @@ class Ressource:
         return f'CPU={self.cpu}, Memory={self.memory}, Storage={self.storage}'    
 
 class Metric:
-    def __init__(self, type, name, value) -> None:
+    def __init__(self, name, value) -> None:
         self.timestamp = time.time()
-        self.type = type
         self.value = value
         self.name = name
 
     def __repr__(self) -> str:
-        return f"Metric: type={self.type}, name={self.name}, value={self.value}, timestamp={self.timestamp}"
+        return f"Metric: name={self.name}, value={self.value}, timestamp={self.timestamp}"
 
+class Gauge(Metric):
+    pass
+class Counter(Metric):
+    pass
 
 def test():
     machine1 = Machine("Database Server 1", "1.2.3.4", Ressource(8, '128G', '4TB'))
     machine2 = Machine("Web Server 1", "1.2.3.5", Ressource(8, '128G', '4TB'))
-    metric1 = Metric("Gauge", 'CPU Utilization', 0.78)
-    metric2 = Metric("Counter", 'Http Request Counter', 4)
-    metric3 = Metric("Gauge", 'Storage', '123.45B')
+    metric1 = Gauge('CPU Utilization', 0.78)
+    metric2 = Counter('Http Request Counter', 4)
+    metric3 = Gauge('Storage', '123.45B')
     machine_x = Machine("Database Server 1", "1.2.3.6", Ressource(8, '256G', '4TB'))
 
     print(machine1)
     print(machine1 == machine_x)
-
-
+    print(metric1==metric2)
 test()
