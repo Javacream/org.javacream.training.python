93335e3e24128ea8c6a61ce3921f1dbec415f307
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-07-23 11:28:53 +0200

    additional methods

--- monitoring/monitoring.py
+++ monitoring/monitoring.py
@@ -31,17 +31,30 @@ class Metric:
 class Gauge(Metric):
     pass
 class Counter(Metric):
-    pass
+    def __init__(self, name, step, value=0):
+        #self.timestamp = time.time()
+        #self.name = name
+        #self.value = value
+        #Metric.__init__(self, name, value)
+        super().__init__(name, value)
+        self.step = step
+
+    def __repr__(self) -> str:
+        return f"{super().__repr__()}, step={self.step}"
+    def increment(self):
+        self.value += self.step
+    def reset(self):
+        self.value = 0    
 
 def test():
     machine1 = Machine("Database Server 1", "1.2.3.4", Ressource(8, '128G', '4TB'))
     machine2 = Machine("Web Server 1", "1.2.3.5", Ressource(8, '128G', '4TB'))
     metric1 = Gauge('CPU Utilization', 0.78)
-    metric2 = Counter('Http Request Counter', 4)
+    metric2 = Counter('Http Request Counter', 5)
     metric3 = Gauge('Storage', '123.45B')
     machine_x = Machine("Database Server 1", "1.2.3.6", Ressource(8, '256G', '4TB'))
 
     print(machine1)
     print(machine1 == machine_x)
-    print(metric1==metric2)
+    print(metric2)
 test()
