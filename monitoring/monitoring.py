4baa65b7ea260c3dea9ada82bb05de1503a5d37b
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-07-23 09:55:26 +0200

    ToDo slide 26, part1

---
+++ monitoring/monitoring.py
@@ -0,0 +1,45 @@
+import time
+
+class Machine:
+    def __init__(self, name, ip, ressources) -> None:
+        self.name = name
+        self.ip = ip
+        self.ressources = ressources
+    def __repr__(self) -> str:
+        return f'Machine: name={self.name}, ip={self.ip}, ressources={self.ressources}'
+    def __eq__(self, other: object) -> bool:
+        return self.name == other.name
+    def __hash__(self) -> int:
+        return self.name.__hash__()
+class Ressource:
+    def __init__(self, cpu, memory, storage) -> None:
+        self.cpu = cpu
+        self.memory = memory
+        self.storage = storage
+    def __repr__(self) -> str:
+        return f'CPU={self.cpu}, Memory={self.memory}, Storage={self.storage}'    
+
+class Metric:
+    def __init__(self, type, name, value) -> None:
+        self.timestamp = time.time()
+        self.type = type
+        self.value = value
+        self.name = name
+
+    def __repr__(self) -> str:
+        return f"Metric: type={self.type}, name={self.name}, value={self.value}, timestamp={self.timestamp}"
+
+
+def test():
+    machine1 = Machine("Database Server 1", "1.2.3.4", Ressource(8, '128G', '4TB'))
+    machine2 = Machine("Web Server 1", "1.2.3.5", Ressource(8, '128G', '4TB'))
+    metric1 = Metric("Gauge", 'CPU Utilization', 0.78)
+    metric2 = Metric("Counter", 'Http Request Counter', 4)
+    metric3 = Metric("Gauge", 'Storage', '123.45B')
+    machine_x = Machine("Database Server 1", "1.2.3.6", Ressource(8, '256G', '4TB'))
+
+    print(machine1)
+    print(machine1 == machine_x)
+
+
+test()
