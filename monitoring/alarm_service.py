19ddfa02efd80618979634405862fdea6e20e386
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-07-24 14:05:28 +0200

    fix

--- monitoring/alarm_service.py
+++ monitoring/alarm_service.py
@@ -14,5 +14,5 @@ class AlarmManager:
          metrics = self.monitoring.get_metrics_for(machine)
          cpu_alarms = [metric for metric in metrics if  metric.name == 'cpu' and metric.value >= self.cpu_limit]
          self.notifier.send(machine, cpu_alarms)
-         memory_alarms = [metric for metric in metrics if  metric.name == 'memory' and metric.value >= self.memory_threshold/100 * machine.ressource.memory]
+         memory_alarms = [metric for metric in metrics if  metric.name == 'ram' and metric.value >= self.memory_threshold/100 * machine.ressource.memory]
          self.notifier.send(machine, memory_alarms)
--- monitoring/test/test_collectors_services.py
+++ monitoring/test/test_collectors_services.py
@@ -5,7 +5,7 @@ class MetricsCollectorTest(unittest.TestCase):
     def test_collect_metrics(self):
         metrics_collector = context.metrics_collector
         metrics = metrics_collector.collect_metrics()
-        self.assertEqual(3, len(metrics))
+        self.assertEqual(4, len(metrics))
 
 class MachineCollectorTest(unittest.TestCase):
         def test_collect_machines(self):
