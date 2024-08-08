3e38b62100c9f7b8222f2e7f641530ddd1e91f1d
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-08-08 11:35:21 +0200

    Beispiel greeter

--- programme/demo/greeter.py
+++
@@ -1,3 +0,0 @@
-prompt = 'Bitte Namen eingeben: '
-name = input(prompt)
-print(f'Sie haben den Namen {name} eingegeben!')
---
+++ programme/lösungen/greeter.py
@@ -0,0 +1,6 @@
+# Dieses Programm frägt Name und Alter ab und gibt eine formatierte Ausgabe auf die Konsole aus
+prompt = 'Bitte Namen eingeben: '
+name = input(prompt)
+prompt = 'Bitte Alter eingeben: '
+age = input(prompt)
+print(f'{name} ist {age} Jahre alt')
