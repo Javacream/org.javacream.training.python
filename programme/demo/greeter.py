6a38a07139fe3d251f8c848fbe32b1d7011a6567
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-08-08 11:21:42 +0200

    ausgabe mit formatted string

--- programme/demo/greeter.py
+++ programme/demo/greeter.py
@@ -1,2 +1,10 @@
-name = input("Bitte Namen eingeben: ")
-print(name)
+prompt = 'Bitte Namen eingeben: '
+name = input(prompt)
+# Variante 1
+out = 'Sie haben den Namen ' + name + 'eingeben'
+print(out)
+# Variante 2
+print ('Sie haben den Namen ', name, ' eingegeben!')
+
+# Variante 3: Formatted String
+print(f'Sie haben den Namen {name} eingegeben!')
