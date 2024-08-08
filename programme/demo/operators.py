d797d5dbee9b21b9b652ea8901f0fe84e4912634
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-08-08 13:26:35 +0200

    operatoren teil 1

---
+++ programme/demo/operators.py
@@ -0,0 +1,20 @@
+var1 = 5
+var2 = 37
+
+result = var1 + var2
+print(result)
+
+var3 = "Hello"
+var4 = " World"
+
+result = var3 + var4 # Das Ergebnis ist "die Summe" der beiden Zeichenketten
+print(result)
+
+# result = var3 + var1 # Fehler
+print(3*var3)
+
+name = "Hugo"
+age = 42
+
+#print(name + ' ist ' + age + ' Jahre alt')
+print(f'{name} ist {age} Jahre alt')
