69d4c60e2a91a7554fdb9e7d439dc7122372b003
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-08-08 11:22:06 +0200

    bevorzugt

--- programme/demo/greeter.py
+++ programme/demo/greeter.py
@@ -1,10 +1,3 @@
 prompt = 'Bitte Namen eingeben: '
 name = input(prompt)
-# Variante 1
-out = 'Sie haben den Namen ' + name + 'eingeben'
-print(out)
-# Variante 2
-print ('Sie haben den Namen ', name, ' eingegeben!')
-
-# Variante 3: Formatted String
 print(f'Sie haben den Namen {name} eingegeben!')
