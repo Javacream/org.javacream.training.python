71559d255d0fbd2d6c25e83ba1d02a377545c911
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-08-08 13:10:51 +0200

    einführen der ersten Typen

---
+++ programme/demo/types.py
@@ -0,0 +1,23 @@
+var1 = 42 # das ist ein int, eine Ganzzahl
+# type_of_var1 = type(var1)
+# print(type_of_var1)
+print(type(var1))
+
+var2 = 'Hello'
+print(type(var2))# das ist ein str, eine Zeichenkette beliebiger Länge
+
+var3 = "Hello"
+print(type(var3))# das ist ein str, eine Zeichenkette beliebiger Länge
+
+var4 = f"Hello"
+print(type(var4))# das ist ein str, eine Zeichenkette beliebiger Länge
+
+var5 = 4.2
+print(type(var5))# das ist ein float, eine Fleißkommazahl
+
+# to be continued...
+# Logische, Boolschen Werte (8.8.)
+# Datencontainer, z.B. Listen (Block 2)
+
+
+
