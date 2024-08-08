d741e797905cb590ebb6ad433f54f61e6406e61c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-08-08 10:16:50 +0200

    syntax error

--- programme/demo/bmi.py
+++ programme/demo/bmi.py
@@ -5,7 +5,7 @@ weight_sawitzki = 75.3
 height_foo = 176
 weight_foo = 63.1
 
-bmi_sawitzki = weight_sawitzki/((height_sawitzki/100)*(height_sawitzki/100))
+bmi_sawitzki = weight_sawitzki geteilt durch  ((height_sawitzki/100) * (height_sawitzki/100))
 bmi_foo = weight_foo/((height_foo/100)*(height_foo/100))
 
 print(bmi_sawitzki)
