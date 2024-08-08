1bbaa8f77234e43f88f47902d018085b01ca6d09
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-08-08 10:18:42 +0200

    NameError

--- programme/demo/bmi.py
+++ programme/demo/bmi.py
@@ -1,11 +1,11 @@
 # Dieses Programm bestimmt für zwei Personen deren jeweiligen Body Mass Index
 
-height_sawitzki = 183
+height_sawitzki = hundertdreiundachtzig
 weight_sawitzki = 75.3
 height_foo = 176
 weight_foo = 63.1
 
-bmi_sawitzki = weight_sawitzki geteilt durch  ((height_sawitzki/100) * (height_sawitzki/100))
+bmi_sawitzki = weight_sawitzki /  ((height_sawitzki/100) * (height_sawitzki/100))
 bmi_foo = weight_foo/((height_foo/100)*(height_foo/100))
 
 print(bmi_sawitzki)
