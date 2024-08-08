efa05c9fd85c54068eb9096f01e068d056a05f67
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-08-08 14:14:19 +0200

    Taschenrechner

---
+++ programme/lösungen/calculator.py
@@ -0,0 +1,15 @@
+number1 = input("Bitte geben Sie die erste Zahl an: ")
+number2 = input("Bitte geben Sie die zweite Zahl an: ")
+
+number1 = float(number1)
+number2 = float(number2)
+
+sum = number1 + number2
+print(f'Die Summe aus {number1} und {number2} = {sum}')
+
+diff = number1 - number2
+
+print(f'Die Differenz von {number1} und {number2} = {diff}')
+
+print(f'Das Produkt von {number1} und {number2} = {number1 * number2}')
+print(f'Der Quotient von {number1} und {number2} = {number1 / number2}')
