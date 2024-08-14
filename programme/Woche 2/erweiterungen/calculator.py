2b726419e916f70bd68d95046659426bec128f6e
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-14 15:14:21 +0200

    Fehlerbehandlung im Calculator

--- programme/Woche 2/erweiterungen/calculator.py
+++ programme/Woche 2/erweiterungen/calculator.py
@@ -5,24 +5,30 @@ OPERATION_MESSAGE = "Bitte geben Sie die gewünschte Operation an (+, -, *, /): 
 
 
 while True:
-    number1 = input(FIRST_NUMBER_MESSAGE)
-    number2 = input(SECOND_NUMBER_MESSAGE)
-    operation = input(OPERATION_MESSAGE)
-    number1 = float(number1)
-    number2 = float(number2)
+    try:
+        number1 = input(FIRST_NUMBER_MESSAGE)
+        number1 = float(number1)
+        try:
+            number2 = input(SECOND_NUMBER_MESSAGE)
+            number2 = float(number2)
+            operation = input(OPERATION_MESSAGE)
 
-    if operation == '+':
-        sum = number1 + number2
-        print(f'Die Summe aus {number1} und {number2} = {sum}')
-    elif operation == '-':
-        diff = number1 - number2
-        print(f'Die Differenz von {number1} und {number2} = {diff}')
-    elif operation == '*':
-        print(f'Das Produkt von {number1} und {number2} = {number1 * number2}')
-    elif operation == '/':
-        print(f'Der Quotient von {number1} und {number2} = {number1 / number2}')
-    else:
-        print(f"ungültige Operation: {operation}")
+            if operation == '+':
+                sum = number1 + number2
+                print(f'Die Summe aus {number1} und {number2} = {sum}')
+            elif operation == '-':
+                diff = number1 - number2
+                print(f'Die Differenz von {number1} und {number2} = {diff}')
+            elif operation == '*':
+                print(f'Das Produkt von {number1} und {number2} = {number1 * number2}')
+            elif operation == '/':
+                print(f'Der Quotient von {number1} und {number2} = {number1 / number2}')
+            else:
+                print(f"ungültige Operation: {operation}")
+        except:
+            print(f'Fehlerhafte Eingabe: {number2}')
+    except:
+        print(f'Fehlerhafte Eingabe: {number1}')
     new_calculation = input(NEW_CALCULATION_INPUT_MESSAGE)
     if new_calculation == 'n':
         break    
