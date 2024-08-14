46ec92e72e0ddb77f8d2228ba4b6e174215a84d4
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-14 09:40:43 +0200

    einführung von sprechenden Variablen

--- programme/Woche 2/lösungen/alter_kategorisierung.py
+++ programme/Woche 2/lösungen/alter_kategorisierung.py
@@ -1,8 +1,11 @@
+YOUTH_LIMIT = 18
+ADULT_LIMIT = 65
+
 age = input("Geben Sie bitte ihr Alter als ganze Zahl ein: ")
 age = int(age)
-if age < 18:
-    print("jugendlich")
-elif age >= 18 and age <= 65:
+if age < YOUTH_LIMIT:
+    print(f"jugendlich, weil das eingegebenen Alter kleiner als {YOUTH_LIMIT} ist")
+elif age >= YOUTH_LIMIT and age <= ADULT_LIMIT:
     print("erwachsen")
 else:
     print ("Rentenalter")    
