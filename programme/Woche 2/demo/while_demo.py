e8b7915b55ea869e8f7e0ee9e1e1918f125809a1
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-14 10:30:41 +0200

    Musterlösung Aufgabe 2

--- programme/Woche 2/demo/while_demo.py
+++ programme/Woche 2/demo/while_demo.py
@@ -1,8 +1,4 @@
 counter = 0
-constraint  = counter < 10
-while constraint:
+while counter < 10:
     counter += 1
-    constraint  = counter < 10
-    print(f'counter: {counter}')
-
-print("Finish")
+    print(counter)
