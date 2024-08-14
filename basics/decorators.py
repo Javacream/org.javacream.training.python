f8c333323b1730f224889b88ba3d55bfb0bda7b7
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-14 07:30:09 +0200

    finish trace decorator

--- basics/decorators.py
+++ basics/decorators.py
@@ -10,21 +10,29 @@ say_goodbye()
 
 # ToDo: Sowohl say_hello als auch say_goodbye mögen bitte zwei zusätzliche Ausgaben enthalten, 'entering', 'exiting'
 
-def trace(fn):
-    def decorator():
-        print("entering")
-        fn()
-        print("exiting")
-    return decorator
-
-say_hello = trace(say_hello)    
-say_goodbye = trace(say_goodbye)
+def trace(detail):
+    def inner(fn):
+        def decorator(*args, **kwargs):
+            if detail:
+                print(f"entering using {args} {kwargs}")
+            else:
+                print(f"entering")    
+            result = fn(*args, **kwargs)
+            if detail:
+                print(f"exiting retrieving {result}")
+            else:
+                print(f"exiting")    
+            return result
+        return decorator
+    return inner
+#say_hello = trace(say_hello, False)    
+#say_goodbye = trace(say_goodbye, True)
 
 say_hello()
 say_goodbye()
 
-@trace # in anderen Sprachen wird soetwas als eine Annotation bezeichnet
-def should_be_decorated():
-    print('HUGO')
+@trace(detail=True) # in anderen Sprachen wird soetwas als eine Annotation bezeichnet
+def should_be_decorated(mult):
+    return mult * 'HUGO'
 
-should_be_decorated()
+print (should_be_decorated(3))
