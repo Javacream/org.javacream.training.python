3cdb4f83c72ed237f2dcdc015421234aa166fd29
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-14 11:01:38 +0200

    beispiel für continue

--- programme/Woche 2/lösungen/zähler-ausgabe.py
+++ programme/Woche 2/lösungen/zähler-ausgabe.py
@@ -3,8 +3,8 @@ END = 10
 STEP = 1
 
 counter = START
-while True:
-    if counter == END:
-        break
+while counter < END:
     counter += STEP
+    if counter == 3:
+        continue
     print(counter)
