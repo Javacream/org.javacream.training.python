5980eaddc04ff011441ce2e4549f13ed413dac5e
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-14 13:47:35 +0200

    Teilnehmer-Idee: Ausgabe von Vor- und Nachname in einer Zeile

--- programme/Woche 2/erweiterungen/people.py
+++ programme/Woche 2/erweiterungen/people.py
@@ -3,6 +3,12 @@ people = [['Sawitzki', 'Rainer'], ['Muster', 'Hannah'], ['Schneider', 'Andrea']]
 index = 1
 for person in people:
     print(f'{index}. Person:')
+    innerindex = 1
     for name in person:
-        print(f'    {name}')
+        if innerindex == 1:
+            lastname = name
+        else:
+            firstname = name
+        innerindex += 1        
+    print(f'    {firstname} {lastname}')
     index += 1
