5a5fb6a6540a1b203c6e1510c2682eabc177bdbe
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-14 14:27:17 +0200

    Muster zu der Teilbar-Aufgabe, Variante 1

---
+++ programme/Woche 2/lösungen/zahlen_analyse.py
@@ -0,0 +1,29 @@
+START = 1
+END = 30
+STEP = 1
+CHECK_DIVIDABLE_BY_3 = 3
+MESSAGE_DIVIDABLE_BY_3 = 'Fizz'
+CHECK_DIVIDABLE_BY_5 = 5
+MESSAGE_DIVIDABLE_BY_5 = 'Buzz'
+MESSAGE_DIVIDABLE_BY_3_and_5 = 'FizzBuzz'
+
+counter = START
+while counter <= END:
+    
+    dividable_by_3 = counter % CHECK_DIVIDABLE_BY_3 == 0
+    dividable_by_5 = counter % CHECK_DIVIDABLE_BY_5 == 0
+    dividable_by_3_and_5 = dividable_by_3 and dividable_by_5
+
+    checked = False
+    if dividable_by_3:
+        print(MESSAGE_DIVIDABLE_BY_3)
+        checked = True
+    if dividable_by_5:
+        print(MESSAGE_DIVIDABLE_BY_5)
+        checked = True
+    if dividable_by_3_and_5:
+        print(MESSAGE_DIVIDABLE_BY_3_and_5)
+        checked = True
+    if not checked:
+        print(counter)
+    counter += STEP
