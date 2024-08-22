a2e6cd90176938ff3a054f5bf07e7c692961bca9
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-22 09:48:37 +0200

    starte mit Collections

---
+++ programme/Woche 3/demo/collections_demo.py
@@ -0,0 +1,6 @@
+names_list = ['Hugo', 'Emil', 'Fritz', 'Hugo', 'Hannah'] # eine Liste mit den angegebenen Elementen
+names_tuple = ('Hugo', 'Emil', 'Fritz', 'Hugo', 'Hannah') # ein Tupel mit den angegebenen Elementen
+names_set = {'Hugo', 'Emil', 'Fritz', 'Hugo', 'Hannah'} # ein Set mit den angegebenen Elementen
+name_address_dict = {'Hugo': 'München', 'Emil': 'Berlin', 'Fritz': 'München', 'Hugo': 'Berlin', 'Hannah': 'Düsseldorf'} # ein Dictionary mit den angegebenen Key-Value-Paaren
+print ('Done')
+
