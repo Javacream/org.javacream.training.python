7f8814d707b8cec11ac04538febc77aadd3c3b75
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-22 11:44:46 +0200

    Musterlösung für reverted list

--- programme/Woche 3/lösungen/reverted_list.py
+++ programme/Woche 3/lösungen/reverted_list.py
@@ -1,18 +1,5 @@
 names_list = ['Hugo', 'Emil', 'Fritz', 'Hugo', 'Hannah']
-
+names_list.reverse()
 for name in names_list:
     print(name)
 
-len_names_list = len(names_list)
-
-counter = len_names_list - 1
-
-while counter >= 0:
-    print(names_list[counter])
-    counter -= 1
-
-counter = -1
-
-while counter > -len_names_list:
-    print(names_list[counter])
-    counter -= 1
