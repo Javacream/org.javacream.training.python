095d6c5dfc16c0b0d0b37352831d19605e3124f5
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-22 10:26:56 +0200

    buildin Funktionen zur Collections-Erstellung

--- programme/Woche 3/demo/collections_demo.py
+++ programme/Woche 3/demo/collections_demo.py
@@ -1,7 +1,7 @@
-names_list = ['Hugo', 'Emil', 'Fritz', 'Hugo', 'Hannah'] # eine Liste mit den angegebenen Elementen
-names_tuple = ('Hugo', 'Emil', 'Fritz', 'Hugo', 'Hannah') # ein Tupel mit den angegebenen Elementen
-names_set = {'Hugo', 'Emil', 'Fritz', 'Hugo', 'Hannah'} # ein Set mit den angegebenen Elementen
-name_address_dict = {'Hugo': 'München', 'Emil': 'Berlin', 'Fritz': 'München', 'Hugo': 'Berlin', 'Hannah': 'Düsseldorf'} # ein Dictionary mit den angegebenen Key-Value-Paaren
+names_list = list(['Hugo', 'Emil', 'Fritz', 'Hugo', 'Hannah']) # eine Liste mit den angegebenen Elementen
+names_tuple = tuple(['Hugo', 'Emil', 'Fritz', 'Hugo', 'Hannah']) # ein Tupel mit den angegebenen Elementen
+names_set = set(['Hugo', 'Emil', 'Fritz', 'Hugo', 'Hannah']) # ein Set mit den angegebenen Elementen
+name_address_dict = dict(Emil= 'Berlin', Fritz= 'München', Hugo= 'Berlin', Hannah= 'Düsseldorf') # ein Dictionary mit den angegebenen Key-Value-Paaren
 
 
 second_name_in_list = names_list[1]
