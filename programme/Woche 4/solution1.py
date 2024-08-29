870a1ca62bf30e1ddce37366d73f9afaf969770e
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-29 10:01:26 +0200

    solution 1 mit varargs

--- programme/Woche 4/solution1.py
+++ programme/Woche 4/solution1.py
@@ -1,9 +1,9 @@
-def sum_of_numbers(numbers):
+def sum_of_numbers(*numbers): # * macht imlizit aus der Parameterliste im Aufruf eine Python-Liste
     result = sum(numbers)
     return result
 
-numbers_list = [2,3,4,5,6]
-result_sum_of_numbers = sum_of_numbers(numbers_list) 
-# implizit: numbers = numbers_list
-# implizit, falls ein return angegeben ist: result_sum_of_numbers = result
+# Lösung mit varargs
+result_sum_of_numbers = sum_of_numbers(2,3,4,5,6) 
 print(result_sum_of_numbers)
+
+print("Hello", "World")
