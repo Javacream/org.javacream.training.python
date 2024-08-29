768fb65e2d8df0b3345c119d49c194fcf6ee1654
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-29 12:12:08 +0200

    kein Überladen von Funktionen in Pyrthon

--- programme/Woche 4/scratch_functions.py
+++ programme/Woche 4/scratch_functions.py
@@ -1,17 +1,15 @@
-def echo(message):
-    return message
+def echo(message:str, prefix):
+    if prefix:
+        return "Echoed " + message
+    else:
+        return message
 
 def echo(message):
     return f"echoing {message}"
 
-# echo = "Hugo"
-def application():
-    message = "Hello"
-    message = "World"
-    echoed = echo("Hello") 
-    print(echoed)
 
-    list = ["A", "B"]
-    # ...
-    names = list()
+def application():
+    echoed = echo("Hello", False) 
+    echo("Hello")
+    print(echoed)
 application()
