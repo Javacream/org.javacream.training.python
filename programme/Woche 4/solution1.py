b45f6d3dd3041dcce56bf49b52e4c7108e367251
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-29 09:31:23 +0200

    step 1

---
+++ programme/Woche 4/solution1.py
@@ -0,0 +1,7 @@
+numbers_list = [2,3,4,5,6]
+def sum_of_numbers(numbers):
+    result = sum(numbers)
+    print(result)
+
+sum_of_numbers(numbers_list) # implizit: numbers = numbers_list
+sum_of_numbers([2,3,4,5,6])
