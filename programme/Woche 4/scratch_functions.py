cea3da70ac234c3ba7682dd76062996c1be77188
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-08-29 13:02:17 +0200

    function alias

--- programme/Woche 4/scratch_functions.py
+++ programme/Woche 4/scratch_functions.py
@@ -1,15 +1,9 @@
-def echo(message:str, prefix):
-    if prefix:
-        return "Echoed " + message
-    else:
-        return message
-
 def echo(message):
     return f"echoing {message}"
 
 
 def application():
-    echoed = echo("Hello", False) 
-    echo("Hello")
+    x = echo
+    echoed = x("Hello") 
     print(echoed)
 application()
