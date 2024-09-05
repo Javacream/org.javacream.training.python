7bc673781f0762c58cea19c306a89637fb80ee3c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-09-05 10:30:06 +0200

    copy

--- programme/Woche 5/referenzen.py
+++ programme/Woche 5/referenzen.py
@@ -4,20 +4,11 @@ def application():
     def fn1(l):
         l.append("C")
         print(l)
-
-    def fn2(l):
-        l = ['A', 'B', 'C', 'D'] # Neuzuweisung eines Wertes an einen Parameter ist Bad practice
-        print(l)
-
-    def fn3(l):
-        result = ['A', 'B', 'C', 'D']
-        return result
-
-    fn1(list1) # implizit: l = list1
+    
+    fn1(list1)
     print(list1)
-    fn2(list1)
+    fn1(list1.copy())
     print(list1)
-    list1 = fn3(list1)
-    print(list1)
+
 
 application()
