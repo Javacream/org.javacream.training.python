0eb0317afaddb605afaf38f15c7790bc30e70bc7
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-09-05 10:24:08 +0200

    referenzen und return

--- programme/Woche 5/referenzen.py
+++ programme/Woche 5/referenzen.py
@@ -9,9 +9,15 @@ def application():
         l = ['A', 'B', 'C', 'D'] # Neuzuweisung eines Wertes an einen Parameter ist Bad practice
         print(l)
 
+    def fn3(l):
+        result = ['A', 'B', 'C', 'D']
+        return result
 
     fn1(list1) # implizit: l = list1
     print(list1)
     fn2(list1)
     print(list1)
+    list1 = fn3(list1)
+    print(list1)
+
 application()
