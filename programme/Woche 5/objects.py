49f0899550bb15b0ba68c5108fafdc61190d8454
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-09-05 11:37:18 +0200

    einführung der Klasse Person

--- programme/Woche 5/objects.py
+++ programme/Woche 5/objects.py
@@ -1,5 +1,23 @@
+# Workaround für 'Objekte sind Unikate'
+class Object:
+    pass
+
 def application():
-    object1 = object()
-    print(object1)
-    print(type(object1))
+    sawitzki = Object()
+    print(sawitzki)
+    print(type(sawitzki))
+
+    sawitzki.lastname = "Sawitzki"
+    sawitzki.firstname = "Rainer"
+    sawitzki.height = 183
+
+    musterfrau = Object()
+    musterfrau.name = "Hannah Musterfrau"
+    musterfrau.weight = 61
+
+    person3 = Object()
+    person3.lastname = "Meuer"
+    person3.firstnames = ['Hugo', 'Emil'] 
+    print(person3)
+
 application()
---
+++ programme/Woche 5/people.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, lastname, firstname, height):
+        self.lastname = lastname
+        self.firstname = firstname
+        self.height = height
+    def say_hello(self):
+        pass    
