8a5eb4602a9651aab345cafb03033fbe99f81a31
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-09-05 09:52:03 +0200

    ToDo Slide 56

--- programme/Woche 5/referenzen.py
+++ programme/Woche 5/referenzen.py
@@ -5,6 +5,13 @@ def application():
         l.append("C")
         print(l)
 
+    def fn2(l):
+        l = ['A', 'B', 'C', 'D']
+        print(l)
+
+
     fn1(list1) # implizit: l = list1
+    print(list1)
+    fn2(list1)
     print(list1)
 application()
