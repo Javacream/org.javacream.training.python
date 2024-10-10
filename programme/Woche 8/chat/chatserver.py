ff3ead989cf8bf903d1757bbc09b8248208b9706
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-10-10 15:27:38 +0200

    Abschluss Woche 8

--- programme/Woche 8/chat/chatserver.py
+++ programme/Woche 8/chat/chatserver.py
@@ -1,6 +1,7 @@
 import socket
 import multiprocessing as mp
-
+import pickle
+import os.path
 def main():
     # HOST = 'localhost' # Dieser Host nimmt nur Anfragen von localhost entgegen, er bindet nur auf localhost
     # HOST = '192.168.178.76' # Der Hoszt nimmt so alle Anfragen entgegen, die an diese IP-Adresse gerichtet sind
@@ -13,19 +14,31 @@ def main():
         while True:
             client_socket, address = server_socket.accept() # Blockierendes Warten auf einen Client-Request
             print(f'Accepted client connection: {client_socket}')
-            process = mp.Process(target=handle_client, args=(client_socket,))
+            process = mp.Process(target=handle_client, args=(client_socket, ))
             process.start()
 
 def handle_client(client_socket):
         with client_socket:
             name = client_message = client_socket.recv(1024).decode()
-            client_socket.sendall(f'Hello {name}\n'.encode())
+            if os.path.isfile(name):
+                with open(name, 'rb') as file:
+                    messages = pickle.load(file)
+            else:
+                messages = []
+            if len(messages) > 0:
+                last_message = messages[-1]
+            else:
+                last_message = "not available"        
+            client_socket.sendall(f'Hello {name}, you have {len(messages)} chat entries, last message is {last_message}\n'.encode())
             while True:
                 client_message = client_socket.recv(1024).decode()
                 print(f'Received message: {client_message}')
                 if client_message == 'bye':
                     print(f'terminating client connection {client_socket}')
                     break
+                messages.append(client_message)
+                with open (name, 'wb') as file:
+                    pickle.dump(messages, file)
                 client_socket.sendall(f'{name} deine Nachricht war {client_message}\n'.encode())
 
 if __name__ == '__main__':
