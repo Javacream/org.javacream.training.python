e2cf89871db370d886ab4012a5985bde09cae399
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-10-11 15:27:05 +0200

    restful beispiel

---
+++ programme/Woche 8/databases/rest-sample.py
@@ -0,0 +1,21 @@
+import requests
+
+# URL des RESTful WebService
+url = "http://javacream.eu:8080/api/books/ISBN1"
+
+# GET-Anfrage senden
+response = requests.get(url)
+
+# Statuscode überprüfen, um sicherzustellen, dass die Anfrage erfolgreich war
+if response.status_code == 200:
+    # Daten im JSON-Format extrahieren
+    data = response.json()
+    
+    # Ausgabe der Daten
+    print("Daten erfolgreich empfangen:")
+    for element in data:
+        print(f"{element}")
+else:
+    print(f"Fehler bei der Anfrage: {response.status_code}")
+
+response = requests.delete(url)
