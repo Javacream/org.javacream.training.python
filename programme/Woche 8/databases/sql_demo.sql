15e5cd4a0ce69e8b3bb4470bb41915be7e7e3a0c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-10-11 11:02:59 +0200

    INSERT INTO

---
+++ programme/Woche 8/databases/sql_demo.sql
@@ -0,0 +1,4 @@
+INSERT INTO PEOPLE (id, lastname, firstname) VALUES(1, 'Sawitzki', 'Rainer')
+INSERT INTO ADDRESSES (id, city, street) VALUES (1, 'München', 'Marienplatz')
+INSERT INTO COMPANIES (id, company_name) VALUES (1, 'Javacream')
+INSERT INTO EMPLOYEES (person_id, company_id) VALUES (1, 1)
