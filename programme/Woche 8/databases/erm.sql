b3f3644a58fb8a33fcd537b895bcf46bfa40852a
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-10-11 10:08:38 +0200

    sql create statements

--- programme/Woche 8/databases/erm.sql
+++ programme/Woche 8/databases/erm.sql
@@ -1,3 +1,5 @@
-CREATE TABLE ADDRESSES (id number primary key, city varchar(32), street varchar(128))
-CREATE TABLE PEOPLE (id number, lastname varchar(32), firstname varchar(32), address_id number, PRIMARY KEY(id), FOREIGN KEY (address_id) REFERENCES (ADDRESSES(id)))
+CREATE TABLE ADDRESSES (id INTEGER PRIMARY KEY, city VARCHAR(32), street VARCHAR(128))
+CREATE TABLE PEOPLE (id INTEGER, lastname VARCHAR(32), firstname VARCHAR(32), address_id INTEGER, PRIMARY KEY(id), FOREIGN KEY (address_id) REFERENCES ADDRESSES(id))
+CREATE TABLE COMPANIES (id INTEGER, company_name VARCHAR(32), PRIMARY KEY(id))
+CREATE TABLE EMPLOYEES (person_id INTEGER, company_id INTEGER, PRIMARY KEY (person_id, company_id), FOREIGN KEY (person_id) REFERENCES PEOPLE(id), FOREIGN KEY (company_id) REFERENCES COMPANIES(id))
 
