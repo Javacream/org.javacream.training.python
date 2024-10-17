7aa80f914bf6bf0004057f59b842704dbe3e4311
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-10-17 10:24:12 +0200

    erstes Kommando absetzen

--- programme/Woche 9/simple_ssh.py
+++ programme/Woche 9/simple_ssh.py
@@ -7,8 +7,12 @@ client = paramiko.client.SSHClient()
 HOST = 'javacream.eu'
 PORT = 22
 USER = 'teilnehmer'
-PASSWORD = 'javacream13!'
+PASSWORD = 'javacream123!'
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(HOST, port=PORT, username=USER, password=PASSWORD) # Nach Ausführung von Connect wird auf javacream.eu ein Shell-Prozess gestartet
 
+remote_process = client.exec_command('ls') # r_stdin, r_stdout, r_stderr = client.exec
+remote_stdout = remote_process[1]
+result = remote_stdout.read().decode()
+print(result)
 client.close()
