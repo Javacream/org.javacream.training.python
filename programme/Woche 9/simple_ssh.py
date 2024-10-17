5e7bc5983be22ec9fb858f0335b641218f297b26
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-10-17 10:27:19 +0200

    refactoring

--- programme/Woche 9/simple_ssh.py
+++ programme/Woche 9/simple_ssh.py
@@ -1,18 +1,25 @@
 import paramiko
 import paramiko.client
 
+def ssh_connect():
+    client = paramiko.client.SSHClient()
+    HOST = 'javacream.eu'
+    PORT = 22
+    USER = 'teilnehmer'
+    PASSWORD = 'javacream123!'
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(HOST, port=PORT, username=USER, password=PASSWORD) # Nach Ausführung von Connect wird auf javacream.eu ein Shell-Prozess gestartet
+    return client
 
-client = paramiko.client.SSHClient()
+def ssh_execute_command(client, cmd):
+    remote_process = client.exec_command(cmd) # r_stdin, r_stdout, r_stderr = client.exec
+    remote_stdout = remote_process[1]
+    result = remote_stdout.read().decode()
+    return result
 
-HOST = 'javacream.eu'
-PORT = 22
-USER = 'teilnehmer'
-PASSWORD = 'javacream123!'
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(HOST, port=PORT, username=USER, password=PASSWORD) # Nach Ausführung von Connect wird auf javacream.eu ein Shell-Prozess gestartet
+def main():
+    client = ssh_connect()
+    result = ssh_execute_command(client, 'ls')
+    print(result)
 
-remote_process = client.exec_command('ls') # r_stdin, r_stdout, r_stderr = client.exec
-remote_stdout = remote_process[1]
-result = remote_stdout.read().decode()
-print(result)
-client.close()
+main()
