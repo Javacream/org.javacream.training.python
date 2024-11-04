c97a486fb194a27d4087b5126592d81ec0c811b7
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-04 11:42:57 +0100

    einführen von Input

--- programme/block1/ein_ausgabe.py
+++ programme/block1/ein_ausgabe.py
@@ -1,4 +1,4 @@
 # Dieses Programm gibt eine Benutzer-Eingabe auf die Konsole aus
 
-out_message = "Ausgabe" # Zuweisung mit = an eine Variable,  Konvention: englisch, Kleinbuchstaben, Betonung mit Unterstrich
+out_message = input() # Zuweisung mit = an eine Variable,  Konvention: englisch, Kleinbuchstaben, Betonung mit Unterstrich
 print(out_message)
