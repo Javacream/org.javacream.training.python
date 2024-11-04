20b6cf29d49e711de2a0af84ab3fc8de01e48ada
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-04 11:31:30 +0100

    spielen mit fehlern

---
+++ programme/block1/ein_ausgabe.py
@@ -0,0 +1,12 @@
+# Kommentar
+# Dieses Programm gibt eine Benutzer-Eingabe auf die Konsole aus
+
+print("Ausgabe") # Das ist der Aufruf der print-Funktion, in runden Klammern der Wert, der ausgegeben werden soll
+
+# Spielen mit falscher Syntax
+# Syntax ist pingelig und in keinster Form tolerant
+
+# print["Ausgabe"] # runde statt eckige -> Fehlerausgabe des Interpreters Type Error?
+# gebeAus("Ausgabe") NameError
+# print("Ausgabe" Syntax Error
+# print("Ausgabe) Synxtax Error
