54196b41c522b942404ce84291d996540bb67523
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-04 14:26:15 +0100

    operatoren

---
+++ programme/block1/operatoren.py
@@ -0,0 +1,31 @@
+# Operatoren sind Fumnktionen / Berechnungen, die mit einem Zeichen zwei Variablen / Literale kombinieren
+
+number1 = 20
+number2 = 22
+
+result = number1 + number2
+result = number1 - number2
+result = number1 * number2
+result = number1 / number2
+result = number1 % 3 # Restzahldivision
+print(result)
+
+# es gibt noch weitere Operatoren, z.B. exponieren ^, -> Dokumentation
+
+
+name1 = "Hugo"
+name2 = "Emil"
+
+result = name1 + name2
+#result = name1 - name2
+#result = name1 * name2
+#result = name1 / name2
+#result = name1 % 3 # Restzahldivision
+print(result)
+
+# result = number1 + name2
+# result = number1 - name2
+result = number1 * name2
+# result = number1 / name2
+# result = number1 % 3 # Restzahldivision
+print(result)
