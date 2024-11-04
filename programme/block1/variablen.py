851219266af2b28ad79270b26be22ea39c6116a4
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-04 13:39:33 +0100

    Details zur Variablen-Deklaration

---
+++ programme/block1/variablen.py
@@ -0,0 +1,18 @@
+# Deklataion und Umgang mit Variablen
+
+# Korrekt, Name der Variable = Wert
+name = "Sawitzki"
+
+# Korrekt: Neuzuweisung
+name = "Rainer Sawitzki"
+
+# Korrekt: Nutzen einer Variablen für eine neue Zuweisung
+trainer_name = name
+
+# Fehler: Nutzen einer Varible, bevor ihr ein Wert zugewiesen wurde
+# print(not_assigned)
+# not_assigned = "Hugo"
+
+# Fehler: Pure Deklaration mit anschließender Wertzuweisung
+# just_declared
+# just_declared = "Hugo"
