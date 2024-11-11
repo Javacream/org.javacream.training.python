29e71f3b7c339530baf7cc2ac883ff95c67fdb6a
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-11 13:54:26 +0100

    Musterlösung Folie 28

---
+++ programme/block2/alters_kategorisierung_erweitert.py
@@ -0,0 +1,22 @@
+while True:
+    try:
+        age = input('Bitte Alter eingeben: ')
+        age = int(age)
+        if age < 0:
+            print(f'negatives Alter {age} ist nicht zulässig')
+        elif age > 150:
+             print(f'so ein Alter ist nicht erreichbar: {age}')
+        else:     
+            if age < 18:
+                print(f'{age } ist ein jugendliches Alter')
+            elif age < 65:
+                print(f'{age} ist ein erwachsenes Alter')
+            else:
+                print(f'{age} ist Rentenalter')    
+
+    except Exception as e:
+        print ('*** Fehler bei der Alterskategorisierung: {e}')
+    
+    again = input("Zum Beenden des Programms 'n' eingeben: ")
+    if again == 'n':
+        break
