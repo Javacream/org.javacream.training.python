266b1f904ae1b541477edc0b27f35180440e8b52
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-11 13:17:31 +0100

    Fehlerbehandlung, aktueller Stand

--- programme/demos/block2/scratch.py
+++ programme/demos/block2/scratch.py
@@ -1,29 +1,3 @@
-counter = 0
-condition = counter < 10
-#while condition: # Das ist eine Endlosschleife
-#    print(counter)
-
-#while condition: # Das ist immer noch eine eine Endlosschleife
-#    condition = counter < 10
-#    print(counter)
-
-
-while condition:
-    condition = counter < 10
-    print(counter)
-    counter = counter + 1
-
-while counter < 10:
-    print(counter)
-#    counter = counter + 1
-    counter +=1 # Eine häufig genutzte Verkürzung
-    #counter++ # Ein Inkrement um 1, unterstützt in vielen Programmiersprache    
-
-
-start = 42
-end_exclusive = 47
-index = start
-while index < end_exclusive:
-    print(index)
-    index += 1
-
+user_input = input('Bitte eine Zahl eingeben: ')
+number = int(user_input) # Hier wird der ValueError "geworfen" 
+print(2 * number) # dies wird nicht mehr ausgeführt, der geworfene Fehler verhindert die Ausführung der nächsten Zeile
