d8840696327c953abcb8e588ad9a46275936c3b6
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-11 13:26:20 +0100

    Einführung von try-except

--- programme/demos/block2/scratch.py
+++ programme/demos/block2/scratch.py
@@ -1,9 +1,9 @@
 user_input = 'Hugo'
 
-# Wie kann denn die Umwandlungsfunktion 'int' einen Fehler signalisieren?
-# Wir tun so, als hätten wir in der Dokumentation gefunden "Wenn Paramter nicht umgewandelt werden kann wird die Zahl -666 zurückgegeben"
-number = int(user_input) # Hier wird der ValueError "geworfen" 
-if number != -666:
-    print(2 * number)
-else:
-    print(f'{number} kann nicht in eine Zahl konvertiert werden')
+try:
+  number = int(user_input)
+  print(2 * number)
+except:
+    print(f'{user_input} kann nicht in eine Zahl konvertiert werden')
+
+print('nächste Programmanweisung')    
