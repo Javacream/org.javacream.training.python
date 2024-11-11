86c78c5f955b506efdeb312a553dcdf0cdc9a19a
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-11 12:15:15 +0100

    kurzeinführung listen

---
+++ programme/demos/block2/kurzeinführung_liste.py
@@ -0,0 +1,5 @@
+# list = ... Bitte so nicht, eine Variable 'list' nennen 
+names = ['Hugo', 'Emil', 'Hannah', 'Andrea'] # [...] das Listen-Literal
+
+for name in names: # name ist eine Variable, die für jeden Schleifendurchlauf mit dem nächsten Element der Liste belegt wird
+    print(name)
