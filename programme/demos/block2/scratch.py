f6f8582ecdcb2f8fd84a467c578b4eb7e8779c2d
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-11 13:35:53 +0100

    spielen mit try-except

--- programme/demos/block2/scratch.py
+++ programme/demos/block2/scratch.py
@@ -2,8 +2,10 @@ user_input = 'Hugo'
 
 try:
   number = int(user_input)
+  # e r r o r SyntxErrors können nie behandelt werden
   print(2 * number)
-except:
-    print(f'{user_input} kann nicht in eine Zahl konvertiert werden')
+except Exception as e: # Dieses "Exception as e" bitte akzeptieren
+    print(f'*** Ein Fehler ist aufgetreten: {e}')
+    # print(f'{user_input} kann nicht in eine Zahl konvertiert werden')
 
 print('nächste Programmanweisung')    
