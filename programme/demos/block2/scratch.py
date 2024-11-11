09d646c199dd58853481af2d2d9e09cef4c76afa
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-11 10:14:37 +0100

    Einführen der logischen Variablen, Boolesche Algebra, Vergleichsoperatoren

---
+++ programme/demos/block2/scratch.py
@@ -0,0 +1,28 @@
+if True:
+    print('erfüllt')
+
+while False:
+    pass
+print('weiter geht')
+
+condition = True
+if condition:
+    pass
+else:
+    pass
+
+result = True and False
+result = True and True
+
+result = True or False
+result = True or True
+
+result = not True
+
+a = 3
+b = 5
+result = (a == b)
+result = (a < b)
+
+print('finish')
+
