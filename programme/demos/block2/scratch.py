6eb75bd173732e53bdf910f2cf081cad1895cca4
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-11 13:21:56 +0100

    Fehlerbhandlung mit speziellen Rückgabewerten, so wird es nicht gemacht

--- programme/demos/block2/scratch.py
+++ programme/demos/block2/scratch.py
@@ -1,3 +1,9 @@
-user_input = input('Bitte eine Zahl eingeben: ')
+user_input = 'Hugo'
+
+# Wie kann denn die Umwandlungsfunktion 'int' einen Fehler signalisieren?
+# Wir tun so, als hätten wir in der Dokumentation gefunden "Wenn Paramter nicht umgewandelt werden kann wird die Zahl -666 zurückgegeben"
 number = int(user_input) # Hier wird der ValueError "geworfen" 
-print(2 * number) # dies wird nicht mehr ausgeführt, der geworfene Fehler verhindert die Ausführung der nächsten Zeile
+if number != -666:
+    print(2 * number)
+else:
+    print(f'{number} kann nicht in eine Zahl konvertiert werden')
