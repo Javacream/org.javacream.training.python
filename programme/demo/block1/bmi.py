d9209ed10ec7c96121602881a9747b28d92c47f8
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-12 10:02:38 +0100

    bmi Programm

---
+++ programme/demo/block1/bmi.py
@@ -0,0 +1,10 @@
+# Dieses Programm berechnet den Body Mass Index einer Person mit Gewicht 74.9 und Größe 183cm
+
+name = 'Sawitzki'
+weight = 74.9
+height = 183
+
+bmi = weight/(height*height)*100*100
+
+print(bmi)
+
