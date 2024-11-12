f2cd96d5df172c349b78269b23c15c0af18844bf
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-12 12:07:29 +0100

    Lösung zu Aufgabe 3

--- programme/block1/bmi.py
+++ programme/block1/bmi.py
@@ -1,5 +1,3 @@
-# Dieses Programm berechnet den Body Mass Index einer Person mit Gewicht 74.9 und Größe 183cm
-
 name = input('Bitte Name eingeben: ')
 weight = input('Bitte Körpergewicht eingeben: ')
 height = input('Bitte Körpergröße angeben: ')
@@ -9,6 +7,4 @@ height = int(height)
 
 bmi = weight/(height*height)*100*100
 
-# print("Die Person namens " + name + " hat einen BMI von " + bmi)
-# print("Die Person namens " + name + " hat einen BMI von " + str(bmi))
 print(f"Die Person namens {name} hat einen BMI von {bmi}")
