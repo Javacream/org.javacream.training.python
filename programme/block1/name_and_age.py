e16336e96ed41ba7ed883ba826562d1e260be2f3
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-12 11:16:39 +0100

    Lösung 2 mit formatted String

--- programme/block1/name_and_age.py
+++ programme/block1/name_and_age.py
@@ -3,4 +3,4 @@
 name = input('Bitte Name eingeben: ')
 age = input('Bitte Alter eingeben: ')
 
-print('Die Person ' + name + ' ist ' + age + ' Jahre alt')
+print(f'Die Person {name} ist {age} Jahre alt')
