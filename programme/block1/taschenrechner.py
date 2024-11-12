42b0a4a3a8ae4da7d40510e8f03c8274a05deb61
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-12 14:31:51 +0100

    Taschenrechner, Aufgabe 4

---
+++ programme/block1/taschenrechner.py
@@ -0,0 +1,16 @@
+number1 = input('Bitte erste Zahl eingeben: ')
+number2 = input('Bitte zweite Zahl eingeben: ')
+
+number1 = float(number1)
+number2 = float(number2)
+
+addition = number1 + number2
+print ('Die Summe der beiden Zahlen ist ' + str(addition))
+
+print ('Die Differenz der beiden Zahlen ist ' + str(number1 - number2))
+
+product = number1 * number2
+
+print (f'Das Produkt der beiden Zahlen {number1} und {number2} ist {product}')
+
+print (f'Der Quotient der beiden Zahlen {number1} und {number2} ist {number1/number2}')
