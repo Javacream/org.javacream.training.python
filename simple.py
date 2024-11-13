50b07bc53ec11b657dd5dd9ef3ea132854857148
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-13 11:09:59 +0100

    calculate bmi

--- simple.py
+++ simple.py
@@ -1,9 +1,9 @@
-# Dieses Programm ist als reine Analogie zu Tabellenkalkulation aufzufassen
+# Dieses Programm berechnet einen BMI für die Person Meier mit Körpergewicht 74.1 und Körpergröße 183cm
 
-A1 = 'Meier' # A1 enthält den Namen der Person
-B1 = 74.1 # Gewicht'
-C1 = 183 # Größe in cm
+name = 'Meier'
+weight = 74.1
+height = 183 
 
-D1 = B1 / (C1 * C1) * 100 * 100 # Berechnung des BMI
+D1 = weight / (height * height) * 100 * 100 # Berechnung des BMI
 
-print (f'Die Person namens {A1} hat mit einem Gewicht von {B1} und einer Körpergröße von {C1}cm einen BMI von {D1}')
+print (f'Die Person namens {name} hat mit einem Gewicht von {weight} und einer Körpergröße von {height}cm einen BMI von {D1}')
