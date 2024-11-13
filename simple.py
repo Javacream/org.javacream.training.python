fb07cffa692b94b476b0133b74e92dea982fcfe3
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-13 11:02:39 +0100

    spreadsheets as program

---
+++ simple.py
@@ -0,0 +1,9 @@
+# Dieses Programm ist als reine Analogie zu Tabellenkalkulation aufzufassen
+
+A1 = 'Meier' # A1 enthält den Namen der Person
+B1 = 74.1 # Gewicht'
+C1 = 183 # Größe in cm
+
+D1 = B1 / (C1 * C1) * 100 * 100 # Berechnung des BMI
+
+print (f'Die Person namens {A1} hat mit einem Gewicht von {B1} und einer Körpergröße von {C1}cm einen BMI von {D1}')
