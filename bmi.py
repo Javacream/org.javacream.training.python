6f23dcb146cbb39ff1b4d59749e14f5e913d5daf
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-13 13:43:52 +0100

    ToDo slide 21

--- bmi.py
+++ bmi.py
@@ -1,12 +1,26 @@
-# Dieses Programm berechnet einen BMI
+MIN_HEIGHT = 30
+MAX_HEIGHT = 250
+MIN_WEIGHT = 3
+MAX_WEIGHT = 333
+while True:
 
-name = input('Enter your name: ')
-weight = input('Enter your weight: ')
-height = input('Enter your height in cm: ')
+    name = input('Enter your name: ')
+    weight = input('Enter your weight: ')
+    height = input('Enter your height in cm: ')
+    try:
+        weight = float(weight)
+        height = int(height)
+        if weight < MIN_WEIGHT or weight > MAX_WEIGHT:
+            print (f'you entered an invalid weight {weight}: must be between {MIN_WEIGHT} and {MAX_WEIGHT}')
+        elif height < 30 or weight > 250:
+            print (f'you entered an invalid height {height}: must be between {MIN_HEIGHT} and {MAX_HEIGHT}')
+        else:
+            bmi = weight / (height * height) * 100 * 100 # Berechnung des BMI
+            print (f'{name} with weight {weight} and height {height}cm has a body mass index of {bmi:.2f}')
+    except:
+        print (f'you entered an invalid weight {weight} or height {height}')
+        
 
-weight = float(weight)
-height = int(height)
-
-bmi = weight / (height * height) * 100 * 100 # Berechnung des BMI
-
-print (f'Die Person namens {name} hat mit einem Gewicht von {weight} und einer Körpergröße von {height}cm einen BMI von {bmi}')
+    again = input ('again? (y|n)')
+    if again == 'n':
+        break
