c3ca96cc2b1f67a7865917d7d99718b3db27f821
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-14 09:58:01 +0100

    introduce parameter type hints

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ def fn1():
     height = 183
     print('called fn1')
 
-def fn2(p1, p2, p3):
+def fn2(p1:str, p2, p3:list):
     print(p1, p2, p3)
     p1 = "Fridolin" # Neuzuweisung der Referenz p1
     p3.append('orange') # Nutzen des Objektes, auf das p3 zeigt
