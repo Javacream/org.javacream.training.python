957042f64fd88971482d122a22397bc2068b40c4
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-14 15:30:33 +0100

    dunder for publishing

--- publishing.py
+++ publishing.py
@@ -6,14 +6,22 @@ class Book:
         self.pages = pages
         self.authors = set()
         self.publisher = publisher
-
+    def __repr__(self):
+        return f'Book isbn={self.isbn}, title={self.title}, price={self.price}, pages={self.pages} '
+    def __eq__(self, other):
+        return self.isbn__eq__(other.isbn)
+    def __hash__(self):
+        return self.isbn.__hash__()
 class Publisher:
     def __init__(self, name):
         self.name = name
         self.books = []
-
+    def __repr__(self):
+        return f'Publisher {self.name}'
 class Author:
     def __init__(self, lastname, firstname):
         self.lastname = lastname
         self.firstname = firstname
         self.books = []
+    def __repr__(self):
+        return f'Author {self.firstname} {self.lastname}'
