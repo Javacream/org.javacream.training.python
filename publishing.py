c01d92589f73e86fad2464f9f6dda6019a08fd3a
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-14 14:08:02 +0100

    solution slide 41

--- publishing.py
+++ publishing.py
@@ -1,6 +1,19 @@
 class Book:
-    def __init__(self, isbn, title, price, pages):
+    def __init__(self, isbn, title, price, pages, publisher):
         self.isbn =isbn
         self.title = title
         self.price = price
         self.pages = pages
+        self.authors = set()
+        self.publisher = publisher
+
+class Publisher:
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+
+class Author:
+    def __init__(self, lastname, firstname):
+        self.lastname = lastname
+        self.firstname = firstname
+        self.books = []
--- publishing_app.py
+++ publishing_app.py
@@ -1,7 +1,21 @@
-from publishing import Book
+from publishing import Book, Publisher, Author
 def main():
-    b1 = Book('ISBN1', 'Python in Action', 19.99, 200)
-    b2 = Book('ISBN2', 'Java', 29.99, 666)
+    p = Publisher('Springer')
+    b1 = Book('ISBN1', 'Python in Action', 19.99, 200, p)
+    b2 = Book('ISBN2', 'Java', 29.99, 666, p)
+    a1 = Author('Schreiber', 'Ling')
+    a2 = Author('Lite', 'Rat')
+
+    p.books.append(b1)
+    p.books.append(b2)
+    a1.books.append(b1)
+    b1.authors.add(a1)
+    a1.books.append(b2)
+    b2.authors.add(a1)
+    a2.books.append(b2)
+    b2.authors.add(a2)
+    print('done')
+
     
 if __name__ == '__main__':
     main()
