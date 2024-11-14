40fee263f26628a8522b6454a1df143442a1835d
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-14 09:46:52 +0100

    copy value of reference

--- functions.py
+++ functions.py
@@ -1,13 +1,18 @@
 def fn1():
+    height = 183
     print('called fn1')
 
-def fn2(p1, p2):
-    print(p1, p2)
+def fn2(p1, p2, p3):
+    print(p1, p2, p3)
+    p3.append('orange')
     return 'OK'
 
 def main():
     fn1()
-    result = fn2('Hugo', 42)
+    name = 'Hugo'
+    number = 42
+    items = ['banana', 'apple']
+    result = fn2(name, number, items)
     print(result)
 
 main()
