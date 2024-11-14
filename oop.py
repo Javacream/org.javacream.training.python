44e4a52daebcb72d17c4448b96cea1eedb4e0077
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-14 13:43:00 +0100

    has a

--- oop.py
+++ oop.py
@@ -1,6 +1,11 @@
 class Person:
-    def __init__(self, lastname, firstname): # Konstruktor
+    def __init__(self, lastname, firstname, address): # Konstruktor
         self.lastname = lastname
         self.firstname = firstname
+        self.address = address
     def introduce(self):
         return f'Hello, my name is {self.firstname} {self.lastname}'
+class Address:
+    def __init__(self, city, street):
+        self.city = city
+        self.street = street
--- oop_app.py
+++ oop_app.py
@@ -1,8 +1,10 @@
-from oop import Person
+from oop import Person, Address
 
 def main():
-    p1 = Person('Musterperson', 'Andrea')
-    p2 = Person('Meier', 'Hannah')
+    a1 = Address('München', 'Marienplatz')
+    a2 = Address('Berlin', 'Alexanderplatz')
+    p1 = Person('Musterperson', 'Andrea', a2)
+    p2 = Person('Meier', 'Hannah', a1)
     print(p1.introduce())
     print(p2.introduce())
 
