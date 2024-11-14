feae171801b588240796f27a3022a083f841d075
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-14 09:29:16 +0100

    introduce main

--- functions.py
+++ functions.py
@@ -5,8 +5,9 @@ def fn2(p1, p2):
     print(p1, p2)
     return 'OK'
 
+def main():
+    fn1()
+    result = fn2('Hugo', 42)
+    print(result)
 
-fn1()
-
-result = fn2('Hugo', 42)
-print(result)
+main()
