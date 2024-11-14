835e3cfd3ed0627a4967e701fab3374ac38b76f3
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-14 13:30:58 +0100

    Solution Slide 36

---
+++ publishing.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, isbn, title, price, pages):
+        self.isbn =isbn
+        self.title = title
+        self.price = price
+        self.paes = pages
---
+++ publishing_app.py
@@ -0,0 +1,7 @@
+from publishing import Book
+def main():
+    b1 = Book('ISBN1', 'Python in Action', 19.99, 200)
+    b2 = Book('ISBN2', 'Java', 29.99, 666)
+    
+if __name__ == '__main__':
+    main()
