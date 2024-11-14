c00a10498251cd5e533e6744959ad30f7b31690f
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-14 10:35:52 +0100

    varargs, default args, keyworded args

--- functions.py
+++ functions.py
@@ -1,19 +1,24 @@
 def fn1():
     height = 183
     print('called fn1')
-
-def fn2(p1:str, p2, p3:list):
+def fn2(p1:str, p2, p3:list = ['Default']):
     print(p1, p2, p3)
     p1 = "Fridolin" # Neuzuweisung der Referenz p1
     p3.append('orange') # Nutzen des Objektes, auf das p3 zeigt
     return 'OK'
 
+def fn3(*args):
+    print(args)
+
+def fn4(**kwargs):
+    print(kwargs)
+
 def main():
-    fn1()
     name = 'Hugo'
     number = 42
     items = ['banana', 'apple']
-    result = fn2(name, number, items)
-    print(result)
+    fn4()
+    fn4(name= 'Hugo', number= 9)
+    fn4(name= 'Hugo')
 
 main()
