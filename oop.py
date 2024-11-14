f45a59f02ca1645771fb0f9e0527f14a75c6f990
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-14 13:52:49 +0100

    has a, n

--- oop.py
+++ oop.py
@@ -2,6 +2,7 @@ class Person:
     def __init__(self, lastname, firstname): # Konstruktor
         self.lastname = lastname
         self.firstname = firstname
+        self.addresses = set()
     def introduce(self):
         return f'Hello, my name is {self.firstname} {self.lastname}'
 class Address:
--- oop_app.py
+++ oop_app.py
@@ -5,7 +5,7 @@ def main():
     a2 = Address('Berlin', 'Alexanderplatz')
     p1 = Person('Musterperson', 'Andrea')
     p2 = Person('Meier', 'Hannah')
-    p2.address = a1
+    p2.addresses.add(a1)
     p1.lastname = 'Changed'
     print(p1.introduce())
     print(p2.introduce())
