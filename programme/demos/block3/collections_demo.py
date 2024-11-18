e7e08d6bdf6e4122cf99fa56de0ed80b7c711f37
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-18 09:29:49 +0100

    listen und strings als erste Beispiele für Collections

---
+++ programme/demos/block3/collections_demo.py
@@ -0,0 +1,9 @@
+names = ['Hugo', 'Frieda', 'Andrea', 'Hugo', 'Fritz']
+for name in names:
+    print(name)
+print(names[2])    
+
+text = "Ein Text"
+for character in text:
+    print(character)
+print(text[2])    
