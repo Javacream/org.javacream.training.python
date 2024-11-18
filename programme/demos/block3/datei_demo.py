2f1c29417a71eda9c629259dd6f52680a9e60cec
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-18 11:20:18 +0100

    besser: with open (...) as file

--- programme/demos/block3/datei_demo.py
+++ programme/demos/block3/datei_demo.py
@@ -1,5 +1,4 @@
 # file = open ('./data.txt') # der optionale 2. Parameter ist 'rt', Lesen einer Text-Datei
-file = open ('./data.txt', 'rt')
-rows = file.readlines()
-file.close() # Falls dieser Aufruf nicht gemacht wird, bleibt die Datei bis zum Ende des Python-Prozesses geöffnet und gegegenenfalls gesperrt
+with open ('./data.txt', 'rt') as file:
+    rows = file.readlines()
 print(rows)
