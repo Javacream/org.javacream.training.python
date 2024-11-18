26b66a329eabcff0582cdfdf6a595ad929a48e09
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-18 14:15:18 +0100

    character_count mit dictionary

--- programme/demos/block3/character_count.py
+++ programme/demos/block3/character_count.py
@@ -9,17 +9,13 @@ with open ('./programme/demos/block3/text.txt', 'rt') as file:
 characters_to_count = input ('Bitte die zu zählenden Zeichen kommasepariert eingeben:')
 characters_to_count = characters_to_count.split(',')
 
-result = []
+result = {}
 
 for character in characters_to_count:
-    result.append([character, 0])
+    result[character] = 0
 
-character_count = 0
 for character in text:
     if character in characters_to_count:
-        for character_count_for_character in result:
-            if character == character_count_for_character[0]:
-                character_count_for_character[1] += 1
-            continue    
+        result[character] += 1
 
 print(result)
