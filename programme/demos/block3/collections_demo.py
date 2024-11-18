11c02131c3d5ad28409cd2696873d2bea0457ad7
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-18 10:03:33 +0100

    set und if in

--- programme/demos/block3/collections_demo.py
+++ programme/demos/block3/collections_demo.py
@@ -1,4 +1,4 @@
-names = ['Hugo', 'Frieda', 'Andrea', 'Hugo', 'Fritz']
+""" names = ['Hugo', 'Frieda', 'Andrea', 'Hugo', 'Fritz']
 for name in names:
     print(name)
 print(names[-2])
@@ -11,3 +11,14 @@ text = "Ein Text"
 for character in text:
     print(character)
 print(text[2])    
+ """
+
+names = {'Hugo', 'Frieda', 'Andrea', 'Hugo', 'Fritz'}
+for name in names:
+    print(name)
+
+element = 'Andreas'
+if element in names:
+    print(f'{element} ist in der Names-Collection')
+else:
+    print(f'{element} ist nicht in der Names-Collection')
