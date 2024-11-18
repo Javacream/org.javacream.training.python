b1cf0437ca6c16524f87572d1492ef470b39ac36
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-18 11:45:01 +0100

    Lösung Slide 38

--- programme/demos/block3/datei_demo.py
+++ programme/demos/block3/datei_demo.py
@@ -1,4 +1,17 @@
-# file = open ('.data.txt') # der optionale 2. Parameter ist 'rt', Lesen einer Text-Datei
-with open ('./programme/demos/block3/data.txt', 'rt') as file:
-    rows = file.readlines()
-print(rows)
+base_path = './programme/demos/block3'
+while True:
+    file_name = input ('Name der Datei: ')
+    path = f'{base_path}/{file_name}'
+    try:
+        with open (path, 'rt') as file:
+            rows = file.readlines()
+        for row in rows:
+            if (row[-1] == '\n'):
+                print(row[:-1])
+            else:
+                print(row)    
+    except Exception as e:
+        print(f'falscher Dateiname: {file_name}: {e}')    
+    again = input ('Nochmal ? j|n')
+    if again == 'n':
+        break    
