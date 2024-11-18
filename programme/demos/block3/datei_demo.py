d06296599e9a5e76a5fa46309c9028f4656cdb14
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-18 11:27:39 +0100

    Anpassen der Dateilokation

--- programme/demos/block3/datei_demo.py
+++ programme/demos/block3/datei_demo.py
@@ -1,4 +1,4 @@
-# file = open ('./data.txt') # der optionale 2. Parameter ist 'rt', Lesen einer Text-Datei
-with open ('./data.txt', 'rt') as file:
+# file = open ('.data.txt') # der optionale 2. Parameter ist 'rt', Lesen einer Text-Datei
+with open ('./programme/demos/block3/data.txt', 'rt') as file:
     rows = file.readlines()
 print(rows)
