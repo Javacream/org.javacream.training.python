1df4934a9e3b6617fe54c9d0e273b58160fb2f5e
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-18 09:44:17 +0100

    slicing, negative Indizes

--- programme/demos/block3/collections_demo.py
+++ programme/demos/block3/collections_demo.py
@@ -1,7 +1,11 @@
 names = ['Hugo', 'Frieda', 'Andrea', 'Hugo', 'Fritz']
 for name in names:
     print(name)
-print(names[2])    
+print(names[-2])
+sub_names_list = names[2:]
+print(sub_names_list)    
+print(len(names))
+print(len(sub_names_list))
 
 text = "Ein Text"
 for character in text:
