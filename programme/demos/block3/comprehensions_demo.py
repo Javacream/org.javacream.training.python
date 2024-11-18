ef7dca7e1ba68a38f1aa542d8eec581caf5f1f68
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-18 14:57:22 +0100

    Einführen der Comprehension Syntax

--- programme/demos/block3/comprehensions_demo.py
+++ programme/demos/block3/comprehensions_demo.py
@@ -2,9 +2,6 @@ names = ['Hugo', 'Frieda', 'Andrea', 'Harald', 'Fritz']
 
 # Länge des Namens, wenn Name in Namenslist mit 'H' beginnt
 
-result = []
-for name in names:
-    if name.startswith('H'):
-        result.append(len(name))
+result = [len(name) for name in names if name.startswith('H')]
 
 print(result)
