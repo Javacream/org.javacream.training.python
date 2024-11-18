2189bc5ef519bfb729623b5746559e4088acdb5b
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-18 10:16:11 +0100

    Blockkommentar (CTRL K CTRL C)

--- programme/demos/block3/collections_demo.py
+++ programme/demos/block3/collections_demo.py
@@ -1,17 +1,16 @@
-""" names = ['Hugo', 'Frieda', 'Andrea', 'Hugo', 'Fritz']
-for name in names:
-    print(name)
-print(names[-2])
-sub_names_list = names[2:]
-print(sub_names_list)    
-print(len(names))
-print(len(sub_names_list))
+# names = ['Hugo', 'Frieda', 'Andrea', 'Hugo', 'Fritz']
+# for name in names:
+#     print(name)
+# print(names[-2])
+# sub_names_list = names[2:]
+# print(sub_names_list)    
+# print(len(names))
+# print(len(sub_names_list))
 
-text = "Ein Text"
-for character in text:
-    print(character)
-print(text[2])    
- """
+# text = "Ein Text"
+# for character in text:
+#     print(character)
+# print(text[2])    
 
 names = {'Hugo', 'Frieda', 'Andrea', 'Hugo', 'Fritz'}
 for name in names:
