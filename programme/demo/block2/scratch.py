def3f126df9ee3b5dc116be94dd60396d60c21cb
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-11-20 11:40:28 +0100

    for in schleife

--- programme/demo/block2/scratch.py
+++ programme/demo/block2/scratch.py
@@ -1,32 +1,5 @@
-# # Endlos-Schleife
-# condition = True
-# while condition:
-#     pass
+names = ['Hugo', 'Hannah', 'Andrea']
+print(names)
 
-# Von-bis-Schleife
-
-start = 0
-counter = start
-end = 5
-# condition = counter < end: Hier wird die Bedingung nur einmal geprüft -> True -> Endlos
-while counter < end:
-    # pass # Nicht vergessen: counter hochzählen -> Endlos
-    print(f'{counter}')
-    #counter = counter + 1
-    counter += 1
-    #counter++ # in Python gibt es kein PlusPlus
-
-counter = 0
-while True:
-    counter += 1
-    print(f'{counter}')
-    if counter >= 5:
-        break
-counter = 0
-while counter < 5:
-    print(f'{counter}')
-    counter += 1
-    if counter % 2:
-        continue
-    print('nächster Durchlauf')
-
+for name in names: # Zuweisung an die Variable name erfolgt intern
+    print(name)
