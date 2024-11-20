2d772c1e38a44deea83f2b8f82af71a1baf50315
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-11-20 13:44:18 +0100

    weitere Verarbeitung in try

--- programme/demo/block2/scratch.py
+++ programme/demo/block2/scratch.py
@@ -3,6 +3,7 @@ while True:
         n1 = int(input())
         n2 = int(input())
         n3 = int(input())
+        print (n1 + n2 / n3)
     except Exception as e:
         print(f'da hat etwas nicht geklappt: {e}')
     again = input('nochmal j|n')
