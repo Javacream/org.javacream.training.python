5c16b4c61e8e89461cb4b20442ccaeeb08856de0
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-11-20 15:09:54 +0100

    Refactoring fizz-buzz.py

--- programme/block2/fizz_buzz.py
+++ programme/block2/fizz_buzz.py
@@ -1,19 +1,31 @@
-number = 1
-while number <= 30:
-    dividable_by_three = False
-    dividable_by_five = False
-    if number % 3 == 0:
-        dividable_by_three = True
-    if number % 5 == 0:
-        dividable_by_five = True
+ITERATION_START = 11
+ITERATION_END = 47
+ITERATION_STEP = 1
 
-    if dividable_by_three and dividable_by_five:
-        print('FizzBuzz')
-    elif dividable_by_three:
-        print('Fizz')
-    elif dividable_by_five:
-        print('Buzz')
+FIRST_DIVIDER = 3
+SECOND_DIVIDER = 7
+
+FIRST_DIVIDER_MESSAGE = "Fiz"
+SECOND_DIVIDER_MESSAGE = "Buz"
+
+BOTH_DIVIDER_MESSAGE = FIRST_DIVIDER_MESSAGE + SECOND_DIVIDER_MESSAGE
+
+number = ITERATION_START
+while number <= ITERATION_END:
+    first_dividable = False
+    second_dividable = False
+    if number % FIRST_DIVIDER == 0:
+        first_dividable = True
+    if number % SECOND_DIVIDER == 0:
+        second_dividable = True
+
+    if first_dividable and second_dividable:
+        print(f'{BOTH_DIVIDER_MESSAGE}')
+    elif first_dividable:
+        print(f'{FIRST_DIVIDER_MESSAGE}')
+    elif second_dividable:
+        print(f'{SECOND_DIVIDER_MESSAGE}')
     else:
         print(f'{number}')
-    number += 1    
+    number += ITERATION_STEP    
 
