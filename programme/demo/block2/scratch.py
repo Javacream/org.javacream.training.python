19a0d6bbb416aff5a1e7b90bf7b482f0ba7a9b98
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-11-20 13:34:55 +0100

    step 3

--- programme/demo/block2/scratch.py
+++ programme/demo/block2/scratch.py
@@ -1,8 +1,10 @@
-try:
-    pass
-except:
-    print('da hat etwas nicht geklappt')
-
-n1 = int(input())
-n2 = int(input())
-n3 = int(input())
+while True:
+    try:
+        n1 = int(input())
+        n2 = int(input())
+        n3 = int(input())
+    except:
+        print('da hat etwas nicht geklappt')
+    again = input('nochmal j|n')
+    if again == 'n':
+        break
