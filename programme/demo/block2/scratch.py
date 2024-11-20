4fa021ad57e98b3afadf13300f858e8f936c54b7
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-11-20 15:48:58 +0100

    Zum else in Zusammenhang mit einer Vorschleife

--- programme/demo/block2/scratch.py
+++ programme/demo/block2/scratch.py
@@ -1,11 +1,5 @@
-while True:
-    try:
-        n1 = int(input())
-        n2 = int(input())
-        n3 = int(input())
-        print (n1 + n2 / n3)
-    except Exception as e:
-        print(f'da hat etwas nicht geklappt: {e}')
-    again = input('nochmal j|n')
-    if again == 'n':
-        break
+for name in ['Hugo', 'Emil']:
+    print(name)
+    break
+else:
+    print('wird aufgerufen nur dann, wenn die Schleife ohne break normal terminiert')    
