4d136ede70659c769b8b0b40b8e01d6415c747b6
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-25 09:39:25 +0100

    functions step1

---
+++ programme/demos/block4/functions.py
@@ -0,0 +1,13 @@
+def function1():
+    print('called function1')
+
+def function2():
+    print('called function2')
+
+function1()
+function2()
+function1()
+function1()
+
+
+
---
+++ programme/demos/block4/revisiting.py
@@ -0,0 +1,3 @@
+message = 'Hello'
+names = ['Hugo', 'Emil']
+print('dome')
