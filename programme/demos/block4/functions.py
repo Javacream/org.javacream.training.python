301eef22e4e8c217e30c61a0d2aa0bfef5eaa93b
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-25 10:22:13 +0100

    mehr als ein Parameter

--- programme/demos/block4/functions.py
+++ programme/demos/block4/functions.py
@@ -2,15 +2,15 @@ def function1(param = 'Parameter 1'):
     for_fn1 = 'Hello'
     print(f'called function1, {for_fn1}, {param}')
 
-def function2(param):
+def function2(p1, p2=42):
     for_fn2 = 'Goodbye'
-    print(f'called function2, {for_fn2}, {param}')
+    print(f'called function2, {for_fn2}, {p1}')
 
 
 def main():
     message = 'var im main-Stack'
     function1()
-    function2(message)
+    function2(message, True)
 
 main()
 
