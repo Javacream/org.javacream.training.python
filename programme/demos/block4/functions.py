19439c00408defbdcdf5afe3e3493ee95286e167
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-25 10:13:44 +0100

    parameter verhalten sich wie lokale Variablen

--- programme/demos/block4/functions.py
+++ programme/demos/block4/functions.py
@@ -1,20 +1,16 @@
-def function1():
+def function1(param = 'Parameter 1'):
     for_fn1 = 'Hello'
-    message = 'Global, changed by fn1'
-    print(f'called function1, {for_fn1}, {message}')
+    print(f'called function1, {for_fn1}, {param}')
 
 def function2():
     for_fn2 = 'Goodbye'
-    #print(f'called function2, {for_fn2}, {message}')
-    #print(f'called function2, {for_fn1}')
+    print(f'called function2, {for_fn2}')
 
 
 def main():
     message = 'var im main-Stack'
     function1()
     function2()
-    function1()
-    function1()
 
 main()
 
