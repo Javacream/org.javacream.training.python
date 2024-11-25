667fa804709d7c93da10640d03b94e2d2596116a
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-25 10:04:47 +0100

    template für eine Anwendung mit main-Funktion

--- programme/demos/block4/functions.py
+++ programme/demos/block4/functions.py
@@ -1,19 +1,22 @@
 def function1():
-    global message
     for_fn1 = 'Hello'
     message = 'Global, changed by fn1'
     print(f'called function1, {for_fn1}, {message}')
 
 def function2():
     for_fn2 = 'Goodbye'
-    print(f'called function2, {for_fn2}, {message}')
+    #print(f'called function2, {for_fn2}, {message}')
     #print(f'called function2, {for_fn1}')
 
-message = 'Global'
-function1()
-function2()
-function1()
-function1()
+
+def main():
+    message = 'var im main-Stack'
+    function1()
+    function2()
+    function1()
+    function1()
+
+main()
 
 
 
