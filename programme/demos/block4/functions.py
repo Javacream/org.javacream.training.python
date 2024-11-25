ca96f67dbfc7adec5e3ad153e730183940652a05
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-25 10:18:39 +0100

    Parameter ohne default MÜSSEN im Aufruf gesetzt werden

--- programme/demos/block4/functions.py
+++ programme/demos/block4/functions.py
@@ -2,15 +2,15 @@ def function1(param = 'Parameter 1'):
     for_fn1 = 'Hello'
     print(f'called function1, {for_fn1}, {param}')
 
-def function2():
+def function2(param):
     for_fn2 = 'Goodbye'
-    print(f'called function2, {for_fn2}')
+    print(f'called function2, {for_fn2}, {param}')
 
 
 def main():
     message = 'var im main-Stack'
-    function1(message) # param wird mit message überschrieben, das ist unmöglich für for_fn1
-    function2()
+    function1()
+    function2(message)
 
 main()
 
