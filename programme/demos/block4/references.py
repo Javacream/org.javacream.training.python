94e32a69ce7de78f91b99f1ba115820b50ee7d6d
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-25 11:33:08 +0100

    return Statement

--- programme/demos/block4/references.py
+++ programme/demos/block4/references.py
@@ -1,6 +1,11 @@
 def add_element_to(data):
     data.append('C')
 
+def change_data(data):
+    data = ['Y', 'Z']
+    return data
+
+
 def main():
     demo_set = ['A', 'B']
     print(f'vor aufruf: {demo_set}')
@@ -9,5 +14,7 @@ def main():
     add_element_to(demo_set.copy())
     print(f'nach aufruf mit copy(): {demo_set}')
     print('done')
+    changed = change_data(demo_set)
+    print(f'nach aufruf von change_data: {demo_set}, {changed}')
 
 main()
