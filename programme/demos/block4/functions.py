c67690a9a6d25c7d5ee40fe951561693bb30c6a6
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-25 09:43:50 +0100

    lokale und globale Variablen, functions step2

--- programme/demos/block4/functions.py
+++ programme/demos/block4/functions.py
@@ -1,9 +1,13 @@
 def function1():
-    print('called function1')
+    message_for_function1 = 'Hello'
+    print(f'called function1, {message_for_function1}, {message}')
 
 def function2():
-    print('called function2')
+    message_for_function2 = 'Goodbye'
+    print(f'called function2, {message_for_function2}, {message}')
+    # print(f'called function2, {message_for_function1}')
 
+message = 'Global'
 function1()
 function2()
 function1()
