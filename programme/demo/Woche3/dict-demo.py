01c501907b52a0058c51880a32fb090b4fc615a7
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-26 10:10:38 +0100

    Dictionary

---
+++ programme/demo/Woche3/dict-demo.py
@@ -0,0 +1,10 @@
+postal_codes = {'81373': 'München', '70567': 'Stuttgart'}
+
+print(postal_codes)
+
+print(postal_codes['81373'])
+
+print(len(postal_codes))
+
+for key in postal_codes:
+    print(f'{key} = {postal_codes[key]}')
