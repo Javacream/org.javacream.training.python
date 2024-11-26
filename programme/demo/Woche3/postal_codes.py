c5ad1ed5edb06c74c486350d110df3a75fe4f925
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-26 13:20:37 +0100

    Lesender Dateizugriff

--- programme/demo/Woche3/postal_codes.py
+++ programme/demo/Woche3/postal_codes.py
@@ -1,4 +1,3 @@
-data_file = open('./programme/demo/Woche3/postal_codes.data', 'rt')
-raw_rows = data_file.readlines()
-data_file.close()
+with open('./programme/demo/Woche3/postal_codes.data', 'rt') as data_file:
+    raw_rows = data_file.readlines()
 print(raw_rows)
