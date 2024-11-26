161d4970e7c2febf0ebc6ca2ee5388a256b6ec4c
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-26 11:45:58 +0100

    Lösung Aufgabe 3

--- programme/Woche3/Aufgabe3.py
+++ programme/Woche3/Aufgabe3.py
@@ -1,13 +1,5 @@
 text = "Das ist ein simpler Text. Auch das Folgende ist simpel"
 
 words_list = text.split(' ')
-#print(words_list)
-#words_list_as_text = str(words_list)
-#print(words_list_as_text)
-
-# words_list_as_text = ''
-# for word in words_list:
-#     words_list_as_text = f'{words_list_as_text} {word}'
-# print(words_list_as_text)
-
+print(words_list)
 print(' '.join(words_list))
