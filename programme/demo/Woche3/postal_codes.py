c5f4b913219b4257b610f9192cbcdced43e05244
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-26 14:37:45 +0100

    Beispiele mit Comprehensions

--- programme/demo/Woche3/postal_codes.py
+++ programme/demo/Woche3/postal_codes.py
@@ -1,23 +1,9 @@
 with open('./programme/demo/Woche3/postal_codes.data', 'rt') as data_file:
     raw_rows = data_file.readlines()
 
-# Daten bereinigen, also \n, falls vorhanden, entfernen
-rows = []
-for row in raw_rows:
-    if row.endswith('\n'):
-        row = row[0:len(row) - 1] # Hinweis: Das geht noch eleganter
-    rows.append(row)
-# Jede Zeile separat nach dem = trennen
-postal_codes = {}
-for row in rows:
-    postal_code_and_city = row.split('=')    
-    postal_code = postal_code_and_city[0]
-    city= postal_code_and_city[1]
-    postal_codes[postal_code] = city
-# und als Key=Value-Paar einem Dictionary hinzufügen
+rows = [row[:-1] if row.endswith('\n') else row for row in raw_rows ]
+postal_codes = {row.split('=')[0]:row.split('=')[1] for row in rows}
 
-print(postal_codes)
-# Benutzereingabe: Postleitzahl? -> Stadt
 
 while True:
     code = input('Bitte geben Sie eine Postleitzahl ein: ')
--- programme/demo/Woche3/reverse_postal_codes.py
+++ programme/demo/Woche3/reverse_postal_codes.py
@@ -1,13 +1,6 @@
 with open('./programme/demo/Woche3/postal_codes.data', 'rt') as data_file:
     raw_rows = data_file.readlines()
-
-# Daten bereinigen, also \n, falls vorhanden, entfernen
-rows = []
-for row in raw_rows:
-    if row.endswith('\n'):
-        row = row[0:len(row) - 1] # Hinweis: Das geht noch eleganter
-    rows.append(row)
-# Jede Zeile separat nach dem = trennen
+rows = [row[:-1] if row.endswith('\n') else row for row in raw_rows ]
 reverse_postal_codes = {}
 for row in rows:
     postal_code_and_city = row.split('=')    
@@ -18,6 +11,7 @@ for row in rows:
         postal_codes = []
         reverse_postal_codes[city] = postal_codes
     postal_codes.append(postal_code)
+
 while True:
     city = input('Bitte geben Sie eine Stadt ein: ')
     codes = reverse_postal_codes.get(city)
