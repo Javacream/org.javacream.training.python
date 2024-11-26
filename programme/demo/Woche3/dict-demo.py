91c2e98e6c5410bf8b86d4d971c0c0342aa571e7
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-26 13:07:32 +0100

    Motivation dict

--- programme/demo/Woche3/dict-demo.py
+++ programme/demo/Woche3/dict-demo.py
@@ -8,3 +8,19 @@ print(len(postal_codes))
 
 for key in postal_codes:
     print(f'{key} = {postal_codes[key]}')
+
+
+# Nachbildung eines dicts, nicht ernst gemein...
+
+key1 = '81372'
+key2 = '70567'
+value1 = 'München'
+value2 = 'Stuttgart'
+
+selection = '81372'
+if selection == key1:
+    result = value1
+elif selection == key2:
+    result = value2
+else:
+    result = "Not found"    
