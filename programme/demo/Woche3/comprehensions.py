58581eb0eca9ea388838d370dc29d1db504b8525
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-26 14:22:12 +0100

    comprehension, teil 1

--- programme/demo/Woche3/comprehensions.py
+++ programme/demo/Woche3/comprehensions.py
@@ -1,6 +1,13 @@
 names = ['Hugo', 'Andrea', 'Hannah', 'Gregor', "Emiliana", 'Harald']
 
+# ___________________________
+
 result = []
 
 for name in names:
     result.append(name)
+
+
+# ___________________________
+comprehension_result = [name for name in names]
+print('done')
