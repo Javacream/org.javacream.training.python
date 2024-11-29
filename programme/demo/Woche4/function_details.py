50b177fa3206b0d3f7a280f09ba8270a5844b19a
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-29 12:07:35 +0100

    Funktionsparameter Variationen

--- programme/demo/Woche4/function_details.py
+++ programme/demo/Woche4/function_details.py
@@ -1,7 +1,10 @@
-def fn_with_param(**kwargs): # Keyworded args
-    for arg in kwargs:
-        print(arg)
-
+def fn_with_param(p1, p2, *args, **kwargs): # Keyworded args
+    print(f'p1={p1}, p2={p2}, args={args}, kwargs={kwargs}')
 def main():
-    fn_with_param(name='Sawitzki', height=183)
+    fn_with_param('Erster Param', 'Zweiter Param', name='Sawitzki', height=183)
+    fn_with_param('Erster Param', 'Zweiter Param')
+    fn_with_param('Erster Param', 'Zweiter Param', 'dies', 'und', 'das')
+    fn_with_param('Erster Param', 'Zweiter Param', 'dies', 'und', 'das', name='Sawitzki', height=183)
+
+
 main()
