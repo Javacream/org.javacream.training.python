8b89644a40cf6ecd36b4b8d8c87e09e5c9d3c9a7
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-29 10:54:13 +0100

    Funktionen mit return

--- programme/demo/Woche4/shopping_list.py
+++ programme/demo/Woche4/shopping_list.py
@@ -1,20 +1,22 @@
 # Unser Shopping-List-Beispiel
 
 def read_raw_shopping_list():
-    raw_shopping_list = 'raw_shopping_list'
-    print(raw_shopping_list)
+    path = './programme/demo/Woche4/raw_shopping_list.txt'
+    with open (path) as file:
+        raw_items = file.readlines()
+    return raw_items
 
 def create_shopping_list():
     shopping_list = 'shopping_list in create'
     print(shopping_list)
 
 def write_shopping_list():
+    path = './programme/demo/Woche4/shopping_list.txt'
     shopping_list = 'shopping_list in write'
     print(shopping_list)
 
 def main():
-    main_var = 'main'
-    read_raw_shopping_list()
+    raw_shopping_list = read_raw_shopping_list()
     create_shopping_list()
     write_shopping_list()
 
