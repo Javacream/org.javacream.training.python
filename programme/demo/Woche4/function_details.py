438ae2613c11bac13e2b694f8bf9cd896f295ade
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-29 11:57:22 +0100

    TypeHints sind keine statischen Typisierungen, die vom Interpreter geprüft werden

--- programme/demo/Woche4/function_details.py
+++ programme/demo/Woche4/function_details.py
@@ -3,6 +3,7 @@ def function_with_string_param(s: str):
 
 def function_with_list_param(l: list[str]):
     first_element = l[0]
+    l.append("D")
     first_element.upper()
     return True
 
@@ -11,4 +12,5 @@ def main():
     character_list = ['A', 'B', 'S']
     function_with_string_param(string)
     result: bool = function_with_list_param(character_list)
+    function_with_list_param(string)
 main()
