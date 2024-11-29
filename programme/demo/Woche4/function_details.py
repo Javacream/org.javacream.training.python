cbbab5744b8ebca0aad6148fe38238c6c66bfc6f
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-29 11:45:28 +0100

    Python kann kein Überschreiben von Funktionen

--- programme/demo/Woche4/function_details.py
+++ programme/demo/Woche4/function_details.py
@@ -1,8 +1,14 @@
 def fn1():
     print('called fn1')
     return "OK"
+def fn1():
+    print('CHANGED called fn1')
+    return "OK"
+def fn1(param):
+    print('called fn1 with param')
+    return "OK"
 
 def main():
-    print(fn1) # Runde Klammern für den Aufruf
+    print(fn1())
 
 main()
