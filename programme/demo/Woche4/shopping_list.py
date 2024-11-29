05c070ed2d968e0b6ef186147c7d4f21cec5e0e6
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-29 10:19:13 +0100

    Schritt 2: Funktionen gruppieren Variablen

--- programme/demo/Woche4/shopping_list.py
+++ programme/demo/Woche4/shopping_list.py
@@ -1,17 +1,24 @@
 # Unser Shopping-List-Beispiel
 
 def read_raw_shopping_list():
-    pass
+    raw_shopping_list = 'raw_shopping_list'
+    print(raw_shopping_list)
 
 def create_shopping_list():
-    pass
+    shopping_list = 'shopping_list in create'
+    print(shopping_list)
 
 def write_shopping_list():
-    pass
+    shopping_list = 'shopping_list in write'
+    print(shopping_list)
 
 def main():
+    main_var = 'main'
     read_raw_shopping_list()
     create_shopping_list()
     write_shopping_list()
 
+outer_var = 'outer'
+
 main()
+
