cf85e68565043a6359f4dda6a1f1d15271b070e1
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-29 09:47:02 +0100

    Funktionen Schritt 1

---
+++ programme/demo/Woche4/shopping_list.py
@@ -0,0 +1,17 @@
+# Unser Shopping-List-Beispiel
+
+def read_raw_shopping_list():
+    pass
+
+def create_shopping_list():
+    pass
+
+def write_shopping_list():
+    pass
+
+def main():
+    read_raw_shopping_list()
+    create_shopping_list()
+    write_shopping_list()
+
+main()
