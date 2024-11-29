fe5e19eb6111d3a09eb5dfca18a302f0e4ac0ac4
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-11-29 11:00:10 +0100

    Parameter mit Default-Wert

--- programme/demo/Woche4/shopping_list.py
+++ programme/demo/Woche4/shopping_list.py
@@ -6,9 +6,10 @@ def read_raw_shopping_list():
         raw_items = file.readlines()
     return raw_items
 
-def create_shopping_list():
-    shopping_list = 'shopping_list in create'
-    print(shopping_list)
+def create_shopping_list(items = ['Apfel', 'Birne', 'Apfel']):
+    unique_items = set(items)
+    shopping_list = {item: items.count(item) for item in unique_items}
+    return shopping_list
 
 def write_shopping_list():
     path = './programme/demo/Woche4/shopping_list.txt'
@@ -17,7 +18,7 @@ def write_shopping_list():
 
 def main():
     raw_shopping_list = read_raw_shopping_list()
-    create_shopping_list()
+    shopping_list = create_shopping_list()
     write_shopping_list()
 
 main()
