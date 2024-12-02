cdb5a4c417facd5a26b441921676e54698b53e3d
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-12-02 12:08:12 +0100

    Bidirektionale Assoziation Person Address

--- programme/demos/Woche5/people.py
+++ programme/demos/Woche5/people.py
@@ -4,6 +4,9 @@ class Person:
         self.lastname = lastname
         self.firstname = firstname
         self.addresses = set(addresses)
+        for address in addresses:
+            address.people.add(self)
+            
     def say_hello(self):
         message = f'Hello, my name is {self.firstname} {self.lastname}'
         return message
@@ -12,7 +15,8 @@ class Person:
 class Address:
     def __init__(self, param_city, param_street):
         self.city = param_city
-        self.street = param_street   
+        self.street = param_street  
+        self.people = set() 
 
 def main():
     a1 = Address('München', 'Marienplatz')
