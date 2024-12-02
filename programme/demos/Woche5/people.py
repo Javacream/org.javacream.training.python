ed4fe9bfdfdae89936c83bd8ec5810357906610d
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-12-02 10:21:33 +0100

    Instanzierung über Konstruktor-Aufruf

--- programme/demos/Woche5/people.py
+++ programme/demos/Woche5/people.py
@@ -2,12 +2,21 @@ class Person:
     def __init__(self, id, lastname, firstname):  # Der Konstruktor der Klasse Person
         self.id = id
         self.lastname = lastname
-        self.firstane = firstname
+        self.firstname = firstname
     def say_hello(self):
         pass
 
 
 class Address:
-    def __init__(self, city, street):
-        self.city = city
-        self.street = street    
+    def __init__(self, param_city, param_street):
+        self.city = param_city
+        self.street = param_street   
+
+my_list = []  # -> leere Liste mit Literal
+my_list = list() # -> leere Liste
+
+# person1 = -> Person mit Literal geht nicht, es gibt kein Personen-Literal
+person1 = Person(1, 'Musterperson', 'Andrea') # Der erste Parameter des Konstruktors, als 'self' wird hier nicht angegeben, sondern intern erzeugt
+person2 = Person(2, 'Schneider', 'Hannah') # Der erste Parameter des Konstruktors, als 'self' wird hier nicht angegeben, sondern intern erzeugt
+print('done')
+a = Address('München', 'Marienplatz')
