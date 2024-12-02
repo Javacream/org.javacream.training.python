594223ea5d8d974825562ab17c8e8f8460252cb6
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-12-02 10:03:53 +0100

    Klassen Person und Address, Slide 67

---
+++ programme/demos/Woche5/people.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, id, lastname, firstname):  # Der Konstruktor der Klasse Person
+        self.id = id
+        self.lastname = lastname
+        self.firstane = firstname
+    def say_hello(self):
+        pass
+
+
+class Address:
+    def __init__(self, city, street):
+        self.city = city
+        self.street = street    
