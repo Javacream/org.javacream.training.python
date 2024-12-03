7ba4cf22c4d7257fae4faa7eb159444e6806fd9a
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-03 09:51:40 +0100

    Person, Klassendiagramm Slide 70

---
+++ programme/demo/Woche5/people.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, lastname, firstname, weight): # Konstruktor
+        self.lastname = lastname 
+        self.firstname = firstname
+        self.weight = weight
+    def say_hello(self):
+        pass
---
+++ programme/demo/Woche5/people_app.py
@@ -0,0 +1,8 @@
+from people import Person
+def main():
+    p1 = Person('H', 'J', 75.9)
+    p2 = Person('A', 'B', 95.9)
+    print('done')
+
+if __name__ == '__main__':
+    main()
