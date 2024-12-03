33e904a337b355b53d6b5094abe72e62850075a5
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-03 11:18:43 +0100

    0..n

--- programme/demo/Woche5/people.py
+++ programme/demo/Woche5/people.py
@@ -1,8 +1,10 @@
 class Person:
-    def __init__(self, lastname, firstname, weight):
+    def __init__(self, lastname, firstname, weight, main_address, *addresses):
         self.lastname = lastname 
         self.firstname = firstname
         self.weight = weight
+        self.main_address = main_address
+        self.addresses = set(addresses)
     def say_hello(self):
         message = f"Hallo, mein Name ist {self.firstname} {self.lastname}"
         return message
--- programme/demo/Woche5/people_app.py
+++ programme/demo/Woche5/people_app.py
@@ -1,12 +1,10 @@
 from people import Person, Address
 def main():
-    address = Address(city='München', street='Marienplatz')
-    p1 = Person('H', 'J', 75.9)
-    p2 = Person('A', 'B', 95.9)
-    p1.address = address
-    print(p1.say_hello())
-    print(p2.say_hello())
-    del (p1.address)
+    a1 = Address(city='München', street='Marienplatz')
+    a2 = Address(city='Berlin', street='Alexanderplatz')
+    a3 = Address(city='Stuttgart', street='Schlossplatz')
+    p1 = Person('H', 'J', 75.9, a2)
+    p2 = Person('A', 'B', 95.9, a1, a2, a3)
     print(p1.say_hello())
     print(p2.say_hello())
 
