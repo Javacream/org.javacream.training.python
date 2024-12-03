f047b486eb4961b570a5a8460439d0b192af7af2
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-03 15:04:06 +0100

    Kapselung

--- programme/Woche5/Aufgabe1/bankaccount.py
+++ programme/Woche5/Aufgabe1/bankaccount.py
@@ -1,17 +1,17 @@
 class BankAccount:
     def __init__(self, customer):
-        self.customer = customer
-        self.account = 0
+        self.__customer = customer
+        self.__account = 0
     def get_account(self):
-        return self.account
+        return f'Das Konto für {self.__customer} hat einen Kontostand von {self.__account}'
     
     def deposit(self, amount):
         if amount <= 0:
             error = Exception(f'amount must be positive, not {amount}')
             raise error
-        self.account += amount    
+        self.__account += amount    
 
     def payout(self, amount):
         if amount <= 0:
             raise Exception(f'amount must be positive, not {amount}')
-        self.account -= amount                
+        self.__account -= amount                
--- programme/Woche5/Aufgabe1/bankaccount_app.py
+++ programme/Woche5/Aufgabe1/bankaccount_app.py
@@ -4,8 +4,10 @@ def main():
     account = BankAccount('Musterperson')
     try:
         account.deposit(9.99)
-        account.deposit(-12.22)
+        account.deposit(12.22)
         account.payout(5)
+        # account.__account = 666 -> ignoriert, privates Attribut
+        account._BankAccount__account = 666 # geht, ein privates Attribut wird nur "komisch" umbenannt
     except Exception as e:
         print(f'error: {e}')
     print(account.get_account())
