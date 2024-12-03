f9a8db7550a775b85f57b7677965dff0dc7a384e
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-03 11:27:04 +0100

    Bidirektionale 1:n

--- programme/demo/Woche5/people.py
+++ programme/demo/Woche5/people.py
@@ -1,10 +1,10 @@
 class Person:
-    def __init__(self, lastname, firstname, weight, main_address, *addresses):
+    def __init__(self, lastname, firstname, weight, address):
         self.lastname = lastname 
         self.firstname = firstname
         self.weight = weight
-        self.main_address = main_address
-        self.addresses = set(addresses)
+        self.address = address
+        address.people.append(self)
     def say_hello(self):
         message = f"Hallo, mein Name ist {self.firstname} {self.lastname}"
         return message
@@ -13,5 +13,6 @@ class Address:
     def __init__(self, city, street):
         self.city = city
         self.street = street
+        self.people = list()
     def get_address(self):
         return f'Addresse: {self.street}, {self.city}'
--- programme/demo/Woche5/people_app.py
+++ programme/demo/Woche5/people_app.py
@@ -3,8 +3,8 @@ def main():
     a1 = Address(city='München', street='Marienplatz')
     a2 = Address(city='Berlin', street='Alexanderplatz')
     a3 = Address(city='Stuttgart', street='Schlossplatz')
-    p1 = Person('H', 'J', 75.9, a2)
-    p2 = Person('A', 'B', 95.9, a1, a2, a3)
+    p1 = Person('H', 'J', 75.9, a1)
+    p2 = Person('A', 'B', 95.9, a1)
     print(p1.say_hello())
     print(p2.say_hello())
 
