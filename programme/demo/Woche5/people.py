33c6ffcff0b08daffe6365482b7646861455bc1c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-03 10:54:45 +0100

    fix

--- programme/demo/Woche5/people.py
+++ programme/demo/Woche5/people.py
@@ -7,9 +7,9 @@ class Person:
         message = f"Hallo, mein Name ist {self.firstname} {self.lastname}"
         return message
     
-    class Address:
-        def __init__(self, city, street):
-            self.city = city
-            self.street = street
-        def get_address(self):
-            return f'Addresse: {self.street}, {self.city}'
+class Address:
+    def __init__(self, city, street):
+        self.city = city
+        self.street = street
+    def get_address(self):
+        return f'Addresse: {self.street}, {self.city}'
