3f4258ef27f2a25bf276c8c4aeae850a166641ea
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-09 13:15:38 +0100

    Orchester mit Fehlern, die durch Testen erkannt und behoben werden müssen

---
+++ programme/demo/Woche6/abstract/music.py
@@ -0,0 +1,34 @@
+class Instrument:
+    def make_sound(self):
+        pass
+
+class Guitar(Instrument):
+    def make_sound(self):
+        return 'Zoing'
+class Violin(Instrument):
+    def make_sound(self):
+        return 'FidelFadel'
+class Drum(Instrument):
+    def play(self):  # Die Methode soll doch "make_sound" heißen?
+        return 'Wumm'
+    
+class Orchester:
+    def __init__(self):
+        self.instruments = []
+    def add(self, instrument: Instrument):
+        self.instruments.append(instrument)
+    def concert(self):
+        for instrument in self.instruments:
+            print(instrument.make_sound())
+
+def main():
+    orchester = Orchester()
+    orchester.add(Instrument()) # WAS SOLL DAS?
+    orchester.add(Violin())
+    orchester.add(Guitar())
+    orchester.add(Violin())
+    orchester.add(Drum())
+    orchester.concert()
+
+if __name__ == '__main__':
+    main()    
