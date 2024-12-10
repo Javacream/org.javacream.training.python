883054103564f962069f185c7e084be02e7f1062
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-10 09:38:48 +0100

    spielen mit lambdas

--- programme/demo/Woche5/Woche6/scratch.py
+++ programme/demo/Woche5/Woche6/scratch.py
@@ -1,10 +1,12 @@
-def by_length(s):
-    return len(s)
-def by_second_char(s):
-    return s[1]
 def main():
-    names = ['Hugo', 'Eduard', 'Zoe', 'Andreana']
-    names.sort(reverse=True, key=by_second_char)
+    def by_length(s):
+        return len(s)
+    def by_second_char(s):
+        return s[1]
+    by_third_char = lambda s : s[2]
+    names = ['Hugo', 'Eduard', 'Zoero', 'Andreana']
+    # names.sort(key=by_third_char)
+    names.sort(key= lambda s: s[3])
     print(names)
 if __name__ == '__main__':
     main()
