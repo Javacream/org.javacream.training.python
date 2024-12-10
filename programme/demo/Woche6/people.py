0e21c32c431eaeaa2ff89d4d0aaf449888d4f532
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-10 10:13:29 +0100

    Start mit people Beispiel

---
+++ programme/demo/Woche6/people.py
@@ -0,0 +1,37 @@
+class Person:
+    def __init__(self, lastname, firstname, weight, address):
+        self.lastname = lastname 
+        self.firstname = firstname
+        self.weight = weight
+        self.address = address
+        address.inhabitants.append(self)
+    def say_hello(self):
+        message = f"Hallo, mein Name ist {self.firstname} {self.lastname}"
+        return message
+    def __repr__(self):
+        return f'Person: lastname={self.lastname}'
+    
+class Address:
+    def __init__(self, city, street):
+        self.city = city
+        self.street = street
+        self.inhabitants = list()
+    def get_address(self):
+        return f'Addresse: {self.street}, {self.city}'
+    
+class Company:
+    def __init__(self, name):
+        self.name = name
+        self.addresses = set()
+
+    
+class Student(Person):
+    def __init__(self, lastname, firstname, weight, address, uni):
+        super().__init__(lastname, firstname, weight, address)
+        self.university = uni
+
+    def say_hello(self):
+        message = super().say_hello()
+        return f"{message}, ich studiere"
+    def study(self):
+        return f'{super().say_hello()} studierend an der Universität {self.university}' 
