553ec5d319f0655ed245a1bfe6ce36abb8cfa163
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-10 10:04:10 +0100

    Ausgabe mit __str__ in einer Liste 'funktioniert nicht'

--- programme/demo/Woche5/Woche6/scratch.py
+++ programme/demo/Woche5/Woche6/scratch.py
@@ -1,12 +1,15 @@
 class A:
     def __str__(self):
-        return f'i am {type(self)}'
+        return f'i am A'
 class B(object): # Erben von (object erfolgt implizit)
-    pass
+    def __str__(self):
+        return f'i am B'
 def main():
     a = A()
     b = B()
     print(a)    
-    print(b) 
+    print(b)
+    objects = [a,b]
+    print(objects)
 if __name__ == '__main__':
     main()
