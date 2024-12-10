66a237e3a4814cb2bd2f6a242572736e8dd49149
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-10 11:51:45 +0100

    Operatoren überladen

--- programme/demo/Woche6/scratch.py
+++ programme/demo/Woche6/scratch.py
@@ -1,58 +1,22 @@
-class DataWithReferenceIdentity:
-    def __init__(self, data):
-        self.data = data
+class Money:
+    def __init__(self, amount):
+        self.amount = amount
     def __repr__(self):
-        return f'Data: data={self.data}'
-class Foo:
-    def __init__(self):
-        self.this = 'that'
-        self.goo = 'goo'
-        self.data = 'Hugo'
-class DataWithValueIdentity:
-    def __init__(self, data):
-        self.data = data
-    def __repr__(self):
-        return f'Data: data={self.data}'
+        return f'Money: amount={self.amount}'
     def __eq__(self, other):
-        if isinstance(other, DataWithValueIdentity):
-            return self.data == other.data
-        else:
-            return False
-
+        return self.amount == other.amount
+    def __hash__(self):
+        return hash(self.amount)
+    def __lt__(self, other):
+        return self.amount < other.amount
+    def __add__(self, other):
+        return Money(self.amount + other.amount)
 def main():
-    def demo_data_with_reference_identity():
-        d1 = DataWithReferenceIdentity('Hugo')
-        d2 = DataWithReferenceIdentity('Emil')
-        d3 = DataWithReferenceIdentity('Hugo')
-        d4 = d1
-        print(d1 == d2)
-        print(d1 == d3)
-        print(d1 == d4)
-    def demo_str():
-        s1 = str('Hugo')
-        s2 = str('Emil')
-        s3 = str('Hugo')
-        s4 = s1
-        print(s1 == s2)
-        print(s1 == s3)
-        print(s1 == s4)
-    def demo_data_with_value_identity():
-        d1 = DataWithValueIdentity('Hugo')
-        d2 = DataWithValueIdentity('Emil')
-        d3 = DataWithValueIdentity('Hugo')
-        d4 = d1
-        a = Foo()
-        print(d1 == d2)
-        print(d1 == d3)
-        print(d1 == d4)
-        print(d1 == a)
+    m1 = Money(20)
+    m2 = Money(5)
 
-    print('with reference:')
-    demo_data_with_reference_identity()
-    print('with str')
-    demo_str()
-    print('with value')
-    demo_data_with_value_identity()
-
+    print (m1 > m2)
+    m3 = m1 + m2
+    print(m3)
 if __name__ == '__main__':
     main()
