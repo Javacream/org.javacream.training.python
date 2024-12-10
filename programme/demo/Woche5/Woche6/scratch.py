88f4d89baa244c61f23de648cf019e0c80cc36d5
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-10 09:47:41 +0100

    Lösung OOP mit Dunder-str

--- programme/demo/Woche5/Woche6/scratch.py
+++ programme/demo/Woche5/Woche6/scratch.py
@@ -1,12 +1,8 @@
+class A:
+    def __str__(self):
+        return 'i am A'
 def main():
-    def by_length(s):
-        return len(s)
-    def by_second_char(s):
-        return s[1]
-    by_third_char = lambda s : s[2]
-    names = ['Hugo', 'Eduard', 'Zoero', 'Andreana']
-    # names.sort(key=by_third_char)
-    names.sort(key= lambda s: s[3])
-    print(names)
+    a = A()
+    print(a)    
 if __name__ == '__main__':
     main()
