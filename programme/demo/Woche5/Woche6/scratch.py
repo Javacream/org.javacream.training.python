9b15ccb632b7e0eeed755f10f59b187428d6ddf8
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-10 09:58:54 +0100

    implizite Vererbung von object

--- programme/demo/Woche5/Woche6/scratch.py
+++ programme/demo/Woche5/Woche6/scratch.py
@@ -1,8 +1,12 @@
 class A:
     def __str__(self):
-        return 'i am A'
+        return f'i am {type(self)}'
+class B(object): # Erben von (object erfolgt implizit)
+    pass
 def main():
     a = A()
+    b = B()
     print(a)    
+    print(b) 
 if __name__ == '__main__':
     main()
