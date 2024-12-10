2166d643b20195da7f4cdbdd863e47dc59165e69
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-10 14:12:41 +0100

    Lösungen zu Woche 6

---
+++ programme/Woche6/Aufgabe1.py
@@ -0,0 +1,11 @@
+class Vehicle:
+    def start(self):
+        print("vehicle is starting")
+    def stop(self):
+        print("vehicle is stopping")
+
+class Car(Vehicle):
+    pass
+
+class Motorbike(Vehicle):
+    pass
