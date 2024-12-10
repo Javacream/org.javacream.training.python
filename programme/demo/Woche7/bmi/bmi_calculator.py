c0c8ad7b8900bad4974485d0393c60a6f7eb4d2d
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-12-10 15:28:57 +0100

    Erweiterung des bmi test Beispiels

--- programme/demo/Woche7/bmi/bmi_calculator.py
+++ programme/demo/Woche7/bmi/bmi_calculator.py
@@ -1,3 +1,7 @@
 def calculate_bmi(height, weight):
+    if height < 50:
+        return False
+    if weight <= 0:
+        return False
     bmi = weight/(height*height)*100*100
     return bmi
--- programme/demo/Woche7/bmi/bmi_calculator_tests.py
+++ programme/demo/Woche7/bmi/bmi_calculator_tests.py
@@ -9,11 +9,22 @@ from bmi_calculator import calculate_bmi
 import unittest
 
 class BmiCalculatorTests(unittest.TestCase):
-    def test_bmi_calculation(self):
+    def test_bmi_calculation_with_valid_height_and_weight(self):
         height = 183
         weight = 75.3
         expected_bmi = 22.485
         calculated_bmi = calculate_bmi(height, weight)
         self.assertAlmostEqual(expected_bmi, calculated_bmi, 5)
+    def test_bmi_calculation_fails_if_height_lt_50(self):
+        height = 45
+        weight = 75.3
+        result = calculate_bmi(height, weight)
+        self.assertFalse(result)
+    def test_bmi_calculation_fails_if_weight_lt_0(self):
+        height = 183
+        weight = -5
+        result = calculate_bmi(height, weight)
+        self.assertFalse(result)
+
 if __name__ == '__main__':
     unittest.main()
