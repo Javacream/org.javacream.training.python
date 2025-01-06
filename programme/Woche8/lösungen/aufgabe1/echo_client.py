2320675f8534f6e0a84dd9516e0b59927e2012f5
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-06 12:01:43 +0100

    implementiere Aufgabe 1

--- programme/Woche8/lösungen/aufgabe1/echo_client.py
+++ programme/Woche8/lösungen/aufgabe1/echo_client.py
@@ -6,6 +6,7 @@ def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket: # client_socket ist nur eine leere Hülle!
         client_socket.connect((IP_ADDRESS, PORT)) # ab jetzt ist der Socket real
         print(client_socket)
-        client_socket.sendall('Hello'.encode('utf-8'))
+        client_socket.sendall('Hello World'.encode('utf-8'))
+        print(client_socket.recv(1024).decode())
 if __name__ == '__main__':
     main()
