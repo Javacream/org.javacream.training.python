a3cf812b395f6882f24e8f92fe0c1366dac15d73
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-06 14:12:52 +0100

    Multiprocessing

--- programme/Woche8/demo/parallel.py
+++ programme/Woche8/demo/parallel.py
@@ -1,5 +1,6 @@
 import time
 import random
+import multiprocessing as mp
 def do_something(id):
     counter = 0
     while True:
@@ -7,8 +8,9 @@ def do_something(id):
         print(f'{id} -> {counter}')
         counter += 1
 def main():
-    do_something('Process 1')
-    do_something('Process 2')
-
+    process1 = mp.Process(target=do_something, args=('Process 1', ))
+    process2 = mp.Process(target=do_something, args=('Process 2', ))
+    process1.start()
+    process2.start()
 if __name__ == '__main__':
     main()
