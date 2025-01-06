adeaeb05254e194dad8e4ad137b67b883c2ff3be
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-06 12:02:57 +0100

    Lösung Aufgabe 1

--- programme/Woche8/lösungen/aufgabe1/echo_client.py
+++ programme/Woche8/lösungen/aufgabe1/echo_client.py
@@ -5,8 +5,8 @@ def main():
     PORT = 12345
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket: # client_socket ist nur eine leere Hülle!
         client_socket.connect((IP_ADDRESS, PORT)) # ab jetzt ist der Socket real
-        print(client_socket)
-        client_socket.sendall('Hello World'.encode('utf-8'))
+        message = input('Nachricht eingeben: ')
+        client_socket.sendall(message.encode('utf-8'))
         print(client_socket.recv(1024).decode())
 if __name__ == '__main__':
     main()
--- programme/Woche8/lösungen/aufgabe1/echo_server.py
+++ programme/Woche8/lösungen/aufgabe1/echo_server.py
@@ -7,12 +7,8 @@ def main():
         server_sock.listen(1)
         while True:
             client_sock, address_info = server_sock.accept()
-            print(address_info)
             message_from_client = client_sock.recv(10+24)
             message_from_client = message_from_client.decode()
-            print(f'Received: {message_from_client}')
             client_sock.sendall(f'From Server: {message_from_client}'.encode())
-
-
 if __name__ == '__main__':
     main()
