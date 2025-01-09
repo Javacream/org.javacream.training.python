44b16d61cd353d0a90a628a302b4527b080007ca
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-09 10:01:47 +0100

    inführen sprechender Variablennamen

--- programme/Woche1/demo/bmi.py
+++ programme/Woche1/demo/bmi.py
@@ -1,6 +1,6 @@
 # Dieses Programm berechnet den BMI einer Person
-A1='Sawitzki' # A1 Name der Person
-B1=183 # B1 Körpergröße in cm
-C1=75.3 # C1 Gewicht in Kilo
-D1=C1/(B1*B1)*10000 # Berechnung des BMI
-print(D1)
+name='Sawitzki'
+height=183
+weight=75.3
+bmi=weight/(height*height)*10000
+print(bmi)
