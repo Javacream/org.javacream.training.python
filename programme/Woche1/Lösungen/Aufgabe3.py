13d767aa29018e96b141ba887870cac3c212d9d8
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-09 12:10:09 +0100

    Lösung Aufgabe 3

--- programme/Woche1/Lösungen/Aufgabe3.py
+++ programme/Woche1/Lösungen/Aufgabe3.py
@@ -1,6 +1,8 @@
 # Dieses Programm berechnet den BMI einer Person
-name='Sawitzki'
-height=183
-weight=75.3
+name=input('Bitte geben Sie ihren Namen ein: ')
+height=input('Bitte geben Sie ihre Körpergröße in Zentimetern ein: ')
+weight=input('Bitte geben Sie ihr Gewicht mit einer Nachkommastelle ein (Hinweis: Trennzeichen muss der Punkt sein!): ')
+height = int(height)
+weight = float(weight)
 bmi=weight/(height*height)*10000
-print(bmi)
+print(f'Die Person namens {name} hat einen BMI von {bmi:.2f}')
