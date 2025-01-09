0ba486da02425db70939e78d80504249a1c5c1db
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-09 09:49:41 +0100

    Hinzufügen von Kommentaren

--- programme/Woche1/demo/bmi.py
+++ programme/Woche1/demo/bmi.py
@@ -1,5 +1,6 @@
-A1='Sawitzki'
-B1=183
-C1=75.3
-D1=C1/(B1*B1)*10000
+# Dieses Programm berechnet den BMI einer Person
+A1='Sawitzki' # A1 Name der Person
+B1=183 # B1 Körpergröße in cm
+C1=75.3 # C1 Gewicht in Kilo
+D1=C1/(B1*B1)*10000 # Berechnung des BMI
 print(D1)
