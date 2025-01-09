ea5ee3c3715cd8f4aa5ca6338398c491c5b3be79
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-09 11:55:45 +0100

    Einführen des formatierten Strings

--- programme/Woche1/demo/type_change.py
+++ programme/Woche1/demo/type_change.py
@@ -5,7 +5,6 @@ weight= '75.3'
 weight = float(weight)
 bmi = weight / (height*height)*10000
 
-# Herr Sawitzki hat einen BMI von 22.4
-bmi_string = str(bmi)
-message = 'Herr ' + name + ' hat einen BMI von ' + bmi_string
+# Herr Sawitzki hat einen BMI von 22.48
+message = f'Herr {name} hat einen BMI von {bmi:.2f}'
 print(message)
