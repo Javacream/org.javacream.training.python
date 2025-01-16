55b5076f59e82f243c478cc90c579fc8f7f9ace3
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-16 09:44:20 +0100

    Start Woche 2: Boolean und Einrückungen

---
+++ programme/Woche2/demo/boolean_scratch.py
@@ -0,0 +1,14 @@
+b1 = True
+b2 = False
+
+result = b1 or b2
+
+
+number1 = 42
+number2 = 99
+
+print(f'ist {number1} kleiner als {number2}: {number1 < number2}')
+
+print(True > False)
+
+print('a42' < 'b')
---
+++ programme/Woche2/demo/indentation_scratch.py
@@ -0,0 +1,13 @@
+condition = False
+
+if condition:
+    print('if-Bedingung ist erfüllt')
+print('done')
+
+
+if condition:
+    pass
+else:
+    pass
+
+print ('done')
