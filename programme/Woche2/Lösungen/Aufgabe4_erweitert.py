dcbab3c7b34a356b9a958ec03fdd0aca3fa38ce8
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-16 14:07:16 +0100

    ToDo Seite 43

--- programme/Woche2/Lösungen/Aufgabe4_erweitert.py
+++ programme/Woche2/Lösungen/Aufgabe4_erweitert.py
@@ -1,30 +1,36 @@
-start_number = input('Bitte geben Sie die Startzahl an: ')
-start_number = int(start_number)
-end_number = input('Bitte geben Sie die Endzahl an: ')
-end_number = int(end_number)
+try:
+    start_number = input('Bitte geben Sie die Startzahl an: ')
+    start_number = int(start_number)
+    end_number = input('Bitte geben Sie die Endzahl an: ')
+    end_number = int(end_number)
 
-first_divider = input('Bitte geben Sie den ersten Teiler an: ')
-first_divider = int(first_divider)
-second_divider = input('Bitte geben Sie den zweiten Teiler an: ')
-second_divider = int(second_divider)
+    first_divider = input('Bitte geben Sie den ersten Teiler an: ')
+    first_divider = int(first_divider)
+    second_divider = input('Bitte geben Sie den zweiten Teiler an: ')
+    second_divider = int(second_divider)
 
-number = start_number
-
-while number <= end_number:
-    dividable_by_first = False
-    dividable_by_second = False
-    if number % first_divider == 0:
-        dividable_by_first = True
-    if number % second_divider == 0:
-        dividable_by_second = True
-
-    if dividable_by_first and dividable_by_second:
-        print('FizzBuzz')
-    elif dividable_by_first:
-        print('Fizz')
-    elif dividable_by_second:
-        print('Buzz')
+    if start_number < end_number:
+        number = start_number
+        end = end_number
     else:
-        print(f'{number}')
-    number += 1    
+        number = end_number
+        end = start_number
+    while number <= end:
+        dividable_by_first = False
+        dividable_by_second = False
+        if number % first_divider == 0:
+            dividable_by_first = True
+        if number % second_divider == 0:
+            dividable_by_second = True
 
+        if dividable_by_first and dividable_by_second:
+            print('FizzBuzz')
+        elif dividable_by_first:
+            print('Fizz')
+        elif dividable_by_second:
+            print('Buzz')
+        else:
+            print(f'{number}')
+        number += 1   
+except Exception as e:
+    print(f'Fehleingabe {e}')
