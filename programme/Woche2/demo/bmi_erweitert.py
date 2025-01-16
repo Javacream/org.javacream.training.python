def6250ce11a90f9858d3ef953c129cf4d078b45
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-16 13:45:15 +0100

    Einführung von try-except

--- programme/Woche2/demo/bmi_erweitert.py
+++ programme/Woche2/demo/bmi_erweitert.py
@@ -4,17 +4,20 @@ while True:
     name=input('Bitte geben Sie ihren Namen ein: ')
     height=input('Bitte geben Sie ihre Körpergröße in Zentimetern ein: ')
     weight=input('Bitte geben Sie ihr Gewicht mit einer Nachkommastelle ein (Hinweis: Trennzeichen muss der Punkt sein!): ')
-    height = int(height)
-    weight = float(weight)
-    bmi=weight/(height*height)*10000
-    if bmi < UNDERWEIGHT_BMI_LIMIT:
-        weight_category = "untergewichtig"
-    elif bmi < OVERWEIGHT_BMI_LIMIT:
-         weight_category = "normalgewichtig"
-    else:
-        weight_category = "übergewichtig"        
-    message = f'Die Person namens {name} ist {weight_category}'
-    print(message)
+    try:
+        height = int(height)
+        weight = float(weight)
+        bmi=weight/(height*height)*10000
+        if bmi < UNDERWEIGHT_BMI_LIMIT:
+            weight_category = "untergewichtig"
+        elif bmi < OVERWEIGHT_BMI_LIMIT:
+            weight_category = "normalgewichtig"
+        else:
+            weight_category = "übergewichtig"        
+        message = f'Die Person namens {name} ist {weight_category}'
+        print(message)
+    except Exception as e:
+        print(f'Fehleingabe, Grund {e}')    
     again = input("Nochmal? 'n' für Abbruch: ")
     if again == 'n':
         break
