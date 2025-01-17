e63b49378bc8c805c1122539312400c6044819a9
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-17 09:24:52 +0100

    Abschlussübung Teil 1: Analyse und Identzifikation von Modulen inklusive Tests

---
+++ programme/Woche10/demo/app.py
@@ -0,0 +1,5 @@
+# Skript, das die Sequenen der Anwendung komplett "von oben bis unten" implementiert
+
+# Wertung: Kann / Wird funktionieren, Qualität eher mangelhaft
+
+# Wartbarkeit und Wiederverwendbarkeit ist schlecht, Copy&Paste, Code Redundanzen, ...
