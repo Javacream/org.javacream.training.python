d2e4707d170e8fa1564156b847392af5a3ff8ba9
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-24 11:22:57 +0100

    2Lösung zu Aufgabe 2

---
+++ programme/Woche3/Lösungen/Aufgabe2.py
@@ -0,0 +1,9 @@
+numbers_list = [1, 3, -9, 45, 22, 3, 22, -33]
+reverted_list = numbers_list[::-1]
+sum_of_list = sum(numbers_list)
+max_of_list = max(numbers_list)
+min_of_list = min(numbers_list)
+
+numbers_set = set(numbers_list)
+numbers_list_without_duplicates = list(numbers_set)
+print(reverted_list, sum_of_list, max_of_list, min_of_list, numbers_list_without_duplicates)
