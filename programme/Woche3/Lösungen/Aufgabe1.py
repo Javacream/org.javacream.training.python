a36c6ba0fc3e4d2437ce5c6f6f84536f30b768ad
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-24 13:39:39 +0100

    Aufgabe 1, Part 1

---
+++ programme/Woche3/Lösungen/Aufgabe1.py
@@ -0,0 +1,22 @@
+name = "Hugo"
+reversed_name = name[::-1]
+print(reversed_name)
+text = "Schreiben Sie Ein Python-Programm, das alle Vokale in einem gegebenen String zählt."
+
+number_of_es = text.count('e')
+print(number_of_es)
+
+number_of_is = text.count('i')
+print(number_of_is),
+
+#...
+
+vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', 'ä', 'ö', 'ü', 'Ä', 'Ö', 'Ü')
+
+vowel_counter = 0
+for character in text:
+    if character in vowels:
+        vowel_counter += 1
+print(vowel_counter)
+
+
---
+++ programme/Woche3/demo/string_scratch.py
@@ -0,0 +1,8 @@
+my_str = "ABC"
+print(len(my_str))
+print(my_str[1])
+
+for element in my_str:
+    print(element)
+
+
