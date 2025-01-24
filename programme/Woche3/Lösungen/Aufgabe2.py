1fb333a9b13cc14a2d4e99db1ba201df7bfdb35c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-24 11:49:05 +0100

    reversed hinzugefügt

--- programme/Woche3/Lösungen/Aufgabe2.py
+++ programme/Woche3/Lösungen/Aufgabe2.py
@@ -1,5 +1,6 @@
 numbers_list = [1, 3, -9, 45, 22, 3, 22, -33]
 reverted_list = numbers_list[::-1]
+reverted_list = reversed()
 sum_of_list = sum(numbers_list)
 max_of_list = max(numbers_list)
 min_of_list = min(numbers_list)
