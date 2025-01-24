c0c373b2cf048a82c835a3459cf4c285a08872ce
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-24 09:35:35 +0100

    Demonstration des Indexzugriffs

---
+++ programme/Woche3/demo/scratch.py
@@ -0,0 +1,18 @@
+number = 42
+
+number = number + 2
+
+# number('Hugo') not callable, also keine Funktion
+# number[1] not subscriptable, also keine Collection
+
+print("Hello")
+# print = print + 2 Ich kann Funktion nichts addieren
+
+# print[1] # not subscriptable, also keine Collection
+
+person1 = ['Sawitzki', 'Rainer']
+# person1 = person1 + 2 Zu einer Liste kann keine Zahl addiert werden
+# person1('Hugo') not callable, also keine Funktion
+
+second_element = person1[1]
+print(second_element)
