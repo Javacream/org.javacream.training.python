883e2dc1c993d2cfa3e0617a250700f3290dd551
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-01-27 14:56:53 +0100

    ToDo Slide 30, bessere fachliche Lesbarkeit durch Variablen

--- bmi.py
+++ bmi.py
@@ -1,14 +1,17 @@
+LOW_WEIGHT_BMI = 18
+MAX_NORMAL_BMI = 25
+MAX_HIGH_WEIGHT_BMI = 30
 name = input('Bitte geben Sie ihren Namen an: ')
 height = input('Bitte geben Sie ihre Körpergröße in cm an (Nur Zahlen!): ')
 weight = input('Bitte geben Sie ihr Körpergewicht in kg an (Nur Zahlen, Komma-Trenner ist der Punkt .): ')
 height = int(height)
 weight = float(weight)
 body_mass_index = weight/(height*height)* 100 * 100
-if body_mass_index < 18:
+if body_mass_index < LOW_WEIGHT_BMI:
     bmi_category = 'untergewichtig'
-elif body_mass_index < 25:
+elif body_mass_index < MAX_NORMAL_BMI:
     bmi_category = 'normalgewichtig'    
-elif body_mass_index < 30:
+elif body_mass_index < MAX_HIGH_WEIGHT_BMI:
     bmi_category = 'übergewichtig'    
 else:
     bmi_category = "fettleibig"
