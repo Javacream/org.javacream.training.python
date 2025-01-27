2ae97df7322ead4182101bdb01d85aedc2b3b909
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-01-27 14:54:55 +0100

    ToDo Slide 30

--- bmi.py
+++ bmi.py
@@ -4,4 +4,12 @@ weight = input('Bitte geben Sie ihr Körpergewicht in kg an (Nur Zahlen, Komma-T
 height = int(height)
 weight = float(weight)
 body_mass_index = weight/(height*height)* 100 * 100
-print(f'Die Person {name} hat mit einer Körpergröße von {height}cm und einem Gewicht von {weight}kg einen BMI von {body_mass_index}')
+if body_mass_index < 18:
+    bmi_category = 'untergewichtig'
+elif body_mass_index < 25:
+    bmi_category = 'normalgewichtig'    
+elif body_mass_index < 30:
+    bmi_category = 'übergewichtig'    
+else:
+    bmi_category = "fettleibig"
+print(f'Die Person {name} hat mit einer Körpergröße von {height}cm und einem Gewicht von {weight}kg einen BMI von {body_mass_index} ist {bmi_category}')
