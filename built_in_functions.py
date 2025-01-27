2de6a4634ba50a0bb748603127ded91fcfaec0e2
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-01-27 12:06:54 +0100

    Operatoren, print, input

---
+++ built_in_functions.py
@@ -0,0 +1,10 @@
+print(42)
+# print 42 syntaktischer Fehler, runde Klammern vergessen
+# print(42 schließende Klammer fehlt
+print()
+print
+print()
+print('done')
+
+name = input("Bitte gebe einen Namen ein: ")
+print(f"Hallo {name}!")
---
+++ operators.py
@@ -0,0 +1,14 @@
+number1 = 21
+number2 = 2
+result = number1 + number2
+print(result)
+
+number3 = "21"
+number4 = "2"
+result = number3 + number4
+print(result)
+
+result = number1 * number4
+print(result)
+
+result = number1 + number3
