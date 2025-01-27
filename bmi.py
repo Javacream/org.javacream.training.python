69d2f7d208ab6149a478643145a2d1cb27f9e9d9
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-01-27 14:00:45 +0100

    ToDo Slide 25

--- bmi.py
+++ bmi.py
@@ -1,5 +1,7 @@
-name = 'Sawitzki'
-height = 183
-weight = 76.3
-body_mass_index = height/(weight*weight)* 100 * 100
-print(body_mass_index)
+name = input('Bitte geben Sie ihren Namen an: ')
+height = input('Bitte geben Sie ihre Körpergröße in cm an (Nur Zahlen!): ')
+weight = input('Bitte geben Sie ihr Körpergewicht in kg an (Nur Zahlen, Komma-Trenner ist der Punkt .): ')
+height = int(height)
+weight = float(weight)
+body_mass_index = weight/(height*height)* 100 * 100
+print(f'Die Person {name} hat mit einer Körpergröße von {height}cm und einem Gewicht von {weight}kg einen BMI von {body_mass_index}')
