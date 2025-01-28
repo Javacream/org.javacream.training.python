180c3d15220f1cbf7ddc6668445468567a9168b5
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-28 09:56:52 +0100

    dict demo

--- dict_demo.py
+++ dict_demo.py
@@ -3,7 +3,7 @@ p2 = {'name': 'Musterperson', 'height': 176}
 print(type(p1))
 print(f'Der Name von p1 ist {p1['name']}')
 print(f'Die Größe von p2 ist {p2['height']}')
-# print(p1[2]) Index out of range
+# print(p1['weight']) # KeyError: weight ist kein bekannter Schlüssel
 p1['height'] = 181
 print(f'Die Größe von p1 ist {p1['height']}')
 print('done')
