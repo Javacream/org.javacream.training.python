3524e0d7d05c1c55aa073f0df9a33404a33c2557
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-28 16:07:48 +0100

    Schritt 4: Duplikatserkennung

--- shopping_list.py
+++ shopping_list.py
@@ -2,4 +2,5 @@ filename = './shopping_list.txt'
 with open(filename, encoding='utf-8') as file:
     rows = file.readlines()
     cleaned_rows = [row[:-1] for row in rows if not row == '\n']
-    print(cleaned_rows)
+    shopping_set = set(cleaned_rows)
+    print(shopping_set)
