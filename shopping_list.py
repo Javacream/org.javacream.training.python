91362da70588f9a3d7b040e39516b751b8e073b0
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-28 15:59:46 +0100

    Schritt 3: Entferne \n für jede Zeile, Holzhammer

--- shopping_list.py
+++ shopping_list.py
@@ -1,4 +1,10 @@
 filename = './shopping_list.txt' 
 with open(filename, encoding='utf-8') as file:
     rows = file.readlines()
-    print(rows)
+    rows_without_eol = []
+    for row in rows:
+        if not row == '\n':
+            row_length = len(row)
+            row_without_eol = row[0:(row_length-1)]
+            rows_without_eol.append(row_without_eol)
+    print(rows_without_eol)
