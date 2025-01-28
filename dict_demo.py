ccd3e0df71a36cc9a6ef0c9158f59481319e511f
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-28 10:11:34 +0100

    Einheitliche Anführungszeichen

--- dict_demo.py
+++ dict_demo.py
@@ -1,9 +1,9 @@
 p1 = {'name': 'Sawitzki', 'height': 183}
 p2 = {'name': 'Musterperson', 'height': 176}
 print(type(p1))
-print(f'Der Name von p1 ist {p1['name']}')
-print(f'Die Größe von p2 ist {p2['height']}')
+print(f"Der Name von p1 ist {p1['name']}")
+print(f"Die Größe von p2 ist {p2['height']}")
 # print(p1['weight']) # KeyError: weight ist kein bekannter Schlüssel
 p1['height'] = 181
-print(f'Die Größe von p1 ist {p1['height']}')
+print(f"Die Größe von p1 ist {p1['height']}")
 print('done')
