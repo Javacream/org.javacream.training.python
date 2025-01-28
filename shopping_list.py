ed2649a5cb415edc8336a796863b411f51cac128
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-28 16:04:31 +0100

    Schritt 3, Noch kompakter mit Comprehension

--- shopping_list.py
+++ shopping_list.py
@@ -1,9 +1,5 @@
 filename = './shopping_list.txt' 
 with open(filename, encoding='utf-8') as file:
     rows = file.readlines()
-    rows_without_eol = []
-    for row in rows:
-        if not row == '\n':
-            row_without_eol = row[:-1]
-            rows_without_eol.append(row_without_eol)
-    print(rows_without_eol)
+    cleaned_rows = [row[:-1] for row in rows if not row == '\n']
+    print(cleaned_rows)
