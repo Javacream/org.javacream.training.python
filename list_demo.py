4480694fc1fb820b3cf8b980b2c6069ddc2eed73
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-28 10:15:22 +0100

    Bessere Beispiel für eine Liste

--- list_demo.py
+++ list_demo.py
@@ -1,9 +1,5 @@
-p1 = ['Sawitzki', 183]
-p2 = ['Musterperson', 176]
-print(type(p1))
-print(f'Der Name von p1 ist {p1[0]}')
-print(f'Die Größe von p2 ist {p2[1]}')
-# print(p1[2]) Index out of range
-p1[1] = 181
-print(f'Die Größe von p1 ist {p1[1]}')
+todos = ['Essen', 'Trinken', 'Schlafen', 'Essen']
+print(type(todos))
+print(f'Das erste, was ich tun muss, ist {todos[0]}')
+print(f'Die zweite ist {todos[1]}')
 print('done')
