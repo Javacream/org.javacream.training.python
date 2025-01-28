dd61ad2dd36bd3bcebc5efc290a039316bfa0716
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-28 16:01:41 +0100

    Schritt 3: Kompaktierung mit Slice

--- shopping_list.py
+++ shopping_list.py
@@ -4,7 +4,6 @@ with open(filename, encoding='utf-8') as file:
     rows_without_eol = []
     for row in rows:
         if not row == '\n':
-            row_length = len(row)
-            row_without_eol = row[0:(row_length-1)]
+            row_without_eol = row[:-1]
             rows_without_eol.append(row_without_eol)
     print(rows_without_eol)
