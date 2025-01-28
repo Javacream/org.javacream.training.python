424f691ff511fc5566b3a04c15adfb050371b770
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-28 10:23:06 +0100

    set demo

---
+++ set_demo.py
@@ -0,0 +1,6 @@
+basket = {'Eier', 'Brot', 'Käse', 'Eier', "Gemüse"}
+print(type(basket))
+#print(f'Das erste, was ich einkaufen muss, ist {basket[0]}')
+#print(f'Das zweite ist {basket[1]}')
+print(basket)
+print('done')
