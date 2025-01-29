b36e14505d9968b6b5ede9ad47a5ee1f6d5e8e4d
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-29 09:36:57 +0100

    Verbose shopping list

--- shopping_list.py
+++ shopping_list.py
@@ -1,6 +1,18 @@
 filename = './shopping_list.txt' 
 with open(filename, encoding='utf-8') as file:
     rows = file.readlines()
-    cleaned_rows = [row[:-1] for row in rows if not row == '\n']
+    #cleaned_rows = [row[:-1] for row in rows if not row == '\n']
+    cleaned_rows = []
+    for row in rows:
+        if not row == '\n':
+            row_length = len(row)
+            if row[row_length-1] == '\n':                
+                shorted_row = row[0:row_length-1]
+                cleaned_rows.append(shorted_row)
+            else:
+                cleaned_rows.append(row)    
+        else:
+            print('Leerzeile entdeckt!')
+    
     shopping_set = set(cleaned_rows)
     print(shopping_set)
