6a41b37a936acb8dc492cd14af9a3f85973fde10
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-29 10:51:26 +0100

    Funktionen Teil 1: Struktur

---
+++ functions_scratch.py
@@ -0,0 +1,14 @@
+def step1():
+    print('step1 wurde aufgerufen')
+
+def step3():
+    print('step3 wurde aufgerufen')
+
+def step2():
+    print('step2 wurde aufgerufen')
+
+def main():
+    step1()
+    step2()
+    step3()
+main()
