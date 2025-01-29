10dd55d4ba0570952a9a3ee181be497be1194e04
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-29 14:08:22 +0100

    Umgang mit unbekanntem Status durch else

--- greeter_app.py
+++ greeter_app.py
@@ -3,11 +3,13 @@ def greet(name, status):
         greeting = f'Hallo, ich bin {name}'
     elif status == "formal":
         greeting = f'Guten Tag, mein Name ist {name}.'
+    else:
+        greeting = name    
     return greeting
 
 def main():
     test_name = 'Hugo'
-    test_status = 'friendly'
+    test_status = 'formal'
     greeting = greet(test_name, test_status)
     print(greeting)
 
