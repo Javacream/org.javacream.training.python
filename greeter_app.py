89a467b73b071f0e40286f126c183da82ecfebeb
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-29 14:04:24 +0100

    2 fachliche Änderung (friendly|formal), Variante 1

--- greeter_app.py
+++ greeter_app.py
@@ -1,10 +1,14 @@
-def greet(name):
-    greeting = f'Hallo, ich bin {name}'
+def greet(name, status):
+    if status == 'friendly':
+        greeting = f'Hallo, ich bin {name}'
+    elif status == "formal":
+        greeting = f'Guten Tag, mein Name ist {name}.'
     return greeting
 
 def main():
     test_name = 'Hugo'
-    greeting = greet(test_name)
+    test_status = 'friendly'
+    greeting = greet(test_name, test_status)
     print(greeting)
 
 main()
