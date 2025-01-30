8616d409199cbe323bc8b84635877f1b2a772b92
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-30 13:56:16 +0100

    Lösung ToDo Slide 78

--- programme/Woche4/demo/shopping.py
+++ programme/Woche4/demo/shopping.py
@@ -1,5 +1,13 @@
 def create_unique_items(items :list[str]):
-    pass
+    return set(items)
 
 def create_shopping_collection(items :list[str]):
-    pass
+    items_and_number = dict()
+    for item in items:
+        number = items_and_number.get(item)
+        if number == None:
+            number = 1
+        else:
+            number += 1
+        items_and_number[item] = number
+    return [f'{item} -> {items_and_number[item]}' for item in items_and_number]    
--- programme/Woche4/demo/shopping_app.py
+++ programme/Woche4/demo/shopping_app.py
@@ -1,11 +1,14 @@
 import file_util
 import shopping
 def main():
-    raw_data = file_util.read_raw('shopping_list.txt')
+    infile = 'programme/Woche4/demo/shopping_list.txt'
+    outfile = 'programme/Woche4/demo/shopping.txt'
+    
+    raw_data = file_util.read_raw(infile)
     data = file_util.clean_data(raw_data)
     unique_items = shopping.create_unique_items(data)
     shopping_info = shopping.create_shopping_collection(data)
     print(unique_items)
-    file_util.write_result(shopping_info, 'shopping.txt')
+    file_util.write_result(shopping_info, outfile)
 if __name__ == '__main__':
     main()
