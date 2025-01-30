cc593d421f873432b37464548d9c4bb947107e1a
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-30 11:03:45 +0100

    Kapselung, 'scoped variables'

---
+++ programme/Woche4/functions_scratch.py
@@ -0,0 +1,17 @@
+def fn1():
+    fn1_var = 'in fn1'
+    print(f'var = {fn1_var}')
+
+def fn2():
+    fn2_var = 'in fn2'
+    print(main_var)
+    print(f'var = {fn2_var}')
+
+def main():
+    main_var = 'Hugo'
+    main_var2 = ['A', 'B', 'C']
+    fn1()
+    fn2()
+
+if __name__ == '__main__':
+    main()
