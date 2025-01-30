5068ed73c21e5cfdf70d8e6b17516c912bd247e0
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-01-30 10:22:56 +0100

    Module in Python

---
+++ programme/Woche4/modul_scratch.py
@@ -0,0 +1,13 @@
+from math import sqrt
+import random
+import os
+import datetime
+def main():
+    number1 = 4
+    number2 = 40
+    print(sqrt(number1))
+    print(random.random())
+    print(os.listdir())
+    print(datetime.datetime.now())
+if __name__ == '__main__':
+    main()
