9eda5e8254cca762305c4f1deefa1449da5ae451
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-07 10:24:13 +0100

    fix Typo

--- programme/Woche5/demo/people.py
+++ programme/Woche5/demo/people.py
@@ -1,21 +1,22 @@
 class Person:
-    def __init__(self, lastname, firstname, adress): # Konstruktor
+    def __init__(self, lastname, firstname, address): # Konstruktor
         self.lastname = lastname
         self.firstname = firstname
-        self.adress = adress
+        self.address = address
     def say_hello(self):
         greeting = f'Hallo, mein Name ist {self.firstname} {self.lastname}'
         return greeting
 
-class Adress:
+class Address:
     def __init__(self, city, street):
         self.city = city
         self.street = street
 
 def main():
-    address_in_munich = Adress("München", "Marienplatz")
-    person1 = Person("Sawitzki", "Rainer", address_in_munich) # Implizit: Ein leeres Objekt wird erzeugt und als erster Parameter ergänzt
-    person2 = Person("Musterfrau", "Hannah", address_in_munich) # Implizit: Ein leeres Objekt wird erzeugt und als erster Parameter ergänzt
+    adress_in_munich = Address("München", "Marienplatz")
+    person1 = Person("Sawitzki", "Rainer", adress_in_munich) # Implizit: Ein leeres Objekt wird erzeugt und als erster Parameter ergänzt
+    person2 = Person("Musterfrau", "Hannah", adress_in_munich) # Implizit: Ein leeres Objekt wird erzeugt und als erster Parameter ergänzt
+    person1.adress = adress_in_munich
     print(type(person1))
     print(person1.say_hello()) # Implizit wird person1 in der Methode zum ersten Parameter self
     print(person2.say_hello())
