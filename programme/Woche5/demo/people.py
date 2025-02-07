e59ff36f9a6a26cfe7d30ef82a7a8f8d4e380494
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-07 11:25:27 +0100

    Worker

--- programme/Woche5/demo/people.py
+++ programme/Woche5/demo/people.py
@@ -1,5 +1,5 @@
 class Person:
-    def __init__(self, lastname, firstname, address): # Konstruktor
+    def __init__(self, lastname, firstname, address):
         self.lastname = lastname
         self.firstname = firstname
         self.address = address
@@ -14,23 +14,30 @@ class Address:
 
 class Student(Person):
     def __init__(self, lastname, firstname, address, university):
-        #self.lastname = lastname
-        #self.firstname = firstname
-        #self.address = address
-        super().__init__(lastname, firstname, address)
         self.university = university
+        super().__init__(lastname, firstname, address)
     def study(self):
         print(f'ich studiere an {self.university}')    
+class Worker(Person):
+    def __init__(self, lastname, firstname, address, company):
+        super().__init__(lastname, firstname, address)
+        self.company = company
+    def work(self):
+        print(f'ich arbeite bei {self.company}')    
 
 def main():
     address_in_munich = Address("München", "Marienplatz")
-    person1 = Person("Sawitzki", "Rainer", address_in_munich) # Implizit: Ein leeres Objekt wird erzeugt und als erster Parameter ergänzt
-    person2 = Person("Musterfrau", "Hannah", address_in_munich) # Implizit: Ein leeres Objekt wird erzeugt und als erster Parameter ergänzt
+    address_in_berlin = Address("Berlin", "Alexanderplatz")
+    address_in_stuttgart = Address("Stuttgart", "Schlossplatz")
+    person1 = Person("Sawitzki", "Rainer", address_in_munich) 
+    person2 = Person("Musterfrau", "Hannah", address_in_berlin)
     student1 = Student('Einstein', 'Albert', address_in_munich, 'TU')
+    worker1 = Worker('Schufter', 'Andrea', address_in_stuttgart, 'Cegos')
     student1.study()
+    worker1.work()
     print(student1.say_hello())
-    print(type(person1))
-    print(person1.say_hello()) # Implizit wird person1 in der Methode zum ersten Parameter self
+    print(worker1.say_hello())
+    print(person1.say_hello())
     print(person2.say_hello())
 if __name__ == '__main__':
     main()
