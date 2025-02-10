f17f99f958a1657de8f616c266f64249a9844967
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-10 14:41:55 +0100

    Eine Fehlerbehandlung, die einen Anwender wahnsinnig machen kann

--- Tag4/errors_demo.py
+++ Tag4/errors_demo.py
@@ -2,12 +2,11 @@
 def main():
     while True:
         input_string = input('Nummer angeben: ')
-        if input_string == 'x':
-            break
         try:
             input_string = int(input_string)
             print(input_string * 2)
+            break
         except:
-            print(f'{input_string} war keine Zahl ')    
+            print(f'{input_string} war keine Zahl, bitte nochmal versuchen ')    
 if __name__ == '__main__':
     main()
