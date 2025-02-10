b85e7f2200d82642a3d401a963e28373ac37552d
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-10 09:34:22 +0100

    from module import

--- Tag4/bmi.app.py
+++ Tag4/bmi.app.py
@@ -1,4 +1,4 @@
-import bmi as bmi_module
+from bmi import calculate_bmi
 def main():
     person_data = [
         [183, 75.6],
@@ -6,7 +6,7 @@ def main():
         [166, 66.6]
     ]
     for person in person_data:
-        bmi = bmi_module.calculate_bmi(person[0], person[1])
+        bmi = calculate_bmi(person[0], person[1])
         print(bmi)
 
 main()
