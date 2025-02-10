279a9e7041829ef902638b143dc89f794c22d386
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-10 14:00:14 +0100

    Prüfe, ob Eingangsdatei existiert

--- Tag4/bmi/people_data.py
+++ Tag4/bmi/people_data.py
@@ -1,13 +1,18 @@
+import os
 def read_person_data(path):
-    with open(path, encoding='utf-8') as file:
-        rows = file.readlines()
-        cleaned_rows = [row[:-1] for row in rows if not row == '\n']
-        people = list()
-        for row in cleaned_rows:
-            person_data = row.split(',')
-            person_dict = {'name': person_data[0], 'weight': float(person_data[1]), 'height': float(person_data[2])}
-            people.append(person_dict)
-        return people
+    people = list()
+    if os.path.isfile(path):
+        with open(path, encoding='utf-8') as file:
+            rows = file.readlines()
+            cleaned_rows = [row[:-1] for row in rows if not row == '\n']
+            for row in cleaned_rows:
+                person_data = row.split(',')
+                person_dict = {'name': person_data[0], 'weight': float(person_data[1]), 'height': float(person_data[2])}
+                people.append(person_dict)
+            return people
+    else:
+        print(f"Datei {path} nicht gefunden")
+    return people
 def write_person_data(path, result_list):
     with open(path, 'wt', encoding='utf-8') as file:
         file.writelines([f'{result}\n' for result in result_list])
