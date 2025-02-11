3f6b5a8426e7c71d5d89ebacf67f1734e068723b
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-11 09:56:42 +0100

    Dunder-eq

--- Tag5/person.py
+++ Tag5/person.py
@@ -17,14 +17,24 @@ class Address:
         self.street = street
     def __repr__(self):
         return f'Address: city={self.city}, street={self.street}'
+    def __eq__(self, other):
+        if isinstance(other, Address):
+           return (self.city == other.city) and (self.street == other.street)
+        else:
+            return False 
 
 def main():
     address_in_munich = Address('München', 'Marienplatz')
     address_in_berlin = Address('Berlin', 'Alexanderplatz')
+    address_in_munich2 = Address('München', 'Marienplatz')
     person1 = Person('Sawitzki', 'Rainer', 75.3, 183, address_in_munich)
     person2 = Person('Musterfrau', 'Andrea', 55.3, 158, address_in_berlin)
-    print(address_in_munich)
-    print(person1)
+    person3 = Person('Musterfrau', 'Andrea', 55.3, 158, address_in_berlin)
+    print(person1 == person2)
+    print(person2 == person3)
+    addresses_are_equal = (address_in_munich == address_in_berlin)
+    print(addresses_are_equal)
+    print(address_in_munich == address_in_munich2)
 
 
 if __name__ == '__main__':
