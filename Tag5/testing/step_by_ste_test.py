533894ad31871b07b8597d9539b5a5c0c3a32e53
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-11 12:07:21 +0100

    Gruppierung in einen Testfall

--- Tag5/testing/step_by_ste_test.py
+++ Tag5/testing/step_by_ste_test.py
@@ -1,5 +1,9 @@
-value = "Hello"
-expected_length_of_list = 1
-my_list = []
-my_list.append(value)
-expected_length_of_list == len(my_list) 
+import unittest
+
+class ListTests(unittest.TestCase):
+    def test_list_append(self):
+        value = "Hello"
+        expected_length_of_list = 1
+        my_list = []
+        my_list.append(value)
+        self.assertEqual(expected_length_of_list, len(my_list)) 
