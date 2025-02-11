b3a4dead5c8236fbf8a07727c1a3057b56b7f3ce
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-11 10:12:44 +0100

    Addressen sind auch mit unterschiedlicher Schreibweise gleich

--- Tag5/person.py
+++ Tag5/person.py
@@ -19,23 +19,19 @@ class Address:
         return f'Address: city={self.city}, street={self.street}'
     def __eq__(self, other):
         if isinstance(other, Address):
-           return (self.city == other.city) and (self.street == other.street)
+           return (self.city.lower() == other.city.lower()) and (self.street.lower() == other.street.lower())
         else:
             return False 
 
 def main():
     address_in_munich = Address('München', 'Marienplatz')
     address_in_berlin = Address('Berlin', 'Alexanderplatz')
-    address_in_munich2 = Address('München', 'Marienplatz')
+    address_in_munich2 = Address('München', 'MARIENPLATZ')
     person1 = Person('Sawitzki', 'Rainer', 75.3, 183, address_in_munich)
     person2 = Person('Musterfrau', 'Andrea', 55.3, 158, address_in_berlin)
     person3 = Person('Musterfrau', 'Andrea', 55.3, 158, address_in_berlin)
-    print(person1 == person2)
-    print(person2 == person3)
-    addresses_are_equal = (address_in_munich == address_in_berlin)
-    print(addresses_are_equal)
-    print(address_in_munich == address_in_munich2)
 
+    print(address_in_munich2 == address_in_munich)
 
 if __name__ == '__main__':
     main()
