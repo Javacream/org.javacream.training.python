69bf8310d2f79bc66325a342a350ad29ca28f1d2
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-11 15:21:19 +0100

    Interaktive Anwendung

---
+++ Tag5/testing/bmi/interactive_bmi_app.py
@@ -0,0 +1,23 @@
+import bmi as bmi_module 
+import people_data
+
+def main():
+    indir = 'Tag5/testing/bmi/data'
+    outdir = 'Tag5/testing/bmi/result'
+
+    infilename = input('Eingabedatei aus data-Verzeichnis: ')
+    outfilename = input('Ausgabedatei in result-Verzeichnis: ')
+    people_list = people_data.read_person_data(f'{indir}/{infilename}')
+    people_result = list()
+    for person in people_list:
+        name = person['name']
+        height = person['height']
+        weight = person['weight']
+        bmi = bmi_module.calculate_bmi(height, weight)
+        bmi_category = bmi_module.bmi_category_for(bmi)
+        people_result.append(f'{name} ist {bmi_category}')
+    people_data.write_person_data(f'{outdir}/{outfilename}', people_result)    
+if __name__ == '__main__':
+    main()
+
+
