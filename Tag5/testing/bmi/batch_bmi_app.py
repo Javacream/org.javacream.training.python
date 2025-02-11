7593c173560b672ecc13efd1be12a5675d244168
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-11 15:22:18 +0100

    Batch Application

---
+++ Tag5/testing/bmi/batch_bmi_app.py
@@ -0,0 +1,22 @@
+import bmi as bmi_module 
+import people_data
+import os
+def main():
+    indir = 'Tag5/testing/bmi/data'
+    outdir = 'Tag5/testing/bmi/result'
+    infiles = os.listdir(indir)
+    for infilename in infiles:
+        people_list = people_data.read_person_data(f'{indir}/{infilename}')
+        people_result = list()
+        for person in people_list:
+            name = person['name']
+            height = person['height']
+            weight = person['weight']
+            bmi = bmi_module.calculate_bmi(height, weight)
+            bmi_category = bmi_module.bmi_category_for(bmi)
+            people_result.append(f'{name} ist {bmi_category}')
+        people_data.write_person_data(f'{outdir}/{infilename}.txt', people_result)    
+if __name__ == '__main__':
+    main()
+
+
