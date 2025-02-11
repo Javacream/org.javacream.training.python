2a3bc2586c67e714140ee6f9e9aa9e8d3f6e65ac
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-11 15:44:35 +0100

    Gruppierung nach Geschlecht

--- Tag5/testing/bmi/batch_bmi_app.py
+++ Tag5/testing/bmi/batch_bmi_app.py
@@ -6,21 +6,22 @@ def main():
     outdir = 'Tag5/testing/bmi/result'
     infiles = os.listdir(indir)
     data_counter = 0
-    people_result = list()
     people_in_categories = dict()
     people_in_categories['untergewichtig'] = []
     people_in_categories['normalgewichtig'] = []
     people_in_categories['übergewichtig'] = []
     people_in_categories['fettleibig'] = []
+    people_by_gender = {'m':[], 'w': [], 'd':[]}
     for infilename in infiles:
         people_list = people_data.read_person_data(f'{indir}/{infilename}')
         for person in people_list:
             bmi = bmi_module.calculate_bmi(person.height, person.weight)
             bmi_category = bmi_module.bmi_category_for(bmi)
             people_in_categories[bmi_category].append(person)
-            people_result.append(f'{person.name} ist {bmi_category}')
+            people_by_gender[person.gender].append(f'{person.name} ist {bmi_category}')
             data_counter += 1
-        people_data.write_person_data(f'{outdir}/{infilename}.txt', people_result)    
+    for gender in people_by_gender:
+        people_data.write_person_data(f'{outdir}/{infilename}.{gender}.txt', people_by_gender[gender])    
     print(f'Anzahl gelesener Personendaten: {data_counter}')
     for category in people_in_categories:
         print(f'In der Kategorie {category} sind {len(people_in_categories[category])} Personen')
