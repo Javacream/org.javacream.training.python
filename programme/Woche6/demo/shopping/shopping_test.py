5e85b632206aa5679d7bea8b4b3b44d97ce6b0c9
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-13 10:20:53 +0100

    Definition der Tests pro Funktion

--- programme/Woche6/demo/shopping/shopping_test.py
+++ programme/Woche6/demo/shopping/shopping_test.py
@@ -3,10 +3,18 @@ import file_util
 import shopping
 
 class FileUtilTests(unittest.TestCase):
-    pass
-
+    def test_read_raw(self):
+        self.fail('ToDo')
+    def test_clean_data(self):
+        self.fail('ToDo')
+    def test_write_result(self):
+        self.fail('ToDo')
 class ShoppingTests(unittest.TestCase):
-    pass
+    def test_create_unique_items(self):
+        self.fail('ToDo')
+    def test_create_shopping_collection(self):
+        self.fail('ToDo')
+
 
 if __name__ == '__main__':
     unittest.main()
