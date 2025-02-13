ff234c29a120095d3d614089364244b833d2a8e4
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-13 10:14:36 +0100

    Import und Testklassen

--- programme/Woche6/demo/shopping/shopping_test.py
+++ programme/Woche6/demo/shopping/shopping_test.py
@@ -1,5 +1,12 @@
 import unittest
+import file_util
+import shopping
 
+class FileUtilTests(unittest.TestCase):
+    pass
+
+class ShoppingTests(unittest.TestCase):
+    pass
 
 if __name__ == '__main__':
     unittest.main()
