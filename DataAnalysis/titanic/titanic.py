e09005664be46f7b250d97d8f4e83630c12030db
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-20 15:53:38 +0100

    titanic.py

--- DataAnalysis/titanic/titanic.py
+++ DataAnalysis/titanic/titanic.py
@@ -1,76 +1,81 @@
 import pandas as pd
-
-titanic_part1_df = pd.read_csv('./DataAnalysis/titanic/titanic.csv')
-titanic_part1_df.head()
-
-titanic_part2_incomplete_df = pd.read_csv('./DataAnalysis/titanic/titanic_part2_incomplete.csv')
-titanic_part2_incomplete_df.head()
-
-titanic_part2_survival_df = pd.read_csv('./DataAnalysis/titanic/titanic_part2_survival.csv')
-titanic_part2_survival_df.head()
-
-titanic_part2_df = pd.merge(titanic_part2_incomplete_df, titanic_part2_survival_df, how='inner', on='PassengerId')
-titanic_part2_df.head()
-
-df = pd.concat([titanic_part1_df, titanic_part2_df], ignore_index=True)
-df
-
-df.shape
-
-df.info()
-
-df .describe()
-
-df.columns
-
-df['Cabin'].unique()
-
-df.drop('PassengerId', axis=1, inplace=True)
-df.drop('Ticket', axis=1, inplace=True)
-
-
-df.loc[(df['Sex'] == 'female') & (df['Age'].isnull())]
-
-male_mean_age = df[df['Sex'] == 'male']['Age'].mean()
-female_mean_age = df[df['Sex'] == 'female']['Age'].mean()
-
-df.loc[(df['Sex'] == 'female') & (df['Age'].isnull()), 'Age'] = female_mean_age
-df.loc[(df['Sex'] == 'male') & (df['Age'].isnull()), 'Age'] = male_mean_age
-
-
-df.loc[df['Embarked'].isnull(), 'Embarked'] = 'U'
-
-
-
-surviced_categorical_series = pd.Categorical(df['Survived'])
-surviced_categorical_series = surviced_categorical_series.rename_categories(['died', 'survived'])
-df['Survived'] = surviced_categorical_series
-df.info()
-
-df['Sex'] = pd.Categorical(df['Sex'])
-df['Pclass'] = pd.Categorical(df['Pclass']).rename_categories({1: 'First', 2: 'Second', 3: 'Third'})
-stations = {'S': 'Southhampton', 'C': 'Cherbourg', 'Q': 'Queenstown', 'U': 'Unknown'}
-embarked_categorical = pd.Categorical(df['Embarked']).rename_categories(stations)
-df['Embarked'] = embarked_categorical
-
-df.loc[df['Cabin'].isna(), 'Cabin'] = 'Z'
-cabin_area = [cabin[0] for cabin in df['Cabin']]
-cabin_area_series = pd.Series(cabin_area)
-df['Cabin'] = pd.Categorical(cabin_area_series)
-
-df.info()
-
-df
-
-
 import matplotlib.pyplot as plt
-df.hist('Age')
-plt.savefig('age_hist.png')
+directory = './DataAnalysis/titanic'
+def load_data():
+    titanic_part1_df = pd.read_csv(f'{directory}/titanic.csv')
+    titanic_part1_df.head()
 
+    titanic_part2_incomplete_df = pd.read_csv(f'{directory}/titanic_part2_incomplete.csv')
+    titanic_part2_incomplete_df.head()
 
-df.hist(color = 'red')
-plt.savefig('age_hist_red.png')
+    titanic_part2_survival_df = pd.read_csv(f'{directory}/titanic_part2_survival.csv')
+    titanic_part2_survival_df.head()
 
-df.hist('Age', bins=10, by='Pclass')
+    titanic_part2_df = pd.merge(titanic_part2_incomplete_df, titanic_part2_survival_df, how='inner', on='PassengerId')
+    df = pd.concat([titanic_part1_df, titanic_part2_df], ignore_index=True)
+    return df
+def drop_columns(raw_data):
+    raw_data.drop('PassengerId', axis=1, inplace=True)
+    raw_data.drop('Ticket', axis=1, inplace=True)
+    return raw_data
+def fill_missing_ages(raw_data):
+    male_mean_age = raw_data[raw_data['Sex'] == 'male']['Age'].mean()
+    female_mean_age = raw_data[raw_data['Sex'] == 'female']['Age'].mean()
 
-df.boxplot()
+    raw_data.loc[(raw_data['Sex'] == 'female') & (raw_data['Age'].isnull()), 'Age'] = female_mean_age
+    raw_data.loc[(raw_data['Sex'] == 'male') & (raw_data['Age'].isnull()), 'Age'] = male_mean_age
+    return raw_data
+
+def create_embarkment_category(raw_data):
+    raw_data.loc[raw_data['Embarked'].isnull(), 'Embarked'] = 'U'
+    stations = {'S': 'Southhampton', 'C': 'Cherbourg', 'Q': 'Queenstown', 'U': 'Unknown'}
+    embarked_categorical = pd.Categorical(raw_data['Embarked']).rename_categories(stations)
+    raw_data['Embarked'] = embarked_categorical
+    return raw_data
+def create_sex_category(raw_data):
+    raw_data['Sex'] = pd.Categorical(raw_data['Sex'])
+    return raw_data
+def create_pclass_category(raw_data):
+    raw_data['Pclass'] = pd.Categorical(raw_data['Pclass']).rename_categories({1: 'First', 2: 'Second', 3: 'Third'})
+    return raw_data
+def create_survived_category(raw_data):
+    surviced_categorical_series = pd.Categorical(raw_data['Survived'])
+    surviced_categorical_series = surviced_categorical_series.rename_categories(['died', 'survived'])
+    raw_data['Survived'] = surviced_categorical_series
+    return raw_data
+def create_cabin_category(raw_data):
+    raw_data.loc[raw_data['Cabin'].isna(), 'Cabin'] = 'Z'
+    cabin_area = [cabin[0] for cabin in raw_data['Cabin']]
+    cabin_area_series = pd.Series(cabin_area)
+    raw_data['Cabin'] = pd.Categorical(cabin_area_series)
+    return raw_data
+
+def clean (raw_data):
+    dropped_data = drop_columns(raw_data)
+    filled_ages = fill_missing_ages(dropped_data)
+    embarked_categorical = create_embarkment_category(filled_ages)
+    sex_categorical = create_sex_category(embarked_categorical)
+    plcass_categorial = create_pclass_category(sex_categorical)
+    survived_categorial = create_survived_category(plcass_categorial)
+    cabin_categorial = create_cabin_category(survived_categorial)
+    return cabin_categorial
+
+def plot(data):
+    data.hist('Age')
+    plt.savefig(f'{directory}/age_hist.png')
+
+    data.hist(color = 'red')
+    plt.savefig(f'{directory}/age_hist_red.png')
+
+    data.hist('Age', bins=10, by='Pclass')
+    plt.savefig(f'{directory}/age_hist_group_by_pclass.png')
+    data.boxplot('Age')
+    plt.savefig(f'{directory}/boxplots.png')
+
+def main():
+    raw_data = load_data()
+    data = clean(raw_data)
+    plot(data)
+
+if __name__ == '__main__':
+    main()
