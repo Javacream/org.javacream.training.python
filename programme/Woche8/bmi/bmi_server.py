37c40b4261da95f80d0df73fcbb42e506644b310
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-26 15:13:46 +0100

    BmiServer mit TcpServer

--- programme/Woche8/bmi/bmi_server.py
+++ programme/Woche8/bmi/bmi_server.py
@@ -1,21 +1,16 @@
-import socket
+from socketserver import TCPServer, BaseRequestHandler
 import pickle
 from bmi import calculate_bmi 
-from multiprocessing import Process
-def handle_client_request(client_socket):
-    with client_socket:    
-        data = pickle.loads(client_socket.recv(1024))
+class BmiRequestHandler(BaseRequestHandler):
+    def handle(self):
+        data = pickle.loads(self.request.recv(1024))
         bmi = calculate_bmi(data[0], data[1])
-        client_socket.sendall(str(bmi).encode())
+        self.request.sendall(str(bmi).encode())
 
 def main():
     IP_ADDRESS = '127.0.0.1'
     PORT = 8001
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.bind((IP_ADDRESS, PORT))
-        server_socket.listen(1)
-        while True:
-            client_socket, address_info = server_socket.accept()
-            Process(target=handle_client_request, args=(client_socket, )).start()
+    server = TCPServer((IP_ADDRESS, PORT), BmiRequestHandler)
+    server.serve_forever()
 if __name__ == '__main__':
     main()
