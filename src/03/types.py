0bdfe25273d62dde25394d309e765f7cbba6a6f5
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-27 20:41:50 +0100

    types and operators

---
+++ src/03/types.py
@@ -0,0 +1,7 @@
+name = 'Musterperson'
+height = 183
+weight = 74.3
+
+print(f'type of name is: {type(name)}')
+print(f'type of height is: {type(height)}')
+print(f'type of weight is: {type(weight)}')
