f1f447b15c5e7c855dcb3a3beeb12ff9270416fc
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-03-14 09:56:49 +0100

    Nautobot

--- programme/Woche10/nautobot/simple_nautobot.py
+++ programme/Woche10/nautobot/simple_nautobot.py
@@ -1,10 +1,8 @@
 import requests
 
-# Konfigurationsparameter
 NAUTOBOT_URL = 'https://demo.nautobot.com/api/'
 API_TOKEN = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
 
-# Header mit API-Token
 headers = {
     'Authorization': f'Token {API_TOKEN}',
     'Content-Type': 'application/json',
@@ -18,9 +16,13 @@ def get_ip_addresses():
     for ip in ip_addresses.get("results", []):
         print(f"IP: {ip['address']}")
 
+def demo():
+    response = requests.get(f"https://demo.nautobot.com/api/ipam/vlan-groups/?depth=1&exclude_m2m=false", headers=headers)
+    print(response.json())
+
 
 def main():
-    get_ip_addresses()
-
+    # get_ip_addresses()
+    demo()
 if __name__ == "__main__":
     main()
