a77b0e7e68f329865b0be8f502101f6b46268ecd
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-03-19 11:44:35 +0100

    Typisierung

---
+++ programme/src/Woche1/type_scratch.py
@@ -0,0 +1,14 @@
+name = 'Sawitzki'
+type_of_name = type(name)
+print(type_of_name)
+
+height = 183
+print(type(height))
+
+weight = 75.1
+print(type(weight))
+
+x = 'Hugo'
+print(type(x))
+x = 42
+print(type(x))
