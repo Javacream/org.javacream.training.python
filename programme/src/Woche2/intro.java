d9142d332b1848564df7ed8672f14f774f8d3923
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-03-19 15:54:41 +0100

    Start Woche 2

---
+++ programme/src/Woche2/intro.java
@@ -0,0 +1,6 @@
+if (2 < 5){
+    System.out.println("Zwei ist kleiner als 5");
+}
+else{
+    System.out.println("Zwei ist größer als 5");
+}
---
+++ programme/src/Woche2/intro.py
@@ -0,0 +1,10 @@
+if 2 < 5:
+    print("Zwei ist kleiner als 5")
+    print('egal')
+else:
+    print("Zwei ist größer als 5")
+
+if 2 > 5:
+    pass
+else:
+    print("...")
