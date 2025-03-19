85af17a477cfc8b67af3c448f90816bfabcb4b3c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-03-19 12:05:31 +0100

    Lösung BMI, Aufgabe 3

--- programme/src/Woche1/bmi.py
+++ programme/src/Woche1/bmi.py
@@ -1,5 +1,7 @@
-person_name='Sawitzki'
-person_height=183
-person_weight=75.1
-body_mass_index=person_weight/(person_height*person_height)*100**2
+person_name = input('Bitte geben Sie Ihren Namen an: ')
+person_height = input('Bitte geben Sie ihre Körpergröße in Zentimetern an: ')
+person_weight = input('Bitte geben Sie ihr Körpergewicht in Kilogramm an, Trennzeichen ist der .: ')
+person_height = int(person_height)
+person_weight = float(person_weight)
+body_mass_index = person_weight / (person_height * person_height) * 100**2
 print(f'Der berechnete BMI für {person_name} ist: {body_mass_index:.2f}')
