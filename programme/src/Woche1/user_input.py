da6b49d2e5aa00f8c7587d64411411e87cfd94ab
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-03-19 11:52:52 +0100

    Bitte Variablen nicht wie BuiltIn-Funktionen nennen

---
+++ programme/src/Woche1/user_input.py
@@ -0,0 +1,10 @@
+user_input = input('Bitte geben Sie einen Wert ein: ')
+print(user_input)
+
+# so nicht, danach funktioniert keine Benutzereingabe mehr...
+input = input('Bitte geben Sie noch einen Wert ein: ')
+print(input)
+
+input = input('Bitte geben Sie noch einen dritten Wert ein: ')
+print(input)
+
