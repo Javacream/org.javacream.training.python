c17403d480ba4ad4eafcd8f5742600245129e133
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-03-19 11:01:52 +0100

    Einführung des formatierten Strings

--- programme/src/Woche1/bmi.py
+++ programme/src/Woche1/bmi.py
@@ -2,4 +2,4 @@ person_name='Sawitzki'
 person_height=183
 person_weight=75.1
 body_mass_index=person_weight/(person_height*person_height)*100*100
-print(body_mass_index)
+print(f'Der berechnete BMI für {person_name} ist: {body_mass_index:.2f}')
