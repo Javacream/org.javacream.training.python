fb63c4a7b1429ad7a5d15fbb73ebbc67ec591dc5
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-03-19 14:26:21 +0100

    Lösungen der Aufgaben 1,2 und 4

---
+++ programme/src/Woche1/calculator.py
@@ -0,0 +1,18 @@
+number1 = input('Bitte die erste Zahl eingeben: ')
+number2 = input('Bitte die zweite Zahl eingeben: ')
+number1 = float(number1)
+number2 = float(number2)
+
+result_sum = number1 + number2
+print('Die Summe der Zahlen ' + str(number1) + ' und ' + str(number2) + ' ist ' + str(result_sum))
+
+result_diff = number1 - number2
+print(f'Die Differenz der Zahlen {number1} und {number2} ist {result_diff}')
+
+result_times = number1 * number2
+print(f'Das Produkt der Zahlen {number1} und {number2} ist {result_times}')
+
+print(f'Der Quotient der Zahlen {number1} und {number2} ist {number1 / number2}') # Jawohl, in den geschweiften Klammern kann man Aktionen ausführen
+
+
+
---
+++ programme/src/Woche1/name_und_alter.py
@@ -0,0 +1,5 @@
+name = input('Bitte Name eingeben: ')
+age = input('Bitte Alter angeben: ')
+
+# print(name + ' ist ' + age + ' Jahre alt')
+print(f'{name} ist {age} Jahre alt')
