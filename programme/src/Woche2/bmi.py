18cc01e4de1dece5e30dc53ae8a9700bb6e7e0f5
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-02 09:36:12 +0200

    Alternative Lösung

--- programme/src/Woche2/bmi.py
+++ programme/src/Woche2/bmi.py
@@ -6,8 +6,7 @@ person_weight = float(person_weight)
 bmi_calculatable = False
 person_height_smaller_than_250 = (person_height < 250)
 person_height_greater_than_10 = (person_height > 50)
-if  person_height_smaller_than_250 and person_height_greater_than_10:
-    bmi_calculatable = True
+bmi_calculatable = person_height_smaller_than_250 and person_height_greater_than_10:
 if bmi_calculatable:
     body_mass_index = person_weight / (person_height * person_height) * 100**2
     print(f'Der berechnete BMI für {person_name} ist: {body_mass_index:.2f}')
