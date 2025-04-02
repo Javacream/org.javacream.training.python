2e6d878010d205098cfc66daa2f3bf2a454b5348
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-02 13:43:41 +0200

    try except mit Beispielen

--- programme/src/Woche2/fehlerbehandlung.py
+++ programme/src/Woche2/fehlerbehandlung.py
@@ -1,6 +1,7 @@
 height = input("Körpergröße?")
 try:
+    print("Versuch der Umwandlung")
     height = int(height)
     print(f"{height} ist als Zahl interpretierbar")
-except:
-    print(f"{height} ist nicht als Zahl interpretierbar")
+except Exception as e:
+    print(f"{height} ist nicht als Zahl interpretierbar: {e}")
