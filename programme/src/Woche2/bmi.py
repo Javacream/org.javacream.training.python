ac9dca0d05a1427c4ad4767cece11693b6b9bf61
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-02 09:37:33 +0200

    Weitere Alternative

--- programme/src/Woche2/bmi.py
+++ programme/src/Woche2/bmi.py
@@ -3,10 +3,7 @@ person_height = input('Bitte geben Sie ihre Körpergröße in Zentimetern an: ')
 person_weight = input('Bitte geben Sie ihr Körpergewicht in Kilogramm an, Trennzeichen ist der .: ')
 person_height = int(person_height)
 person_weight = float(person_weight)
-bmi_calculatable = False
-person_height_smaller_than_250 = (person_height < 250)
-person_height_greater_than_10 = (person_height > 50)
-bmi_calculatable = person_height_smaller_than_250 and person_height_greater_than_10:
+bmi_calculatable =  (person_height < 250) and (person_height > 50)
 if bmi_calculatable:
     body_mass_index = person_weight / (person_height * person_height) * 100**2
     print(f'Der berechnete BMI für {person_name} ist: {body_mass_index:.2f}')
