b217f3db10413e36828ce77317bcfa3aee6a9b5e
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-02 11:13:40 +0200

    Kompaktere Programmierung

--- programme/src/Woche2/while_schleife.py
+++ programme/src/Woche2/while_schleife.py
@@ -1,8 +1,5 @@
-condition = True
-
 actual_number = 0
 END = 5
-while condition:
+while actual_number < END:
     actual_number += 1 #  Identisch zu actual_number = actual_number + 1
     print(f"{actual_number}")
-    condition = actual_number < END
