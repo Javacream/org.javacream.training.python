77d5e5b437bd2f1431dbb7a3724f4ce7bc9a5041
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-02 09:30:38 +0200

    Erweitern des BMI-Beispiels mit if-else

---
+++ programme/src/Woche2/bmi.py
@@ -0,0 +1,14 @@
+person_name = input('Bitte geben Sie Ihren Namen an: ')
+person_height = input('Bitte geben Sie ihre Körpergröße in Zentimetern an: ')
+person_weight = input('Bitte geben Sie ihr Körpergewicht in Kilogramm an, Trennzeichen ist der .: ')
+person_height = int(person_height)
+person_weight = float(person_weight)
+bmi_calculatable = False
+if person_height < 250:
+    if person_height > 50:
+        bmi_calculatable = True
+if bmi_calculatable:
+    body_mass_index = person_weight / (person_height * person_height) * 100**2
+    print(f'Der berechnete BMI für {person_name} ist: {body_mass_index:.2f}')
+else:
+    print("Mit den Eingaben kann kein sinnvoller BMI berechnet werden!")
