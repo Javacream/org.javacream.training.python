f4621007084a1c8bcd063e20b6dd7e9f6b0f36f5
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-03 14:16:45 +0200

    Lösungen Liste

---
+++ programme/src/Woche3/Aufgaben_Listen/Aufgabe1.py
@@ -0,0 +1,18 @@
+todos = ['Einkaufen', 'Putzen', 'Schlafen', 'Essen', 'Putzen']
+
+start = len(todos) - 1
+end = 0
+index = start
+while index <= start:
+    print(todos[index])
+    index -= 1
+    if index < 0:
+        break
+
+todos_reversed_slice = todos[::-1]
+for todo in todos_reversed_slice:
+    print(todo)
+
+todos_reversed = reversed(todos)
+for todo in todos_reversed:
+    print(todo)
---
+++ programme/src/Woche3/Aufgaben_Listen/Aufgabe3.py
@@ -0,0 +1,3 @@
+todos = ['Einkaufen', 'Putzen', 'Schlafen', 'Essen', 'Putzen']
+todos_without_duplicates = set(todos)
+print(todos_without_duplicates)
