19a2ca6a78c53fdfdb6e2a6d662d4d948fbe2eba
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-03 13:52:57 +0200

    Collections Kategorien

---
+++ programme/src/Woche3/collection_kategorien.py
@@ -0,0 +1,15 @@
+todos = ['Einkaufen', 'Putzen', 'Schlafen', 'Essen', 'Putzen']
+
+person = {"lastname": "Sawitzki", "firstname": "Rainer", "weight": 76.5}
+
+todos_set = {'Einkaufen', 'Putzen', 'Schlafen', 'Essen', 'Putzen'}
+
+seasons = ('Frühling', 'Sommer', 'Herbst', 'Winter')
+
+interval = range(1,5)
+interval = range(1, 5, 2)
+
+# list = ['A', 'B']
+empty_list = list()
+
+print("done")
