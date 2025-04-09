e55faf707419d1add03a926c3b2aea35215d8a57
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-09 13:45:14 +0200

    with-Syntax

--- programme/src/Woche4/file_demo.py
+++ programme/src/Woche4/file_demo.py
@@ -1,7 +1,6 @@
 def main():
-    readme_file = open ("README.md", 'rt', encoding='utf-8')
-    lines = readme_file.readlines()
-    readme_file.close()
+    with open ("README.md", 'rt', encoding='utf-8') as readme_file:
+        lines = readme_file.readlines()
     for line in lines:
         print(line)
 main()
