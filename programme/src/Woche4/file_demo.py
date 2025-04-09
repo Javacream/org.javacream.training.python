3267d7419fa95fb9418e842893b51b9a9bf8dd9f
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-09 13:47:36 +0200

    Schreiben einer Datei

--- programme/src/Woche4/file_demo.py
+++ programme/src/Woche4/file_demo.py
@@ -3,4 +3,6 @@ def main():
         lines = readme_file.readlines()
     for line in lines:
         print(line)
+    with open ("result.txt", 'wt', encoding='utf-8') as result_file:
+        result_file.write("Hugo")        
 main()
