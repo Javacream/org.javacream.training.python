20e3cf9e3e3c5c0c881b6cf688a223ad65947c63
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-09 09:52:21 +0200

    Variablen Scopes

--- programme/src/Woche4/bmi_demo/bmi.py
+++ programme/src/Woche4/bmi_demo/bmi.py
@@ -1,12 +1,12 @@
-def calculate_bmi(weight, height):
-    body_mass_index = weight/(height**2)*100**2
+def calculate_bmi(w, h):
+    body_mass_index = w/(h**2)*100**2
     return body_mass_index
 
 def main():
     weight = 75.7
     height = 183
-    
-    calculated_bmi = calculate_bmi(weight, height)
+    name = "Hugo"
+    calculated_bmi = calculate_bmi(weight, height) # Hier wird impliziet w=weight und h=height aufgerufen
     print(calculated_bmi)
 
     print(calculate_bmi(99.6, 176))
