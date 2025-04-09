c9552bb0c88b25e98a6a3868fdef3c73de5fec52
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-09 13:42:30 +0200

    Schliessen einer Datei

--- programme/src/Woche4/file_demo.py
+++ programme/src/Woche4/file_demo.py
@@ -1,6 +1,7 @@
 def main():
     readme_file = open ("README.md", 'rt', encoding='utf-8')
     lines = readme_file.readlines()
+    readme_file.close()
     for line in lines:
         print(line)
 main()
