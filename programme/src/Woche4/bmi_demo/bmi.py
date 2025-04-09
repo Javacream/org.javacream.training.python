a7bb5d7cc5ededf4b5ba59eff81c9be90220de3b
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-09 10:09:06 +0200

    Varargs

--- programme/src/Woche4/bmi_demo/bmi.py
+++ programme/src/Woche4/bmi_demo/bmi.py
@@ -1,4 +1,6 @@
-def calculate_bmi(weight=74.3, height=181):
+def calculate_bmi(*people_data):
+    weight = people_data[0]
+    height = people_data[1]
     body_mass_index = weight/(height**2)*100**2
     return body_mass_index
 
@@ -7,6 +9,6 @@ def main():
     height = 183
     calculated_bmi = calculate_bmi(weight, height) # Hier wird impliziet w=weight und h=height aufgerufen
     print(calculated_bmi)
-    print(calculate_bmi())
-    print(calculate_bmi(199))
+    calculated_bmi = calculate_bmi(weight, height, True, 42, "Hugo") # Hier wird impliziet w=weight und h=height aufgerufen
+
 main()
