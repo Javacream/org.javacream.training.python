84414375ca8183b795d7ccf037f6457ec6555315
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-09 10:13:52 +0200

    Keyworded Args

--- programme/src/Woche4/bmi_demo/bmi.py
+++ programme/src/Woche4/bmi_demo/bmi.py
@@ -1,14 +1,13 @@
-def calculate_bmi(*people_data):
-    weight = people_data[0]
-    height = people_data[1]
+def calculate_bmi(**person_data):
+    weight = person_data["w"]
+    height = person_data["h"]
     body_mass_index = weight/(height**2)*100**2
     return body_mass_index
 
 def main():
     weight = 75.7
     height = 183
-    calculated_bmi = calculate_bmi(weight, height) # Hier wird impliziet w=weight und h=height aufgerufen
+    calculated_bmi = calculate_bmi(w=weight, h=height) # Hier wird impliziet w=weight und h=height aufgerufen
     print(calculated_bmi)
-    calculated_bmi = calculate_bmi(weight, height, True, 42, "Hugo") # Hier wird impliziet w=weight und h=height aufgerufen
 
 main()
