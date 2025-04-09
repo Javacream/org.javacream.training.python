1b8d4377edc929da94bb95750f6d086d89b698fb
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-09 10:04:17 +0200

    Default für Parameter

--- programme/src/Woche4/bmi_demo/bmi.py
+++ programme/src/Woche4/bmi_demo/bmi.py
@@ -1,4 +1,4 @@
-def calculate_bmi(weight, height):
+def calculate_bmi(weight=74.3, height=181):
     body_mass_index = weight/(height**2)*100**2
     return body_mass_index
 
@@ -7,4 +7,6 @@ def main():
     height = 183
     calculated_bmi = calculate_bmi(weight, height) # Hier wird impliziet w=weight und h=height aufgerufen
     print(calculated_bmi)
+    print(calculate_bmi())
+    print(calculate_bmi(199))
 main()
