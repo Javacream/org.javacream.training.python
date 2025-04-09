031b1561588f299a2ade7072c933049e3659f89e
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-09 09:45:57 +0200

    Einführen von main

--- programme/src/Woche4/bmi_demo/bmi.py
+++ programme/src/Woche4/bmi_demo/bmi.py
@@ -2,11 +2,13 @@ def calculate_bmi(weight, height):
     body_mass_index = weight/(height**2)*100**2
     return body_mass_index
 
+def main():
+    weight = 75.7
+    height = 183
+    
+    calculated_bmi = calculate_bmi(weight, height)
+    print(calculated_bmi)
 
-weight = 75.7
-height = 183
+    print(calculate_bmi(99.6, 176))
 
-calculated_bmi = calculate_bmi(weight, height)
-print(calculated_bmi)
-
-print(calculate_bmi(99.6, 176))
+main()
