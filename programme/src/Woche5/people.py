2077ac439c63cdb8385bf01ce9332c081caccab2
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-16 10:19:39 +0200

    Ein paar Details

--- programme/src/Woche5/people.py
+++ programme/src/Woche5/people.py
@@ -14,11 +14,7 @@ class Person:
 def main():
     person1 = Person("Sawitzki", 183, 75.7)
     person2 = Person("Meier", 189, 83.5)
-    print(person1.name)
-    person1.name = "Musterperson"
-    print(person1.name)
-    print(person1.say_hello())    
-    print(person2.say_hello())
-    print(person1.greet(True))    
-    print(person2.greet(False))
+    person1.name = "Mustermann"
+    person1.given_name = "Hugo"
+    print("done")
 main()
