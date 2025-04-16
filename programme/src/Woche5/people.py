43c2f447502623d67b8bd7d5118716b246afef44
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-16 11:12:25 +0200

    Eine Person hat exakt eine Adresse

--- programme/src/Woche5/people.py
+++ programme/src/Woche5/people.py
@@ -1,8 +1,13 @@
+class Address:
+    def __init__(self, city, street):
+        self.city = city
+        self.street = street
 class Person:
-    def __init__(self, name, height, weight):
+    def __init__(self, name, height, weight, address):
         self.name = name
         self.height = height
         self.weight = weight
+        self.address = address
     def say_hello(self):
         return "Hello!"
     def greet(self, friendly):
@@ -10,11 +15,10 @@ class Person:
             return f"Hi, my name is {self.name}"
         else:
             return f"Good day, my name is {self.name}"
-
 def main():
-    person1 = Person("Sawitzki", 183, 75.7)
-    person2 = Person("Meier", 189, 83.5)
-    person1.name = "Mustermann"
-    person1.given_name = "Hugo"
+    a1 = Address("München", "Marienplatz")
+    a2 = Address("Berlin", "Alexanderplatz")
+    person1: Person = Person("Sawitzki", 183, 75.7, a1)
+    person2 = Person("Meier", 189, 83.5, a2)
     print("done")
 main()
