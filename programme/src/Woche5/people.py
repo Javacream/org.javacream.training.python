c91583221ba2670027772af37e390be52613956a
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-16 11:16:58 +0200

    Eine Person hat beliebig viele Adressen

--- programme/src/Woche5/people.py
+++ programme/src/Woche5/people.py
@@ -3,11 +3,11 @@ class Address:
         self.city = city
         self.street = street
 class Person:
-    def __init__(self, name, height, weight, address):
+    def __init__(self, name, height, weight):
         self.name = name
         self.height = height
         self.weight = weight
-        self.address = address
+        self.addresses = set()
     def say_hello(self):
         return "Hello!"
     def greet(self, friendly):
@@ -18,7 +18,10 @@ class Person:
 def main():
     a1 = Address("München", "Marienplatz")
     a2 = Address("Berlin", "Alexanderplatz")
-    person1: Person = Person("Sawitzki", 183, 75.7, a1)
-    person2 = Person("Meier", 189, 83.5, a2)
+    person1: Person = Person("Sawitzki", 183, 75.7)
+    person2 = Person("Meier", 189, 83.5)
+    person1.addresses.add(a1)
+    person1.addresses.add(a2)
+    person2.addresses.add(a2)
     print("done")
 main()
