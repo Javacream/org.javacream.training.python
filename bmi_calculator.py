5c5f3e5feba2508a36014d8024ccb99948937112
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-05 15:21:20 +0200

    BMI Wertung, ToDo Slide 29

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -4,4 +4,16 @@ height = input("Bitte Körpergröße in Meter: ")
 weight = float(weight)
 height = float(height)
 body_mass_index = weight/(height**2)
-print(f'Berechneter BMI für {name}: {body_mass_index:.2f}')
+
+is_underweighted = body_mass_index < 18
+is_normalweighted = body_mass_index >= 18 and body_mass_index <= 22
+is_overweighted = body_mass_index > 22 and body_mass_index <25
+
+if is_underweighted:
+    print(f'{name} ist untergewichtig')
+elif is_normalweighted:
+    print(f'{name} ist normalgewichtig')
+elif is_overweighted:
+    print(f'{name} ist übergewichtig')
+else:
+    print(f'{name} ist fettleibig')
