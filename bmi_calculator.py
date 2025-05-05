c13760f927114c8be6ca72e368cc07b54f830bed
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-05 13:41:29 +0200

    Mathematische Operatoren, z.B. Pozenz

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,4 +1,5 @@
+name = input("Bitte Name eingeben: ")
 weight = 76.3
 height = 1.83
-body_mass_index = weight/(height*height)
-print(f'Berechneter BMI: {body_mass_index:.2f}')
+body_mass_index = weight/(height**2)
+print(f'Berechneter BMI für {name}: {body_mass_index:.2f}')
