9b8a922bd8a8c004337c548773b04f81b17a4451
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-05 14:33:34 +0200

    Ablaufsteuerung mit Blöcken = Einrückungen

---
+++ if_else_scratch.py
@@ -0,0 +1,9 @@
+status = True
+# Syntaktisch NICHT korrekt, aber sehr schlecht lesbar
+#if status {print("Status war wahr")}else{print("Status war falsch")}
+
+# Syntaktisch korrekt und gut lesbar
+if status:
+    print("Status ist wahr")
+else:
+    print("Status ist falsch")
