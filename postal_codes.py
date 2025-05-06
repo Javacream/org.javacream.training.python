20454bc123df001b3f9c151d1b9d9b1fbd2d167d
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-06 13:56:54 +0200

    Start Postal Codes

---
+++ postal_codes.py
@@ -0,0 +1,35 @@
+postal_codes_to_cities = {
+    "10115": "Berlin",
+    "20095": "Hamburg",
+    "30159": "Hannover",
+    "01067": "Dresden",
+    "04109": "Leipzig",
+    "50667": "Köln",
+    "60311": "Frankfurt am Main",
+    "70173": "Stuttgart",
+    "80331": "München",
+    "90402": "Nürnberg",
+    "28195": "Bremen",
+    "39104": "Magdeburg",
+    "99084": "Erfurt",
+    "55116": "Mainz",
+    "66111": "Saarbrücken",
+    "14467": "Potsdam",
+    "01099": "Dresden-Neustadt",
+    "18055": "Rostock",
+    "17489": "Greifswald",
+    "37073": "Göttingen",
+    "48143": "Münster",
+    "52062": "Aachen",
+    "86150": "Augsburg",
+    "90403": "Nürnberg-Altstadt",
+    "24103": "Kiel"
+}
+
+city_to_postal_codes  = {
+    "Berlin": ["10115", "10117", "10119", "10405", "10555"],
+    "Hamburg": ["20095", "20097", "20354", "20535", "21029"],
+    "München": ["80331", "80333", "80469", "80636", "80939"],
+    "Köln": ["50667", "50670", "50733", "50933", "51103"],
+    "Frankfurt am Main": ["60311", "60313", "60486", "60528", "60599"]
+}
