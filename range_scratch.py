33f4ee125e0ff0458571ed761b95e5f002fa39b0
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-06 15:06:56 +0200

    Ranges

---
+++ range_scratch.py
@@ -0,0 +1,12 @@
+numbers = range(1, 5)
+numbers = range(1, 5, 2)
+print(numbers)
+
+for number in numbers:
+    print(number)
+
+
+for number in range(1, 4):
+    print(number)
+
+print(numbers[0])    
