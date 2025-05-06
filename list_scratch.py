13d339def9b4b5e1da882303160e67c8e09ddea6
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-06 11:00:45 +0200

    Liste Teil2: Variationen

--- list_scratch.py
+++ list_scratch.py
@@ -1,3 +1,9 @@
 numbers = [1,3,42,-9,5]
-print(type(numbers))
+names = ['Hugo', 'Andrea', 'Helga']
+mixed = ['Hugo', 76.6, 1.83]
+nested = [
+    ['Hugo', 76.6, 1.83],
+    ['Andrea', 66.6, 1.66],
+    ['Helga', 86.6, 1.93]
+]
 print(numbers)
