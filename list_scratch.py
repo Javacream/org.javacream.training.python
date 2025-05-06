03d79aae7e1a7b2fb2f84035714a8c52f48afd66
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-06 11:06:53 +0200

    Weitere Variationen

--- list_scratch.py
+++ list_scratch.py
@@ -2,8 +2,13 @@ numbers = [1,3,42,-9,5]
 names = ['Hugo', 'Andrea', 'Helga']
 mixed = ['Hugo', 76.6, 1.83]
 nested = [
-    ['Hugo', 76.6, 1.83],
+    [['Hugo', 'Franz'], 76.6, 1.83],
     ['Andrea', 66.6, 1.66],
     ['Helga', 86.6, 1.93]
 ]
+
+person1 = ['Hugo', 'Sawitzki']
+person2 = ['Andrea', 'Meier']
+person3 = ['Helga', 'Müller']
+people = [person1, person2, person3]
 print(numbers)
