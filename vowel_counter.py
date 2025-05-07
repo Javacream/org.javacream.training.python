bc77fe300df941a51a8c5e3376da35d0960dd0ae
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-07 13:56:09 +0200

    Ideen für das Vokale zählen...

--- vowel_counter.py
+++ vowel_counter.py
@@ -1,7 +1,8 @@
-vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+vowels = ('a', 'e', 'i', 'o', 'u', 'ä', 'ö', 'ü')
 text = input ('Bitte einen beliebigen Text eingeben: ')
+text_lower_case = text.lower()
 vowel_counter = 0
-for character in text:
-    if character in vowels:
-        vowel_counter += 1
+for vowel in vowels:
+    vowel_counter += text_lower_case.count(vowel)
+
 print(f"Der eingegebene Text {text} enthält {vowel_counter} Vokale")        
