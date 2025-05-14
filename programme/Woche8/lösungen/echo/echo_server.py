b4f67d535eaac1fdc2eea85d957d99bf11435aa1
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-14 13:40:49 +0200

    Parallelisierung des Servers

--- programme/Woche8/lösungen/echo/echo_server.py
+++ programme/Woche8/lösungen/echo/echo_server.py
@@ -1,5 +1,14 @@
 import socket
 from datetime import datetime
+from multiprocessing import Process
+def handle_request(client_socket, address_info):
+    with client_socket:
+        client_message = client_socket.recv(1024).decode('utf-8')
+        with open ('echo.log', 'a') as log_file:
+            log_file.write(f'got message from client {address_info} at {datetime.now()}: {client_message}\n')
+        client_socket.sendall(f'echoing {client_message}'.encode())
+
+
 def main():
     IP_ADDRESS = '127.0.0.1'
     PORT = 8000 # Portnummern unter 1000 sind "reserviert" für spezielle Anwendungen, erlaubt ist alles bis hin zu 65000
@@ -8,13 +17,7 @@ def main():
         server_socket.listen(1)
         while True:
             client_socket, address_info = server_socket.accept() # Hier wird die Anwendung blockiert, bis endlich eine Client-Anforderung kommt
-            with client_socket:
-                client_message = client_socket.recv(1024).decode('utf-8')
-                if client_message == 'secret_stop_code':
-                    print("received stop message, exiting...")
-                    break
-                with open ('echo.log', 'a') as log_file:
-                    log_file.write(f'got message from client {address_info} at {datetime.now()}: {client_message}\n')
-                client_socket.sendall(f'echoing {client_message}'.encode())
+            process = Process(target=handle_request, args=(client_socket, address_info)) 
+            process.start()
 if __name__ == '__main__':
     main()
