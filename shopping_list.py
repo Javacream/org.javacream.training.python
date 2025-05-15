6bfca28a76d7c14aef9af4683a29d560c9f4ef34
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-15 15:18:15 +0200

    Shopping mit Comprehension

--- shopping_list.py
+++ shopping_list.py
@@ -1,17 +1,11 @@
 def read_shopping_list(path):
     with open(path, 'rt', encoding='utf-8') as file:
         raw_rows = file.readlines()
-        rows = []
-        for raw_row in raw_rows:
-            rows.append(raw_row.replace('\n', ''))
+    rows = [raw_row.replace('\n', '') for raw_row in raw_rows]
     return rows
 
 def create_shopping_data(data_list):
-    shopping_data = []
-    for data in data_list:
-        splitted = data.split(" ")
-        data_tuple = (splitted[0], splitted[1], int(splitted[2]))
-        shopping_data.append(data_tuple)
+    shopping_data = [(data.split(" ")[0], data.split(" ")[1], int(data.split(" ")[2])) for data in data_list]
     return shopping_data
 
 def write_shopping_data(shopping_data):
