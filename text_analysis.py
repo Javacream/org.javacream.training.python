294338af5ed7e5f4004366327b1ff9a5851ba917
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-15 12:11:53 +0200

    Optionale Änderung

--- text_analysis.py
+++ text_analysis.py
@@ -51,6 +51,15 @@ def lengthiest_words(text):
 def write_result(result_dictionary):
     print(result_dictionary)
 
+def create_result(character_count, alphanumeric_character_count, word_count, unique_word_count, smallest, lengthiest):
+    result = dict()
+    result['character_count'] = character_count
+    result['alphanumeric_character_count'] = alphanumeric_character_count
+    result['word_count'] = word_count
+    result['unique_character_count'] = unique_word_count
+    result['smallest'] = smallest
+    result['lengthiest'] = lengthiest
+
 def main():
     file_path = 'text.txt'
     content = read_file(file_path)
@@ -60,12 +69,6 @@ def main():
     unique_word_count = count_unique_words(content)
     smallest = smallest_words(content)
     lengthiest = lengthiest_words(content)
-    result = dict()
-    result['character_count'] = character_count
-    result['alphanumeric_character_count'] = alphanumeric_character_count
-    result['word_count'] = word_count
-    result['unique_character_count'] = unique_word_count
-    result['smallest'] = smallest
-    result['lengthiest'] = lengthiest
+    result = create_result(character_count, alphanumeric_character_count, word_count, unique_word_count, smallest, lengthiest)
     write_result(result)
 main()
