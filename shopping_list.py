5357c10e65395bfce6ae391f456ff5d0cbb5274c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-15 16:04:02 +0200

    ToDo Slide 67

--- shopping_list.py
+++ shopping_list.py
@@ -11,14 +11,40 @@ def create_shopping_data(data_list):
 def write_shopping_data(shopping_data):
     print(shopping_data)
 
-def unique_names(shopping_data):
+def create_unique_names(shopping_data):
     uniques = {data[0] for data in shopping_data}
     return uniques
-    
+
+def create_unique_products(shopping_data):
+    uniques = {data[1] for data in shopping_data}
+    return uniques
+
+def create_products_amounts(shopping_data):
+    products = create_unique_products(shopping_data)
+    products_amount = dict()
+    for product in products:
+        products_amount[product] = 0
+    for data in shopping_data:
+        product = data[1]
+        amount = data[2]
+        old_amount = products_amount[product]
+        products_amount[product] = old_amount + amount
+    return products_amount
+
+def create_products_by_people(shopping_data):
+    people = create_unique_names(shopping_data)
+    products_by_people = {person : set() for person in people}
+    for data in shopping_data:
+        products_by_people[data[0]].add(data[1])
+    return products_by_people    
 def main():
     path = "shopping_list.txt"
     rows = read_shopping_list(path)
     shopping_data = create_shopping_data(rows)
     write_shopping_data(shopping_data)
-    print(unique_names(shopping_data))
+    print(f'Namensliste: {create_unique_names(shopping_data)}')
+    print(f'Produktliste: {create_unique_products(shopping_data)}')
+    print(f'Produktliste mit Anzahl: {create_products_amounts(shopping_data)}')
+    print(f'Produktliste pro Person: {create_products_by_people(shopping_data)}')
+
 main()
