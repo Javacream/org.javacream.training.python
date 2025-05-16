0ae82c49caad1b79ed1138de7b92c1fa30b4335e
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-16 14:37:01 +0200

    ToDo Slide 84

---
+++ people.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, firstname, lastname, weight, height):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.weight = weight
+        self.height = height
---
+++ people_simple_app.py
@@ -0,0 +1,6 @@
+from people import Person
+def main():
+    sawitzki = Person('Rainer', 'Sawitzki', 76.6, 1.83)
+    print(sawitzki.height)
+
+main()
