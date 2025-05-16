a65533934017f81310819ae9b553158776ccaab8
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-16 14:15:19 +0200

    OOP und Type Hints

--- books_search_app.py
+++ books_search_app.py
@@ -2,7 +2,7 @@ import books
 
 def main():
     isbn = input('Bitte eine ISBN-Nummer eingeben: ')
-    book = books.search(isbn)
+    book :books.Book = books.search(isbn)
     if book == ():
         print(f'Kein Buch mit der ISBN {isbn} gefunden')
     else:
