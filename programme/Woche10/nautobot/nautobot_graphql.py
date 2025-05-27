2ddf4b4903ac9c24d6fd1aad7b5dce3e5a6cbb95
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-05-27 14:17:09 +0200

    Create nautobot_graphql.py

---
+++ programme/Woche10/nautobot/nautobot_graphql.py
@@ -0,0 +1,37 @@
+import requests
+
+NAUTOBOT_URL = 'https://demo.nautobot.com/api/'
+API_TOKEN = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+headers = {
+    "Authorization": API_TOKEN,
+    "Content-Type": "application/json"
+}
+
+# GraphQL-Query
+query = """
+{
+  devices {
+    name
+    device_type {
+      model
+    }
+    site {
+      name
+    }
+    status {
+      value
+    }
+  }
+}
+"""
+
+response = requests.post(NAUTOBOT_URL, json={"query": query}, headers=headers)
+
+# Ergebnis anzeigen
+if response.status_code == 200:
+    data = response.json()
+    for device in data["data"]["devices"]:
+        print(f"Name: {device['name']}, Modell: {device['device_type']['model']}, Standort: {device['site']['name']}")
+else:
+    print(f"Fehler: {response.status_code}")
+    print(response.text)
