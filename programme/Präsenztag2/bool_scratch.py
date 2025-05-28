dd2360569829df12c41ef252febf999f62e0b635
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-28 16:04:57 +0200

    Konstrollstrukturen und Einrückungen

---
+++ programme/Präsenztag2/bool_scratch.py
@@ -0,0 +1,20 @@
+state = True
+print(type(state))
+
+b1 = True
+b2 = False
+
+print (b1 and b2)
+print (b1 or b2)
+
+print(not b1)
+
+n1 = 40
+n2 = 9
+
+print(n1 < n2)
+print(n1 == n2)
+print(n1 > n2)
+print(n1 != n2)
+
+
---
+++ programme/Präsenztag2/counter.py
@@ -0,0 +1,6 @@
+print('Start des Programms')
+counter = 1
+while counter < 10: 
+    print(counter)
+    counter = counter + 1
+print('Ende des Programms')
