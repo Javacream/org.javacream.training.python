b4c956706c4f9431177a941cfffa73df5417a2e6
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-28 11:36:52 +0200

    Type und formatierter String

--- programme/Präsenztag1/bmi_calculator.py
+++ programme/Präsenztag1/bmi_calculator.py
@@ -1,5 +1,6 @@
 # Dieses Programm berechnet einen Body-Mass-Index
+name = 'Hugo'
 weight = 76.6
 height = 183
 body_mass_index = weight / (height/100 * height/100)
-print(body_mass_index)
+print(f'{name} hat einen Body Mass Index von {body_mass_index:.2f}')
---
+++ programme/Präsenztag1/type_demo.py
@@ -0,0 +1,36 @@
+name = 'Hugo'
+weight = 76.6
+height = 183
+
+
+type_of_name = type(name)
+print(type_of_name)
+
+print(type(weight))
+print(type(height))
+
+n1 = 20
+n2 = 2.2
+
+result = n1 + n2
+print(result)
+
+result = n1 - n2
+print(result)
+
+result = n1 * n2
+print(result)
+
+s1 = 'Hugo'
+s2 = 'Emil'
+result = s1 + s2
+print(result)
+
+#result = s1 - s2
+#print(result)
+
+#result = s1 + n1
+#print(result)
+
+result = s1 * n1
+print(result)
