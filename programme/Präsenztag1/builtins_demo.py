00c5cc3889c36bd41fc66e28a5b554acdc62bbfd
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-28 13:24:19 +0200

    BuiltIns

---
+++ programme/Präsenztag1/builtins_demo.py
@@ -0,0 +1,15 @@
+print()
+print('Hello')
+print('Hello', 'World')
+
+name = 'Hugo'
+var_type = type(name)
+print(var_type)
+
+# type = type(42) # Bitte nicht!
+#print(type)
+
+# sum = 5 + 4 # Ebenso, sum ist eine BuiltIn-Funktion
+
+name = input('Bitte geben Sie Ihren Namen ein: ')
+print(f'der eingegebene Name war {name}')
