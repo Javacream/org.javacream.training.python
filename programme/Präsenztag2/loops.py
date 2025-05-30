d0b68fd7e04c8011fba1591a791889b5052f2816
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-30 13:56:09 +0200

    Strings als Collection

--- programme/Präsenztag2/loops.py
+++ programme/Präsenztag2/loops.py
@@ -6,6 +6,7 @@ r = range(1, 5, 2)
 for i in r:
     print(i)
 
+print(r[0])
 candidate = 3
 if candidate in r:
     print(f'{candidate} ist in {r} enthalten')
@@ -13,6 +14,20 @@ else:
     print(f'{candidate} ist in {r} nicht enthalten')
 
 names = ['A', 'B', 'C']
+for name in names:
+    print(name)
+
+
+candidate = 'D'
+if candidate in names:
+    print(f'{candidate} ist in {names} enthalten')
+else:
+    print(f'{candidate} ist in {names} nicht enthalten')
+
+print(f'Länge von names: {len(names)}')
+print(f'Erstes Element von names: {names[1]}')
+
+names = 'ABC'
 for name in names:
     print(name)
 
