37723b4c0eff655cf273d98afcbd7d68e8c90e2e
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-30 10:04:51 +0200

    BMI-Plausibilitt

--- programme/Präsenztag2/bmi_calculator.py
+++ programme/Präsenztag2/bmi_calculator.py
@@ -10,12 +10,15 @@ elif height <= 50 or height > 250:
     print(f'Die eingegebene Körpergröße {height} ist außerhalb des gültigen Bereichs 50-250' )
 else:
     body_mass_index = weight / (height/100 * height/100)
-    if body_mass_index < 18:
-        category = 'untergewichtig'
-    elif body_mass_index < 25:
-        category = 'normalgewichtig'
-    elif body_mass_index < 30:
-        category = 'übergewichtig'
+    if body_mass_index < 12 or body_mass_index > 45:
+        print(f'Der für {name} berechnete Body Mass Index von {body_mass_index:.2f} ist unplausibel, muss zwischen 12 und 45 liegen')        
     else:
-        category = 'fettleibig'
-    print(f'{name} ist mit einem Body Mass Index von {body_mass_index:.2f} {category}')
+        if body_mass_index < 18:
+            category = 'untergewichtig'
+        elif body_mass_index < 25:
+            category = 'normalgewichtig'
+        elif body_mass_index < 30:
+            category = 'übergewichtig'
+        else:
+            category = 'fettleibig'
+        print(f'{name} ist mit einem Body Mass Index von {body_mass_index:.2f} {category}')
