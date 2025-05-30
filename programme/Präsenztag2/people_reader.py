a01a101ee8650f0e9c808ad80f93c6a4c0a256f2
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-30 15:10:59 +0200

    Lesen einer Datei mit with (bevorzugte Variante)

--- programme/Präsenztag2/people_reader.py
+++ programme/Präsenztag2/people_reader.py
@@ -2,6 +2,6 @@
 # Dateiinhalt einer Variaben Zuordnen
 # Schließen der Datei
 
-people_file = open('programme/Präsenztag2/people.txt', encoding='utf-8')
-content = people_file.readlines()
-people_file.close()
+with open('programme/Präsenztag2/people.txt', encoding='utf-8') as people_file:
+    content = people_file.readlines()
+print(len(content))
