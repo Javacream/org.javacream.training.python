49288067468adf0867f84424a3aea0cbbadabf47
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-05-30 13:50:36 +0200

    Collections: Liste

--- programme/Präsenztag2/loops.py
+++ programme/Präsenztag2/loops.py
@@ -11,3 +11,17 @@ if candidate in r:
     print(f'{candidate} ist in {r} enthalten')
 else:
     print(f'{candidate} ist in {r} nicht enthalten')
+
+names = ['A', 'B', 'C']
+for name in names:
+    print(name)
+
+
+candidate = 'D'
+if candidate in names:
+    print(f'{candidate} ist in {names} enthalten')
+else:
+    print(f'{candidate} ist in {names} nicht enthalten')
+
+print(f'Länge von names: {len(names)}')
+print(f'Erstes Element von names: {names[1]}')
