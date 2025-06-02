38182bf6bcc7eb0b942ee875d8d89a6550e24fa2
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-06-02 20:14:57 +0200

    Aktiv / Passiv Version 2

--- ip_addresses.py
+++ ip_addresses.py
@@ -30,18 +30,16 @@ for type in types:
 # Aktiven IP-Adressen
 
 active_ip_counter = 0
+inactive_ip_counter = 0
 for s in status:
     if s == '1':
         active_ip_counter += 1
-
-print(f'Anzahl aktiver IP-Adressen: {active_ip_counter} ')
-
-inactive_ip_counter = 0
-for s in status:
-    if s == '0':
+    else:
         inactive_ip_counter += 1
 
+print(f'Anzahl aktiver IP-Adressen: {active_ip_counter} ')
 print(f'Anzahl inaktiver IP-Adressen: {inactive_ip_counter} ')
+
 
 
 print('done')
