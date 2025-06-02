eeb9197d9431296bb12c72e8a7cc7a2226d56b96
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-06-02 20:42:21 +0200

    Aufgabe 5 ohne Expertenwissen

--- ip_addresses.py
+++ ip_addresses.py
@@ -34,6 +34,32 @@ active_ip_counter = sum(status)
 print(f'Anzahl aktiver IP-Adressen: {active_ip_counter} ')
 print(f'Anzahl inaktiver IP-Adressen: {len(status) - active_ip_counter} ')
 
+# Anzahl host / DHCP
+
+host_ips = 0
+
+for type in types:
+    if type == 'host':
+        host_ips +=1
+print(f'Anzahl host: {host_ips} ')
+print(f'Anzahl DHCP: {len(types) - host_ips} ')
+
+active_host_ips = 0
+index = 0
+for type in types:
+    if type == 'host' and status[index] == 1:
+        active_host_ips += 1
+    index += 1
+
+print(f'Anzahl aktiver host: {active_host_ips} ')
+
+index = 0
+with open ('inactive_dns_entries.txt', 'at', encoding='utf-8') as result_file:
+    for dns_name in dns_names:
+        if dns_name != '' and status[index] == 0:
+            print(f'DNS-Eintrag {dns_name} ist inaktiv')
+            result_file.write(f'DNS-Eintrag {dns_name} ist inaktiv\n')
+        index += 1
 
 
 print('done')
