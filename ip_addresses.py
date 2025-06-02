93bc1670f1f884d947d0bc5b326f287827d70973
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-06-02 15:10:13 +0200

    Aufgabe 3

--- ip_addresses.py
+++ ip_addresses.py
@@ -6,3 +6,20 @@ list_of_cleaned_rows = []
 for row in list_of_rows:
     cleaned_row = row.replace('\n', '')
     list_of_cleaned_rows.append(cleaned_row)
+
+list_of_cleaned_rows = list_of_cleaned_rows[1:]
+
+ip_addresses = []
+types = []
+status = []
+dns_names = []
+
+for data in list_of_cleaned_rows:
+    splitted = data.split(',')
+    ip_addresses.append(splitted[0])
+    types.append(splitted[1])
+    status.append(splitted[2])
+    dns_names.append(splitted[3])
+
+
+print('done')
