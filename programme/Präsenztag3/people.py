0bfcb301d8c97ddedea5a87e4d12e5e2cf31c5e3
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-03 15:49:50 +0200

    Lesen der people.csv

--- programme/Präsenztag3/people.py
+++ programme/Präsenztag3/people.py
@@ -1,16 +1,12 @@
 with open('./programme/Präsenztag3/people.csv', 'rt', encoding='utf-8') as people_csv_file:
-    content = people_csv_file.read()
-rows = content.splitlines()
-# strange = content.split('i')
-
-with open('./programme/Präsenztag3/people.csv', 'rt', encoding='utf-8') as people_csv_file:
-    list_of_lines = people_csv_file.readlines()
-
-alternative_rows = []
-for line in list_of_lines:
-    alternative_rows.append(line.replace('\n', ''))
-
-with open('./programme/Präsenztag3/people.csv', 'rt', encoding='utf-8') as people_csv_file:
     rows = people_csv_file.read().splitlines()
 
+for actual_row in rows:
+    list_of_person_data = actual_row.split(',')  
+    # print(f'{list_of_person_data[0]} {list_of_person_data[1]} ist {float(list_of_person_data[2])}kg schwer und {int(list_of_person_data[3])} cm groß')
+    firstname = list_of_person_data[0]
+    lastname = list_of_person_data[1]
+    weight = float(list_of_person_data[2])
+    height = int(list_of_person_data[3])
+    print(f'{firstname} {lastname} ist {weight}kg schwer und {height} cm groß')
 print('done')
