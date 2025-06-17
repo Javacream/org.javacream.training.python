0da367020d1e5b5b1b489154ab802b5c63a535f4
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-17 13:47:34 +0200

    Type Hints

--- programme/Präsenztag7/people.py
+++ programme/Präsenztag7/people.py
@@ -1,10 +1,10 @@
 class Person:
-    def __init__(self, lastname, firstname, weight, height):
+    def __init__(self, lastname: str, firstname: str, weight: float, height: int):
         self.lastname = lastname
         self.firstname = firstname
         self.weight = weight
         self.height = height
-        self.addresses = list()
+        self.addresses: list[Address] = list()
     def say_hello(self):
         return f'Hello, my name is {self.firstname} {self.lastname}'
     
