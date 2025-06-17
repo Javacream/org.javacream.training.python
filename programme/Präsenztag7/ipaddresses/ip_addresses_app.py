7790ae1f2ee154ff93c7c30e6ae0972c4927dfd0
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-17 15:16:16 +0200

    statt comprehension klassische Schleife

--- programme/Präsenztag7/ipaddresses/ip_addresses_app.py
+++ programme/Präsenztag7/ipaddresses/ip_addresses_app.py
@@ -4,10 +4,16 @@ def main():
     service = IpAddressService()
     for i in range (1,5):
         service.create(f'1.2.3.{i}')
-    print(f'Active addresses: {[address.ipv4_address for address in service.get_active()]}')
+
+    for address in service.get_active():
+        print(address.ipv4_address)
     print(f'Passive addresses: {[address.ipv4_address for address in service.get_passive()]}')
+
     service.activate('1.2.3.2')
-    print(f'Active addresses: {[address.ipv4_address for address in service.get_active()]}')
+
+    for address in service.get_active():
+        print(address.ipv4_address)
     print(f'Passive addresses: {[address.ipv4_address for address in service.get_passive()]}')
+
 if __name__ == '__main__':
     main()
--- programme/Präsenztag7/ipaddresses/ipaddresses.py
+++ programme/Präsenztag7/ipaddresses/ipaddresses.py
@@ -19,16 +19,31 @@ class IpAddressService:
         self.ip_addresses[ipv4_address] = new_address
         return new_address
     def get_active(self):
-        return [address for address in self.ip_addresses.values() if address.status == 1]
+        # 0. Leere Ergebnisliste erzeugen
+        # 1. Gib mir alle Values=Werte aus dem Dictionary
+        # 2. Filtere alle Values, deren Status auf 1 gesetzt ist
+        # 3. Füge diese gefilterten Werte der Ergebnisliste hinzu
+        # 4. Gebe diese Liste als Ergebnis zurück
+        
+        result = []
+        addresses = self.ip_addresses.values() 
+        for address in addresses:
+            if address.status == 1:
+                result.append(address)
+        return result
+
     def get_passive(self):
         return [address for address in self.ip_addresses.values() if address.status == 0]
+
     def activate(self, ipv4_address):
         address = self.ip_addresses.get(ipv4_address)
         if address != None:
            address.status = 1 
+
     def deactivate(self, ipv4_address):
         address = self.ip_addresses.get(ipv4_address)
         if address != None:
            address.status = 0 
+
     def delete(self, ipv4_address):
         self.ip_addresses.pop(ipv4_address, None)
