da3010b2c8efc24e1f5d19ac7c8ca59cd75463b2
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-17 12:08:21 +0200

    Eine Person hat exakt eine Adresse

--- programme/Präsenztag7/people.py
+++ programme/Präsenztag7/people.py
@@ -1,8 +1,15 @@
 class Person:
-    def __init__(self, lastname, firstname, weight, height):
+    def __init__(self, lastname, firstname, weight, height, address):
         self.lastname = lastname
         self.firstname = firstname
         self.weight = weight
         self.height = height
+        self.address = address
     def say_hello(self):
         return f'Hello, my name is {self.firstname} {self.lastname}'
+    
+
+class Address:
+    def __init__(self, city, street):
+        self.city = city
+        self.street = street
--- programme/Präsenztag7/people_app.py
+++ programme/Präsenztag7/people_app.py
@@ -1,10 +1,12 @@
 # import people
-from people import Person
+from people import Person, Address
 
 def main():
-    p1 = Person('Sawitzki', 'Rainer', 76.6, 183)
-    p2 = Person('Musterperson', 'Hannah', 66.6, 176)
-    p3 = Person('Eg', 'Al', 96.6, 186)
+    a1 = Address('München', 'Marienplatz')
+    a2 = Address('Berlin', 'Alexanderplatz')
+    p1 = Person('Sawitzki', 'Rainer', 76.6, 183, a1)
+    p2 = Person('Musterperson', 'Hannah', 66.6, 176, a2)
+    p3 = Person('Eg', 'Al', 96.6, 186, a1)
     print(p1.lastname)
     print(p2.lastname)
     print(p2.weight)
