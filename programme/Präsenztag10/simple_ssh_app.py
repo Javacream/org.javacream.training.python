a1c4c43a86d9c0d9d4667619612ac77aa09176dc
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-24 09:54:32 +0200

    paramiko first contact

---
+++ programme/Präsenztag10/simple_ssh_app.py
@@ -0,0 +1,26 @@
+import paramiko
+def ssh_connect():
+    client = paramiko.client.SSHClient()
+    HOST = 'javacream.eu'
+    PORT = 22
+    USER = 'teilnehmer'
+    PASSWORD = 'javacream123!'
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(HOST, port=PORT, username=USER, password=PASSWORD) # Nach Ausführung von Connect wird auf javacream.eu ein Shell-Prozess gestartet
+    return client
+
+def ssh_execute_command(client: paramiko.client.SSHClient, cmd):
+    remote_process = client.exec_command(cmd) # r_stdin, r_stdout, r_stderr = client.exec
+    remote_stdout = remote_process[1]
+    remote_stderr = remote_process[2]
+    result = (remote_stdout.read().decode(), remote_stderr.read().decode())
+    return result
+
+def main():
+    client = ssh_connect()
+    result = ssh_execute_command(client, 'ls')
+    print(result)
+    client.close()
+
+if __name__ == '__main__':
+    main()
