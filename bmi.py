0207010ae4a9ddb5521c054e0ee18332f8a34b85
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-25 12:14:57 +0200

    ToDo Slide 21

--- bmi.py
+++ bmi.py
@@ -1,9 +1,10 @@
-print('starting...')
-weight  = 76.6 
-height = 183
-name = 'Hugo'
+name = input('Enter your name: ')
+weight  = input('Enter your weight in kg: ') 
+height  = input('Enter your height in cm: ') 
 
+weight = float(weight)
+height = int(height)
 height = height / 100 
 bmi = weight / (height * height) 
 
-print(bmi)
+print(f'{name} has a body mass index of {bmi:.2f}')
