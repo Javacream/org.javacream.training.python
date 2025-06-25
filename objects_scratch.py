5a6fbfb0693bec6d3e2c50787a2a6700e901d485
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-25 15:53:03 +0200

    dot-operator

---
+++ objects_scratch.py
@@ -0,0 +1,11 @@
+todos_sawitzki = list() # = []
+todos_participants = list()
+
+todos_sawitzki.append("speak")
+todos_participants.append('ask')
+todos_sawitzki.append("answer")
+todos_participants.append("hear")
+
+collection = dict()
+# collection.
+print('done')
