68121de8652fb107263323f522cb3c4f068adc45
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-25 14:12:37 +0200

    DRY und sprechende Variablen bzw. Konstanten

--- bmi.py
+++ bmi.py
@@ -1,15 +1,20 @@
+MIN_WEIGHT = 3
+MAX_WEIGHT = 333
+MIN_HEIGHT = 30
+MAX_HEIGHT = 250
+
 while True:
     name = input('Enter your name: ')
     weight  = input('Enter your weight in kg: ') 
 
     weight = float(weight)
-    if weight < 3 or weight > 333:
-        print(f'you entered an invalid weight: {weight}, must be between 3 and 333')
+    if weight < MIN_WEIGHT or weight > MAX_WEIGHT:
+        print(f'you entered an invalid weight: {weight}, must be between {MIN_WEIGHT} and {MAX_WEIGHT}')
     else:    
         height  = input('Enter your height in cm: ') 
         height = int(height)
-        if height < 30 or height > 250:
-            print(f'you entered an invalid height: {height}, must be between 30 and 250')
+        if height < MIN_HEIGHT or height > MAX_HEIGHT:
+            print(f'you entered an invalid height: {height}, must be between {MIN_HEIGHT} and {MAX_HEIGHT}')
         else:
             height = height / 100 
             bmi = weight / (height * height) 
