cec323857432f408085ded8014e8f4060cd673cc
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-25 13:42:22 +0200

    ToDo Slide 30

--- bmi.py
+++ bmi.py
@@ -1,10 +1,19 @@
-name = input('Enter your name: ')
-weight  = input('Enter your weight in kg: ') 
-height  = input('Enter your height in cm: ') 
+while True:
+    name = input('Enter your name: ')
+    weight  = input('Enter your weight in kg: ') 
 
-weight = float(weight)
-height = int(height)
-height = height / 100 
-bmi = weight / (height * height) 
-
-print(f'{name} has a body mass index of {bmi:.2f}')
+    weight = float(weight)
+    if weight < 3 or weight > 333:
+        print(f'you entered an invalid weight: {weight}, must be between 3 and 333')
+    else:    
+        height  = input('Enter your height in cm: ') 
+        height = int(height)
+        if height < 30 or height > 250:
+            print(f'you entered an invalid height: {height}, must be between 30 and 250')
+        else:
+            height = height / 100 
+            bmi = weight / (height * height) 
+            print(f'{name} has a body mass index of {bmi:.2f}')
+    again = input('Again? y|n: ')
+    if again == 'n':
+        break
