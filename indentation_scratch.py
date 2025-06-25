dbbae094effd1cb1b5abd4ae94606416ba4aea45
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-25 13:33:11 +0200

    indents

---
+++ indentation_scratch.py
@@ -0,0 +1,12 @@
+number = 42
+
+if number < 40:
+    print(f'{number} is smaller than 40')
+else:
+    print('...')
+    print(f'{number} is greater than or equal 40')
+
+if number < 30:
+    pass
+else:
+    pass    
