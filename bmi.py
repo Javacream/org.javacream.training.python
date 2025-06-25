c24b451ab13fef9458b580ce0bb7814abf927895
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-25 11:41:45 +0200

    bmi for debugging

--- bmi.py
+++ bmi.py
@@ -1,10 +1,9 @@
-# Dieses Skript berechnet einen BMI-Wert, siehe Wikipedia
+print('starting...')
+weight  = 76.6 
+height = 183
+name = 'Hugo'
 
-weight = 76.6 # Fließkommazahl mit dem Punkt als Trennzeichen
-height = 183 # Ganzzahhl
-name = 'Hugo' # Zeichenkette mit beliebiger Länge, eingeschlossen in einfache oder doppelte Hochkommas
-
-height_in_meter = height / 100 # Variablennamen in Python nur Kleinbuchstaben und _ zur Unterteilung
-bmi = weight / (height_in_meter * height_in_meter) # weight / (height **2), also ** -> Pozenzieren
+height = height / 100 
+bmi = weight / (height * height) 
 
 print(bmi)
