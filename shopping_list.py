25d008075fe691fdf935bf51747fd0ffe1bfffe7
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-26 10:26:38 +0200

    Comprehension

--- shopping_list.py
+++ shopping_list.py
@@ -1,9 +1,5 @@
 path = 'data/raw_shopping_list.txt'
 with open(path, 'rt', encoding='utf-8') as file:
     raw_rows = file.readlines()
-rows = []
-for raw_row in raw_rows:
-    cleaned_row = raw_row.replace('\n', '')
-    if cleaned_row.strip() != '':
-        rows.append(cleaned_row)
+rows = [raw_row.replace('\n', '') for raw_row in raw_rows if raw_row.replace('\n', '').strip() != '']
 print(rows)
