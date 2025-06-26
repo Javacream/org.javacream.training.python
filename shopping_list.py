cd0f243dff6609f69eae6fb99cb64cad3615b327
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-26 12:04:46 +0200

    Type Hints

--- shopping_list.py
+++ shopping_list.py
@@ -5,11 +5,12 @@ def read_raw_shopping_list():
     with open(path, 'rt', encoding='utf-8') as file:
         raw_rows = file.readlines()
     return raw_rows
-def clean(raw_rows):
+
+def clean(raw_rows: list[str]):
     rows = [raw_row.replace('\n', '') for raw_row in raw_rows if raw_row.replace('\n', '').strip() != '']
     return rows
 
-def create_shopping_list(rows):
+def create_shopping_list(rows: list[str]):
     items = [row.split(SEPARATOR)[1] for row in rows]
     unique_items = set(items)
     shopping_list = []
@@ -18,12 +19,12 @@ def create_shopping_list(rows):
     with open('data/shopping_list.txt', 'wt', encoding='utf-8') as file:
         file.writelines(shopping_list)
 
-def calculate_number_of_people(rows):
+def calculate_number_of_people(rows: list[str]):
     people = [row.split(SEPARATOR)[0] for row in rows]
     unique_people = set(people)
     print(f'{len(unique_people)} people need items')
 
-def group_items(rows):
+def group_items(rows: list[str]):
     grouped_items = dict()
     for row in rows:
         splitted = row.split(SEPARATOR)
@@ -40,4 +41,5 @@ def main():
     create_shopping_list(cleaned_data)
     calculate_number_of_people(cleaned_data)
     group_items(cleaned_data)
+
 main()    
