33745327f1c1e7a8289c6bb67cd4cd4a3ed4017e
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-26 09:39:22 +0200

    file part 3 with exception handling

--- shopping_list.py
+++ shopping_list.py
@@ -1,5 +1,10 @@
-path = 'data/raw_shopping_list.txt'
-file = open(path, 'rt', encoding='utf-8')
-content = file.read()
-file.close()
-print(content)
+try:
+    path = 'data/raw_shopping_list.txt'
+    file = open(path, 'rt', encoding='utf-8')
+    content = file.read()
+    print(content)
+except:
+    print('something went wrong')
+finally:
+    if file:
+        file.close()
