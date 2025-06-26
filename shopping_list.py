a6c7458750b85e13818773ccd7b791cf4f9ba403
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-26 09:42:27 +0200

    file read, best practice with with-statement

--- shopping_list.py
+++ shopping_list.py
@@ -1,10 +1,4 @@
-try:
-    path = 'data/raw_shopping_list.txt'
-    file = open(path, 'rt', encoding='utf-8')
+path = 'data/raw_shopping_list.txt'
+with open(path, 'rt', encoding='utf-8') as file:
     content = file.read()
-    print(content)
-except:
-    print('something went wrong')
-finally:
-    if file:
-        file.close()
+print(content)
