2335afd6108cb1c7c42a2d017f3536a6ee0b6f0c
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-07-21 14:22:54 +0200

    repitition

--- bmi.py
+++ bmi.py
@@ -1,17 +1,17 @@
-height = input('Please enter your height: ')
-weight = input('Please enter your weight: ')
-name = input('Please enter your name: ')
-height = int(height)
-weight = float(weight)
-body_mass_index = weight /(height*height) * 10000
+while True:
+    height = input('Please enter your height: ')
+    weight = input('Please enter your weight: ')
+    name = input('Please enter your name: ')
+    height = int(height)
+    weight = float(weight)
+    body_mass_index = weight /(height*height) * 10000
+    if body_mass_index < 18:
+        bmi_category = 'underweight'
+    elif body_mass_index < 25:
+        bmi_category = 'normalweight'
+    elif body_mass_index < 30:
+        bmi_category = 'overweight'
+    else:
+        bmi_category = 'obese'
 
-if body_mass_index < 18:
-    bmi_category = 'underweight'
-elif body_mass_index < 25:
-    bmi_category = 'normalweight'
-elif body_mass_index < 30:
-    bmi_category = 'overweight'
-else:
-    bmi_category = 'obese'
-
-print(f'the person {name} is {bmi_category}')
+    print(f'the person {name} is {bmi_category}')
