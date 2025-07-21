59c9262dc0964cd3109094a2fdad534f9546c848
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-07-21 12:06:03 +0200

    input an type conversion

--- first.py
+++ first.py
@@ -1,5 +1,7 @@
-height = 160
-weight = 95.7
-name = 'Sawitzki'
+height = input('Please enter your height: ')
+weight = input('Please enter your weight: ')
+name = input('Please enter your name: ')
+height = int(height)
+weight = float(weight)
 body_mass_index = weight /(height*height) * 10000
 print(f'the person {name} has a body mass index of {body_mass_index:.2f}')
