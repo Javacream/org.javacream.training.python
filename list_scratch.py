c7863e5ba5532f3456e017c506961e39414fe48b
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-07-23 09:55:33 +0200

    append method

--- list_scratch.py
+++ list_scratch.py
@@ -4,9 +4,4 @@ print(name_with_index_1)
 for e in names:
     print(e)
 
-# alternative iteration
-number_of_elements = len(names)
-counter = 0
-while counter < number_of_elements:
-    print(names[counter])
-    counter = counter + 1
+names.append('John')
