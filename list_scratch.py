2e7aaa70151258a1c0d984f9db16f0f33354eea1
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-07-23 13:24:57 +0200

    range, if in, slicing

--- list_scratch.py
+++ list_scratch.py
@@ -9,3 +9,16 @@ names.append('John')
 print('Hello')
 # append('John')
 # names.print('Hello')
+
+sub_names = names[1:3:2]
+print(sub_names)
+
+# details
+
+# defaults
+sub_names = names[:3] # default start = 0
+sub_names = names[1:] # default end = len(names)
+sub_names = names[:] # possible, but names.copy() is a better alternative
+sub_names = names[::-1] # reverted list
+print(sub_names)
+print("Hugo"[::-1])
---
+++ range_scratch.py
@@ -0,0 +1,10 @@
+r = range(1, 8)
+
+for number in r:
+    print(number)
+
+
+if 4 in r:
+    print('4 is part of r')
+else:
+    print('4 is not part of r')
