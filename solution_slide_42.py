d043ebbebe56400a6d6536580cf739028125eb2c
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-07-23 11:41:17 +0200

    solution slide 42

---
+++ solution_slide_42.py
@@ -0,0 +1,33 @@
+def number_of_characters(string):
+    return len(string)
+
+def number_of_character(char, string):
+    return string.count(char)
+
+def number_of_words(string):
+    return len(string.split(' '))
+
+def is_int(string):
+    return string.isdigit()
+
+def is_float(string):
+    if string.count('.') > 1:
+        return False
+    else:
+        string = string.replace('.', '')
+    return string.isdigit()
+
+def main():
+    test_string = 'Hello my friend'
+    print(f'numbers of characters: {number_of_characters(test_string)}')
+    print(f'numbers of character e: {number_of_character('e', test_string)}')
+    print(f'numbers of words: {number_of_words(test_string)}')
+    test_int_string = '123'
+    print(f'is 123 numeric: {is_int(test_int_string)}')
+    print(f'is 12x3 numeric: {is_int("12x3")}')
+    test_float_string = '12.3'
+    print(f'is 12.3 numeric: {is_float(test_float_string)}')
+    print(f'is 1.2.3 numeric: {is_float("1.2.3")}')
+
+
+main()
