e49c37962db6d3b6e4b661ab8d62ab7cde255f3b
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-07-23 09:30:33 +0200

    iteration with while

--- list_scratch.py
+++ list_scratch.py
@@ -3,3 +3,10 @@ name_with_index_1 = names[2]
 print(name_with_index_1)
 for e in names:
     print(e)
+
+# alternative iteration
+number_of_elements = len(names)
+counter = 0
+while counter < number_of_elements:
+    print(names[counter])
+    counter = counter + 1
