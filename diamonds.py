581d57ba7e6cacc8c7daec507250391abc5a92da
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-07-28 15:39:25 +0200

    hist

--- diamonds.py
+++ diamonds.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def main():
     df = pd.read_csv('diamonds.csv')
-    print(df.head())
-    print(df.info())
-    print(df.describe())
+    #print(df.head())
+    #print(df.info())
+    #print(df.describe())
+    df['carat'].hist()
+    plt.savefig('diamonds_carat.png')
 main()
