1394935896b544096f67b137dfb382c75be580ec
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-09 14:57:53 +0200

    start with calculator

---
+++ programme/block1/simple_calculator.py
@@ -0,0 +1,16 @@
+number1 = 20.4
+number2 = 21.6
+
+# sum = number1+ number2 # sum ist eine BuiltIn-Function
+
+sum_result = number1 + number2
+
+print('Die Summe von ' + str(number1) + ' und ' + str(number2) + ' ist ' + str(sum_result))
+
+difference_result = number1 - number2
+
+print(f'Die Differenz von {number1} und {number2} ist {difference_result}')
+
+print(f'Das Produkt von {number1} und {number2} ist {number1 * number2}')
+
+print(f'Der Quotient von {number1} und {number2} ist {number1 / number2}')
