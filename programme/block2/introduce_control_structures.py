7a376da924267603add630e5a1b30cf09c07c5ee
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-09 15:15:30 +0200

    introduce indents as syntax

---
+++ programme/block2/introduce_control_structures.py
@@ -0,0 +1,24 @@
+number1 = 20
+number2 = 22
+
+# Pseudocode
+# if number1 < number2 then print('was less') else print('was greater than'))
+
+# e.g. Java / C# are block-oriented, block using curly braces 
+#if (number1 < number2) {print('was less')} else {print('was greater than')}
+# best practice: readability
+
+# if (number1 < number2) {
+#     print('was less')
+# } 
+# else {
+#     print('was greater than')
+# }
+
+# Python uses indents as syntax
+if number1 < number2:
+    print('was less')
+    print('done')
+else:
+    print('was greater than')
+
