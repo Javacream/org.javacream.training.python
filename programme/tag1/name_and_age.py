d8932144a6088b9720117c2093c6d568ba1d1d29
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-10 11:53:40 +0200

    solution slide 21

--- programme/tag1/name_and_age.py
+++ programme/tag1/name_and_age.py
@@ -1,3 +1,5 @@
 name = input('Bitte Namen eingeben: ')
 age = input('Bitte Alter eingeben: ')
-print(name, age)
+#output_message = name + ' ist ' + age + ' Jahre alt'
+output_message = f'{name} ist {age} Jahre alt'
+print(output_message)
