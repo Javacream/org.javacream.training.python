f2be85e9f711b83a84f94c884de68cbbbfa38307
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-10 14:25:36 +0200

    operators details

---
+++ programme/tag1/details_operators.py
@@ -0,0 +1,26 @@
+# +-operator
+
+number1 = 4
+number2 = 38
+
+# sum = number1 + number2 # sum ist der Name einer BuiltIn-Function
+
+plus_result = number1 + number2
+print(plus_result)
+
+str1 = 'Hu'
+str2 = 'go'
+
+plus_result = str1 + str2
+print(plus_result)
+
+# plus_result = str1 + number1
+plus_result = str1 + str(number1)
+print(plus_result)
+
+# *-Operator
+
+times_result = number1 * number2
+# times_result = str1 * str2
+times_result = number1 * str1
+print(times_result)
