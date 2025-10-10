d98fd6c8daa7be0125957411c5fe895d39d8927c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-10 14:17:38 +0200

    exponentiation

--- programme/tag1/bmi_calculator.py
+++ programme/tag1/bmi_calculator.py
@@ -5,7 +5,7 @@ height = input('Bitte Größe in cm eingeben: ')
 weight = float(weight)
 height = int(height)
 height = height / 100
-bmi = weight / (height * height)
+bmi = weight / (height ** 2)
 
 result = f'{name} hat einen Body Mass Index von {bmi:.2f}'
 
