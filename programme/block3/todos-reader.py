917b20eb1da28ebd257394ff7026e63d1616dbac
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-24 15:26:22 +0200

    totdos reader, version2

--- programme/block3/todos-reader.py
+++ programme/block3/todos-reader.py
@@ -1,8 +1,8 @@
 try:
     with open('data/info.txt', encoding='utf-8') as file:
-        content = file.readlines()
-    for raw_todo in content:
-        todo = raw_todo.replace('\n', '')
+        content = file.read()
+    todos = content.split('\n')
+    for todo in todos:
         print(todo)
 except Exception as e:
     print(e)
