a5f53da94b1805fb18ed278f7fe829b73b9c8f66
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-24 14:11:04 +0200

    set exercise

---
+++ programme/block3/Aufgaben_Set/Aufgaben.py
@@ -0,0 +1,17 @@
+set1 = {'A', 'B', 'C', 'K', 'L'}
+set2 = {'C', 'L', 'E'}
+
+for element in set1:
+    if element in set2:
+        print(f'{element} ist im Schnitt von set1 und set2 enthalten')
+
+for element in set1:
+    if not element in set2:
+        print(f'{element} ist Bestandteil der Vereinigung von set1 und set2')
+for element in set2:
+    print(f'{element} ist Bestandteil der Vereinigung von set1 und set2')
+
+
+print(set1.union(set2))
+print(set1.intersection(set2))
+        
