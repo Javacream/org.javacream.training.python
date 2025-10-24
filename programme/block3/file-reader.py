570e54d38d2e953a7e7d81c9a4a6b00ac20f8aac
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-24 15:15:11 +0200

    file reader template

--- programme/block3/file-reader.py
+++ programme/block3/file-reader.py
@@ -1,7 +1,6 @@
 try:
-    file = open('data/info.txt', encoding='utf-8')
-    content = file.read()
-    file.close()
+    with open('data/info.txt', encoding='utf-8') as file:
+        content = file.read()
     print(content)
 except Exception as e:
     print(e)
