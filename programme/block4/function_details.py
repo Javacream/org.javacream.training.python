b16f91bc73e0a70a2a7de7f628b5cc77f17bae3b
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-30 11:22:52 +0100

    kwargs

--- programme/block4/function_details.py
+++ programme/block4/function_details.py
@@ -1,33 +1,13 @@
-def sum_elements(numbers):
-    result = 0
-    for number in numbers:
-        result += number
-    return result
+def do_something(dict_param):
+    for key in dict_param:
+        print(f'{key}={dict_param[key]}')
 
-def sum_elements_varargs(*numbers):
-    result = 0
-    for number in numbers:
-        result += number
-    return result
-
-def sum_elements_varargs_with_param(offset, *numbers):
-    result = offset
-    for number in numbers:
-        result += number
-    return result
-
-def sum_elements_param_with_varargs(*numbers, offset):
-    result = offset
-    for number in numbers:
-        result += number
-    return result
-
+def do_something_with_kwargs(**dict_param):
+    for key in dict_param:
+        print(f'{key}={dict_param[key]}')
 
 def main():
-    values = (1, 6, 4, 8)
-    print(sum_elements(values))
-    print(sum_elements_varargs(1, 6, 4, 8))
-    print(sum_elements_varargs_with_param(-42, 1,2,3))
-    # print(sum_elements_param_with_varargs(-42, 1,2,3))
-    print(sum_elements_param_with_varargs(1,2,3, offset=42))
+    person = {'name': 'Rainer Sawitzki', 'height': 183, 'weight': 75.8}
+    do_something(person)
+    do_something_with_kwargs(name='Rainer Sawitzki', height=183, weight=75.8)
 main()
