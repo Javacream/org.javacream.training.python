ff997a909830e94e4494c85cbd1bf76654fe0e65
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-30 10:06:52 +0100

    final solution1 with main

--- programme/block4/solution1.py
+++ programme/block4/solution1.py
@@ -4,6 +4,9 @@ def sum_elements(number_list):
         result = result + number
     return result
 
-list_of_numbers = [1, 7, -5, 28]
-sum_of_list = sum_elements(list_of_numbers)
-print(sum_of_list)
+def main():
+    list_of_numbers = [1, 7, -5, 28]
+    sum_of_list = sum_elements(list_of_numbers) # implizit wird hier intern ausgeführt: number_list = list_of_numbers
+    print(sum_of_list)
+
+main()
