2f30d53dd35bf8410b709fb663e0823cfd5cedae
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-31 13:59:39 +0100

    multiple return values

--- programme/block4/exercise6.py
+++ programme/block4/exercise6.py
@@ -1,8 +1,15 @@
-import datetime
+from datetime import datetime as calendar
 
 def get_date_and_time():
-    return datetime.datetime.now()
+    actual_datetime = calendar.now()
+    actual_date = actual_datetime.date()
+    actual_time = actual_datetime.time()
+    return actual_date, actual_time
 
 def main():
-    print(get_date_and_time())
+    datetime = get_date_and_time()
+    # date = datetime[0]
+    # time = datetime[1]
+    date, time = datetime
+    print(f'Date: {date}, time:{time}')
 main()
