3b3343965408e228004c82ecd7b17a7937ead552
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-31 11:39:43 +0100

    keyworded arguments

--- programme/block4/params_scratch.py
+++ programme/block4/params_scratch.py
@@ -4,9 +4,10 @@ def sum_elements(numbers):
 def sum_elements2(*numbers):
     return sum(numbers)
 
-def main():
-    n = (1, 5, 2, -4, 3)
-    print(sum_elements(n))
-    print(sum_elements2(1, 5, 2, -4, 3))
+def sum_elements3(*numbers, offset, multiplier):
+    return multiplier*(offset + sum(numbers))
 
+
+def main():
+    print(sum_elements3(1, 2, 3, 4, offset=42, multiplier=-1))
 main()
