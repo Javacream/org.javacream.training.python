f1274646de8397698cff37a50cc6f69b093789ef
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-31 15:24:51 +0100

    advanced functions

--- programme/block4/function_scratch.py
+++ programme/block4/function_scratch.py
@@ -1,11 +1,29 @@
-def fn1():
-    global message
-    message = 'CHANGED'
-    print(message)
+def do_something():
+    print('doing something')
+
+do_something()
+
+def do_something(p):
+    print(f'doing something completely different with {p}')
+
+do_something(True)
+
+name = 'Sawitzki'
+name = 'Musterperson'
+
+x = do_something
+x(42)
+
+def decorate(fn):
+    print('before')
+    fn('Hugo')
+    print('after')
+
 
 def main():
-    fn1()
-    print(message)
-
-message = 'global var'
+    message = 'Hello'
+    def inner_function():
+        print('inner function')
+    inner_function()
+    decorate(do_something) 
 main()
