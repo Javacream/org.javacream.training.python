a0e21d07db0c6ae9a9ec188e4e735779325772e0
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-31 09:51:24 +0100

    solution exercise1

--- programme/block4/exercise1.py
+++ programme/block4/exercise1.py
@@ -4,6 +4,9 @@ def sum_elements(number_list):
         result = result + number
     return result
 
-list_of_numbers = [1, 7, 19, -4]
-sum_of_numbers = sum_elements(list_of_numbers)
-print(sum_of_numbers)
+def main():
+    list_of_numbers = [1, 7, 19, -4]
+    sum_of_numbers = sum_elements(list_of_numbers)
+    print(sum_of_numbers)
+
+main()
