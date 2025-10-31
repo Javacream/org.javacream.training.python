f94d4f79b8ac2b457909866e39f92b15afa7ed61
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-10-31 13:31:50 +0100

    module scratch

---
+++ programme/block4/module_scratch.py
@@ -0,0 +1,9 @@
+import os as operating_system
+from os import listdir, mkdir
+from os import listdir as ls
+def main():
+    os = 'Hugo'
+    print(operating_system.listdir())
+    print(listdir())
+    print(ls())
+main()
