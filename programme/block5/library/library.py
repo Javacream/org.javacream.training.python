6184f2671c143ae898af4b397843db59b1853869
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-06 14:00:30 +0100

    solution 2

---
+++ programme/block5/library/library.py
@@ -0,0 +1,24 @@
+class Book:
+    def __init__(self, title:str, author: str, genre: str, status=False):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.status = status
+
+class Library:
+    def __init__(self):
+        self.books = set()
+
+    def append(self, book: Book):
+        self.books.add(book)
+
+    def lend(book: Book):
+        book.status = True
+
+    def show(self) -> list[Book]:
+        result = []
+        for book in self.books:
+            if book.status == False:
+                result.append(book)
+        return result
+
