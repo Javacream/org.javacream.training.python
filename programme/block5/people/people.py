e5c2a664a4967d4b4f019ae84421afd4645991fb
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-06 11:24:27 +0100

    start with people

---
+++ programme/block5/people/people.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, lastname: str, firstname: str, height: int, weight: float):
+        self.lastname = lastname
+        self.firstname = firstname
+        self.height = height
+        self.weight = weight
+
+
+
---
+++ programme/block5/people/people_app.py
@@ -0,0 +1,10 @@
+from people import Person
+
+def main():
+    p1 = Person('Sawitzki', 'Rainer', 183, 76.2)
+    p2 = Person('Musterperson', 'Hannah', 179, 66.2)
+
+    print(p1.lastname)
+    print(p2.lastname)
+
+main()
