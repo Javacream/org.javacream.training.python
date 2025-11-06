9895ff0f3e6bb5e3f1dade24ef3b07bf0308eae8
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-06 11:39:47 +0100

    solution slide 100

--- programme/block5/people/people.py
+++ programme/block5/people/people.py
@@ -5,5 +5,9 @@ class Person:
         self.height = height
         self.weight = weight
 
+class Address:
+    def __init__(self, city:str, street: str):
+        self.city = city
+        self.stree = street
 
 
--- programme/block5/people/people_app.py
+++ programme/block5/people/people_app.py
@@ -1,10 +1,22 @@
-from people import Person
+from people import Person, Address
 
 def main():
     p1 = Person('Sawitzki', 'Rainer', 183, 76.2)
     p2 = Person('Musterperson', 'Hannah', 179, 66.2)
+    a1 = Address('München', 'Marienplatz')
+    a2 = Address('Berlin', 'Alexanderplatz')
 
-    print(p1.lastname)
-    print(p2.lastname)
+
+    print(p1.lastname, p2.lastname)
+    print(a1.city, a2.city)
+
+    p1.weight = 75.8
+    print(p1.weight)
+
+    p1.eye_color = 'blue'
+    print(p1.eye_color)
+    # print(p2.eye_color) # error: p2 has no attribute eye_color
+
+
 
 main()
