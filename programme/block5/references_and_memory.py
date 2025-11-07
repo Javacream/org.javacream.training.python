7cc6f495d69778a60fae4841878a092c25bb03a1
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-07 10:09:12 +0100

    simplified references

--- programme/block5/references_and_memory.py
+++ programme/block5/references_and_memory.py
@@ -1,15 +1,10 @@
-def my_function(names_list, text, n):
-   # names_list.append('Andrea')
-    names_list = ['Andrea']
-    text ='CHANGED'
-    n = 42
-    print(f'In my_function: Names: {names_list}, text={text}, n={n}')
-
 def main():
     message = 'Hello'
     number = 9
     names = ['Hugo', 'Emil', 'Hannah']
-    my_function(names, message, number) # implizit: names_list = names, text = message, n number
-    print(f'In main: Names {names}, message={message}, number={number}')
-
+    names2 = names
+    names2.append('Andrea')
+    print(names)
+    number = 42
+    message = 'CHANGED'
 main()
