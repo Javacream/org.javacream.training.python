64f0df3e9be1815651ec93a98658f7a16b0616e4
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-07 11:28:26 +0100

    start classes

---
+++ programme/block5/people/people-app.py
@@ -0,0 +1,7 @@
+from people import Person
+def main():
+    p1 = Person('Sawitzki', 'Rainer', 183, 75.9)
+    p2 = Person('Musterperson', 'Hannah', 173, 55.9)
+    print(p1.lastname, p2.lastname)
+
+main()
