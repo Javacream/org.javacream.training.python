c17fb239839d3a7fe4a0d762bb4bbdb5552989d9
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-10 12:00:49 +0100

    dunder eq and gt

--- programme/block6/people/people.py
+++ programme/block6/people/people.py
@@ -5,7 +5,12 @@ class Address:
     def copy(self):
         return Address(self.city, self.street)
     def __repr__(self):
-        return f'Address(city={self.city}, street={self.street})'   
+        return f'Address(city={self.city}, street={self.street})'
+    def __eq__(self, other):
+        if isinstance(other, Address):
+            return (self.city == other.city) and (self.street == other.street)
+        else:
+            return False
 class Person(object):
     def __init__(self, lastname: str, firstname: str, height: int, weight: float, address: Address):
         self.lastname = lastname
@@ -19,6 +24,11 @@ class Person(object):
     def __repr__(self):
         return f'Person(lastname={self.lastname}, firstname={self.firstname}, height={self.height}, weight={self.weight})'
 
+    def __gt__(self, other):
+        if isinstance(other, Person):
+            return self.height > other.height
+        else:
+            return False
 class Student(Person):
     def __init__(self, lastname: str, firstname: str, height: int, weight: float, address: Address, university: str):
         super().__init__(lastname, firstname, height, weight, address)        
--- programme/block6/people/people_tests.py
+++ programme/block6/people/people_tests.py
@@ -19,4 +19,31 @@ class PeopleTests(unittest.TestCase):
         # representation = p.__repr__()
         representation = repr(p)
         self.assertEqual(expected_representation, representation)
+
+    def test_people_equality(self):
+        p1 = Person('A', 'B', 188, 76.6, Address('some', 'where'))
+        p2 = Person('X', 'Y', 148, 96.6, Address('some', 'where'))
+        p3 = p1
+        p4 = Person('A', 'B', 188, 76.6, Address('some', 'where'))
+
+        self.assertFalse(p1 == p2)
+        self.assertTrue(p1 == p3)
+        self.assertFalse(p1 == p4)
+
+
+    def test_address_equality(self):
+        a1 = Address('some', 'where')
+        a2 = Address('any', 'place')
+        a3 = a1
+        a4 = Address('some', 'where')
+
+        self.assertFalse(a1 == a2)
+        self.assertTrue(a1 == a3)
+        self.assertTrue(a1 == a4)
+
+    def test_people_greatness(self):
+        p1 = Person('A', 'B', 188, 76.6, Address('some', 'where'))
+        p2 = Person('X', 'Y', 148, 96.6, Address('some', 'where'))
+        self.assertTrue(p1 > p2)
+
 unittest.main()
