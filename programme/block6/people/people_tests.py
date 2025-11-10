3a78b68a341ebb2a4daf244c8b2b6b0211566b5d
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-10 11:20:31 +0100

    dunder repr

--- programme/block6/people/people_tests.py
+++ programme/block6/people/people_tests.py
@@ -2,11 +2,6 @@ import unittest
 from people import Person, Address
 
 class PeopleTests(unittest.TestCase):
-    def test_demo(self):
-        data = 'Hugo'
-        expeceted_result = 'H'
-        result = data[0]
-        self.assertEqual(expeceted_result, result)
 
     def test_say_hello(self):
         p = Person('A', 'B', 188, 76.6, Address('some', 'where'))
@@ -14,4 +9,11 @@ class PeopleTests(unittest.TestCase):
         result = p.say_hello()
         self.assertEqual(expeceted_result, result)
 
+
+    def test_print_person(self):
+        p = Person('A', 'B', 188, 76.6, Address('some', 'where'))
+        expected_representation_starts_with = '<people.Person object at '
+        # representation = p.__repr__()
+        representation = repr(p)
+        self.assertTrue(representation.startswith(expected_representation_starts_with), f'{representation} does not start with {expected_representation_starts_with}')
 unittest.main()
