176b65a4863650f0596d60836ee5f3a9b48ed780
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-11 10:00:20 +0100

    finish test and implementation

--- programme/block6/bmi/bmi.py
+++ programme/block6/bmi/bmi.py
@@ -1,5 +1,14 @@
-def calculate_bmi(weight: float, height: int) -> float:
-    pass
+def calculate_bmi(weight: float, height: int) -> float:#
+    height = height/100
+    bmi = weight/(height*height)
+    return bmi
 
 def categorize_bmi(bmi: float) -> str:
-    pass
+    if bmi < 18.5:
+        return 'underweight'
+    elif bmi < 24.9:
+        return 'normalweight'
+    elif bmi < 30:
+        return 'overweight'
+    else:
+        return 'obese'
--- programme/block6/bmi/bmi_tests.py
+++ programme/block6/bmi/bmi_tests.py
@@ -5,18 +5,18 @@ class BmiTests(unittest.TestCase):
 
     def test_weight_75_3_and_height_183_calculates_bmi_22_0(self):
         weight = 75.3
-        height = 183
+        height = 185
         expected_bmi = 22.0
         calculated_bmi = calculate_bmi(weight, height)
         self.assertAlmostEqual(expected_bmi, calculated_bmi, 1)
     def test_bmi_16_is_underweight(self):
         bmi = 16
         expected_category = 'underweight'
-        calculated_category = None
+        calculated_category = categorize_bmi(bmi)
         self.assertEqual(expected_category, calculated_category)
-    def test_bmi_18_5_is_underweight(self):
+    def test_bmi_18_5_is_normalweight(self):
         bmi = 18.5
-        expected_category = 'underweight'
+        expected_category = 'normalweight'
         calculated_category = categorize_bmi(bmi)
         self.assertEqual(expected_category, calculated_category)
 
