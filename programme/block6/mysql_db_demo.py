f2d1ad12cb2b81a29664729f98f1441afa49f8b7
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-11 14:06:30 +0100

    mysql select demo

---
+++ programme/block6/mysql_db_demo.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+def main():
+    connection = mysql.connector.connect(
+        host='javacream.eu',
+        port=3406,
+        database='javacream', 
+        user='user',
+        password='user'
+    )
+    cursor = connection.cursor()
+
+    read_sql = f"SELECT * FROM PEOPLE"
+    cursor.execute(read_sql)
+    rows = cursor.fetchmany(10)
+
+    for row in rows:
+        print(row)
+
+    connection.close()
+if __name__ == '__main__':
+    main()
