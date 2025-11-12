e8be7b4b7a70b47c7bd1ac33427ff5e59a0f93b9
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-12 14:04:00 +0100

    play with type

---
+++ types_scratch.py
@@ -0,0 +1,15 @@
+name = 'Sawitzki'
+height = '183'
+weight = '76.3'
+
+type_of_name = type(name)
+print(type_of_name)
+
+print(type(height))
+print(type(weight))
+
+weight = float(weight)
+height = int(height)
+
+print(type(height))
+print(type(weight))
