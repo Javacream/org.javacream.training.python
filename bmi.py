a1da85f36332289b3bc7358c804f923a822abf60
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-12 14:07:34 +0100

    bmi with user input and type conversion

--- bmi.py
+++ bmi.py
@@ -1,4 +1,8 @@
-weight = 76.3
-height = 1.83
+name = input('please enter your name: ')
+weight = input(f'{name}, please enter your weight in kg: ')
+height = input(f'{name}, please enter your height in cm: ')
+weight = float(weight)
+height = int(height)
+height = height / 100
 body_mass_index = weight/(height*height)
-print(f'the calculated bmi is: {body_mass_index:.2f}')
+print(f'the calculated bmi for {name} is: {body_mass_index:.2f}')
