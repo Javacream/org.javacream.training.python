54fcf877fc5058d1761cd22e30d6bf43ec7ca072
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-12 15:19:58 +0100

    solution slide 35

--- bmi.py
+++ bmi.py
@@ -5,4 +5,14 @@ weight = float(weight)
 height = int(height)
 height = height / 100
 body_mass_index = weight/(height*height)
-print(f'the calculated bmi for {name} is: {body_mass_index:.2f}')
+if body_mass_index < 18.5:
+    bmi_category = 'underweighted'
+elif body_mass_index < 22:
+    bmi_category = 'normal weighted'
+elif body_mass_index < 30:
+    bmi_category = 'overweighted'
+else:
+    bmi_category = 'obese'
+
+
+print(f'{name} with a weight of {weight}kg and a height of {height:.2f}m has a bmi of {body_mass_index:.2f} and is {bmi_category}')
