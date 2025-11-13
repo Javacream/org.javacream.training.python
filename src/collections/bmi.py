aa378c62b123da14f9ca254ead1dcb2fa5a89e87
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-13 11:56:40 +0100

    solution slide 43

---
+++ src/collections/bmi.py
@@ -0,0 +1,12 @@
+data = [
+    ['Hugo', 76.6, 185],
+    ['Andrea', 66.6, 166],
+    ['Hannah', 57, 176]
+]
+
+for person in data:
+    name = person[0]
+    weight = person[1]
+    height = person[2] / 100
+    bmi = weight/(height**2)
+    print(f'{name} with height={height} and weight={weight} has a bmi of {bmi:.2f}')
