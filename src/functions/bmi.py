a25eef96d59485245e1ab1cf49ce7a17002fb280
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-14 13:48:36 +0100

    introduce main

--- src/functions/bmi.py
+++ src/functions/bmi.py
@@ -3,5 +3,10 @@ def calculate_bmi(weight, height):
     bmi = weight / (height**2)
     return bmi
 
-calculated_bmi = calculate_bmi(76.5, 183)
-print(f'{calculated_bmi:.2f}')
+def main():
+    person_weight = 76.5
+    person_height = 183
+    calculated_bmi = calculate_bmi(person_weight, person_height) # implizit weight = person_weight und height = person_height
+    print(f'{calculated_bmi:.2f}')
+
+main()
