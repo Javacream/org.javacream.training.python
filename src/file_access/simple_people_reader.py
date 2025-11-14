d30f794b3d5808bb182b431f61d8e2fd3d7a9d80
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-14 11:38:28 +0100

    finish, template for file access

--- src/file_access/simple_people_reader.py
+++ src/file_access/simple_people_reader.py
@@ -1,6 +1,5 @@
 path = 'data/people.txt'
-people_file = open(path, encoding='utf-8')
-content = people_file.read()
+with open(path, encoding='utf-8') as people_file:
+    content = people_file.read()
 rows = content.split('\n')
-people_file.close()
 print(rows)
