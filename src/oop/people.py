69a8a13d2d83bf73f014bebed49ea8fa854a1cf7
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-20 14:12:09 +0100

    play with Person

---
+++ src/oop/people.py
@@ -0,0 +1,18 @@
+class Person:
+    def __init__(self, lastname, firstname, height=170, weight=67):
+        self.lastname = lastname
+        self.firstname = firstname
+        self.height = height
+        self.weight = weight
+
+def main():
+    person1 = Person('Sawitzki', 'Rainer', 183, 75.8)
+    person2 = Person('Muster', 'Hannah', 783, 55.8)
+    person3 = Person('Musterperson', 'Andrea')
+    person1_dict = {'lastname': 'A', 'firstname': 'B', 'height': 183, 'weight': 75.9}
+    person2_dict = {'last': 'A', 'first': 'B', 'h': 183, 'w': 75.9, 'eye_color': 'blue'}
+
+
+    print('done')
+
+main()
