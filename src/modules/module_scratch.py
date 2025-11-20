1416ee91fb4012113777e0d14c20036e6957f692
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-20 09:39:08 +0100

    introduce import as

--- src/modules/module_scratch.py
+++ src/modules/module_scratch.py
@@ -1,9 +1,15 @@
 import math
+import datetime as dtm
+# dtm = datetime
 def perimeter(radius):
     # pi_approx = 3.1415
     return 2* radius * math.pi
 
+def actual_date_and_time():
+    #dtm = datetime
+    return dtm.datetime.now()
+
 def main():
     print(perimeter(2))
-
+    print(actual_date_and_time())
 main()
