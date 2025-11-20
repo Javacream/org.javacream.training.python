da17439ece82dd0e803ffe7da6588451836bc743
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-20 14:28:11 +0100

    introduce methods

--- src/oop/people.py
+++ src/oop/people.py
@@ -4,6 +4,8 @@ class Person:
         self.firstname = firstname
         self.height = height
         self.weight = weight
+    def say_hello(self):
+        return f'Hello, my name is {self.firstname} {self.lastname}'
 
 def main():
     person1 = Person('Sawitzki', 'Rainer', 183, 75.8)
@@ -12,7 +14,8 @@ def main():
     person1_dict = {'lastname': 'A', 'firstname': 'B', 'height': 183, 'weight': 75.9}
     person2_dict = {'last': 'A', 'first': 'B', 'h': 183, 'w': 75.9, 'eye_color': 'blue'}
 
-
+    print(person1.say_hello())
+    print(person2.say_hello())
     print('done')
 
 main()
