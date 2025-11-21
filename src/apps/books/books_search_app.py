c01bb07c3dd30ccede40c81639342761796a66b6
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-21 09:35:16 +0100

    start todo find_all

--- src/apps/books/books_search_app.py
+++ src/apps/books/books_search_app.py
@@ -2,7 +2,7 @@ from books_service import BooksService
 
 def main():
     books_service = BooksService()
-    isbn = input('Enter the ISBN to search for: ')
+    isbn = input('Enter the ISBN to search for or a for all books: ')
     print(books_service.search_by(isbn))
 
 main()
--- src/apps/books/books_service.py
+++ src/apps/books/books_service.py
@@ -1,5 +1,9 @@
 import requests
 class BooksService:
+    def __init__(self):
+        self.url = 'http://javacream.eu:8081/api/books'
     def search_by(self, isbn):
-        url = f'http://javacream.eu:8081/api/books/{isbn}'
+        url = f'{self.url}/{isbn}'
         return requests.get(url).json()
+    def find_all(self):
+        pass
