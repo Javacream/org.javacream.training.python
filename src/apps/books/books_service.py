f0e6a60670dfe92e2c915a15f249daa0ebbcb5ae
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-21 12:07:57 +0100

    switch to mysql

--- src/apps/books/books_service.py
+++ src/apps/books/books_service.py
@@ -1,21 +1,29 @@
-import requests
+import mysql.connector 
 class Book:
-    def __init__(self, book_data_dict: dict):
-        self.isbn: str = book_data_dict['isbn']
-        self.title: str = book_data_dict['title']
-        self.price: float = book_data_dict['price']
-        self.available: bool = book_data_dict['available']
+    def __init__(self, book_data: tuple):
+        self.isbn: str = book_data[0]
+        self.title: str = book_data[1]
+        self.price: float = book_data[2]
+        self.available: bool = book_data[3]
     def __repr__(self):
         return f'Book(isbn={self.isbn}, title={self.title}, price={self.price}, available={self.available})'
 class BooksService:
     def __init__(self):
-        self.url = 'http://javacream.eu:8081/api/books'
+        self.connection = mysql.connector.connect(
+            host='javacream.eu', 
+            port=3406,
+            database='javacream',
+            user = 'user',
+            password='user')
+        self.cursor = self.connection.cursor()
     def search_by(self, isbn: str) -> Book:
-        url = f'{self.url}/{isbn}'
-        data = requests.get(url).json()
+        sql = f"SELECT * FROM BOOKS WHERE ISBN = '{isbn}'"
+        self.cursor.execute(sql)
+        data = self.cursor.fetchone()
         book = Book(data)
         return book
     def find_all(self) -> list[Book]:
-        url = self.url
-        datas = requests.get(url).json()
+        sql = f"SELECT * FROM BOOKS"
+        self.cursor.execute(sql)
+        datas = self.cursor.fetchall()
         return [Book(data) for data in datas]
