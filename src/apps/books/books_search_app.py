b79a6eccf3eda8fe9b4d3a3b6d70842b58009702
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-21 10:58:08 +0100

    add type hints

--- src/apps/books/books_search_app.py
+++ src/apps/books/books_search_app.py
@@ -4,9 +4,11 @@ def main():
     books_service = BooksService()
     user_input = input('Enter the ISBN to search for or a for all books: ')
     if user_input == 'a':
-        print(books_service.find_all())
+        books = books_service.find_all()
+        print(books)
     else:
         isbn = user_input
-        print(books_service.search_by(isbn))
+        book = books_service.search_by(isbn) 
+        print(book.title)
 
 main()
--- src/apps/books/books_service.py
+++ src/apps/books/books_service.py
@@ -1,21 +1,21 @@
 import requests
+class Book:
+    def __init__(self, book_data_dict):
+        self.isbn: str = book_data_dict['isbn']
+        self.title: str = book_data_dict['title']
+        self.price: float = book_data_dict['price']
+        self.available: bool = book_data_dict['available']
+    def __repr__(self):
+        return f'Book(isbn={self.isbn}, title={self.title}, price={self.price}, available={self.available})'
 class BooksService:
     def __init__(self):
         self.url = 'http://javacream.eu:8081/api/books'
-    def search_by(self, isbn):
+    def search_by(self, isbn: str) -> Book:
         url = f'{self.url}/{isbn}'
         data = requests.get(url).json()
         book = Book(data)
         return book
-    def find_all(self):
+    def find_all(self) -> list[Book]:
         url = self.url
         datas = requests.get(url).json()
         return [Book(data) for data in datas]
-class Book:
-    def __init__(self, book_data_dict):
-        self.isbn = book_data_dict['isbn']
-        self.title = book_data_dict['title']
-        self.price = book_data_dict['price']
-        self.available = book_data_dict['available']
-    def __repr__(self):
-        return f'Book(isbn={self.isbn}, title={self.title}, price={self.price}, available={self.available})'
