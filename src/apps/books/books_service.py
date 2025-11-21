c58eaa0f0f579ddbf48f91fd53534dafb39e2da6
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-11-21 10:10:11 +0100

    introduce class Book

--- src/apps/books/books_service.py
+++ src/apps/books/books_service.py
@@ -4,7 +4,23 @@ class BooksService:
         self.url = 'http://javacream.eu:8081/api/books'
     def search_by(self, isbn):
         url = f'{self.url}/{isbn}'
-        return requests.get(url).json()
+        data = requests.get(url).json()
+        book = Book(data)
+        return book
     def find_all(self):
         url = self.url
-        return requests.get(url).json()
+        datas = requests.get(url).json()
+        result = []
+        for data in datas:
+            book = Book(data)
+            result.append(book)
+        return result
+
+class Book:
+    def __init__(self, book_data_dict):
+        self.isbn = book_data_dict['isbn']
+        self.title = book_data_dict['title']
+        self.price = book_data_dict['price']
+        self.available = book_data_dict['available']
+    def __repr__(self):
+        return f'Book(isbn={self.isbn}, title={self.title}, price={self.price}, available={self.available})'
