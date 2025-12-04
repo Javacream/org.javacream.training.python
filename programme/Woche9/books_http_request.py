11f4f2bf019712373c26b09e0a8057c644f9d4b5
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-12-04 16:02:46 +0100

    Lösung für das Buchbeispiel

---
+++ programme/Woche9/books_http_request.py
@@ -0,0 +1,9 @@
+import requests
+def main():
+    book = {'isbn': 'ISBN1', 'title': 'java in action', 'price': 1.99, 'available': False}
+    response = requests.put('http://javacream.eu:8081/api/books/ISBN1', json=book)
+    if response.ok:
+        data = response.json()
+        print(data)
+    print(data)
+if __name__ == '__main__': main()
