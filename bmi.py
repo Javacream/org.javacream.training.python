5025f1d799a0bfb2b5a567aac5d35d754237d8cb
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-12 13:12:18 +0100

    play with types

--- bmi.py
+++ bmi.py
@@ -1,3 +1,4 @@
+print('starting...')
 weight = 76.6 
 height = 183
 height = height / 100
---
+++ type_scratch.py
@@ -0,0 +1,23 @@
+n1 = 4
+n2 = 1.5
+n3 = 4 + 1.5
+print(n3)
+
+s1 = 'Hello'
+s2 = '!'
+s3 = s1 + s2
+print(s3)
+
+result = s1 + str(n1)
+print(result)
+
+print(s1*n1)
+
+n4 = '42'
+print(type(n4))
+n4 = int(n4)
+print(type(n4))
+
+value = input('Please enter a number: ')
+print(type(value))
+
