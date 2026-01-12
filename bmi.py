ebc6a5f084acac5656e3c8f66d1a8bbc0826df15
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-12 13:21:27 +0100

    todo slide 21

--- bmi.py
+++ bmi.py
@@ -1,6 +1,8 @@
-print('starting...')
-weight = 76.6 
-height = 183
+name = input('Please enter your name: ')
+weight = input(f'{name}, please enter your weight in kg: ')
+height = input(f'{name}, please enter your height in cm: ')
+weight = float(weight) 
+height = int(height)
 height = height / 100
-bmi = weight / (height*height)
-print(bmi)
+bmi = weight / (height**2)
+print(f'{name} with weight {weight} and height {height} has a bmi of {bmi:.2f}')
