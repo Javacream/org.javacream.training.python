c5bd3d51e3d40b677531a1944987f91bddbfce33
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-12 15:05:11 +0100

    list and tuple

---
+++ list_scratch.py
@@ -0,0 +1,12 @@
+todos = ['Eat', 'Drink', 'Sleep', 'Eat']
+print(todos)
+
+print(f'length of list {todos}: {len(todos)}')
+
+for element in todos:
+    print(element)
+
+print(todos[1])
+
+todos.append('Shop')
+print(todos)
---
+++ tuple_scratch.py
@@ -0,0 +1,10 @@
+todos = ('Eat', 'Drink', 'Sleep', 'Eat')
+print(todos)
+
+print(f'length of tuple {todos}: {len(todos)}')
+
+for element in todos:
+    print(element)
+
+print(todos[1])
+
