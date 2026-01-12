be0432fc13fc2e17ec95563ff98a751caa4e6440
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-12 13:38:57 +0100

    try except

--- bmi.py
+++ bmi.py
@@ -1,8 +1,14 @@
 name = input('Please enter your name: ')
-weight = input(f'{name}, please enter your weight in kg: ')
-height = input(f'{name}, please enter your height in cm: ')
-weight = float(weight) 
-height = int(height)
-height = height / 100
-bmi = weight / (height**2)
-print(f'{name} with weight {weight} and height {height} has a bmi of {bmi:.2f}')
+try:
+    weight = input(f'{name}, please enter your weight in kg: ')
+    weight = float(weight) 
+    try:
+        height = input(f'{name}, please enter your height in cm: ')
+        height = int(height)
+        height = height / 100
+        bmi = weight / (height**2)
+        print(f'{name} with weight {weight} and height {height} has a bmi of {bmi:.2f}')
+    except:
+        print(f'{name} entered a wrong height: {height}')        
+except:
+    print(f'{name} entered a wrong weight: {weight}')
