6ff8bfe7ea732439958a6a6b1fb8f6844c2bcfdc
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-12 15:43:08 +0100

    set, string, conversion

---
+++ collections_types.py
@@ -0,0 +1,5 @@
+todos = ['Eat', 'Drink', 'Sleep', 'Eat']
+todos_tuple = tuple(todos)
+todos_set = set(todos)
+todos_list = list(todos_set)
+todos_string = str(todos_tuple)
---
+++ strings_scratch.py
@@ -0,0 +1,12 @@
+todos = 'Eat, Drink, Sleep, Eat'
+print(todos)
+
+print(f'length of string {todos}: {len(todos)}')
+
+for element in todos:
+    print(element)
+
+print(todos[1])
+
+todos.j
+
