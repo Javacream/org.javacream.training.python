3edff9347e5ae1da1e1a2a3097b8103f3ef54b33
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-12 11:13:35 +0100

    first programs

---
+++ bmi.py
@@ -0,0 +1,4 @@
+weight = 76.6 # Variablennamen Kleinbuchstaben, englisch, Zuweisung mit =, Gleitkommazahl mit . als Deziamltrenner
+height = 183 # Ganzzahl ohne Komma
+bmi = weight / (height*height) # Alle mathematischen Grundrechenarten + Klammeregeln werden untersützt
+print(bmi)
