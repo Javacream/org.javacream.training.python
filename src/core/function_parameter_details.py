5735dd89754fdb1bba9594dd575f89ed9f4aa604
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-19 11:13:13 +0100

    keyworded args

--- src/core/function_parameter_details.py
+++ src/core/function_parameter_details.py
@@ -1,1 +1,8 @@
-def fn ()
+def fn (p1, p2, *varargs, height, weight):
+    print('calling fn')
+
+def main():
+    height = 183
+    fn('A', 'B', height=height, weight=76.6)
+    fn('A', 'B', 1,2,3, height=183, weight=76.6)
+main()
