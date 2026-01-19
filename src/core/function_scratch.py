7830e7dbc0b34d3af673d838e4a0cf4536bc057d
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-19 09:51:53 +0100

    scopes in detail

--- src/core/function_scratch.py
+++ src/core/function_scratch.py
@@ -3,7 +3,9 @@ def add(number1, number2):
     return result
 
 def main():
-    add_result = add(22, 20)
+    n1 = 22
+    n2 = 20
+    add_result = add(n1, n2) # implizit im Aufruf: number1 = n1, number2 = n2
     print(add_result)
 
 main()
