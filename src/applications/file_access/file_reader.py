bd1e08ffbe9b136953303e8da7a10e4feee2237b
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-19 11:37:22 +0100

    split \n

--- src/applications/file_access/file_reader.py
+++ src/applications/file_access/file_reader.py
@@ -2,8 +2,9 @@ def read_demo():
     path = 'src/applications/file_access/simple.txt'
     with open(path, 'rt', encoding='utf-8') as file:
         content = file.read()
+    rows = content.split('\n')    
     print(content)
-
+    print('Hello')
 def write_demo():
     path = 'src/applications/file_access/result.txt'
     with open(path, 'wt', encoding='utf-8') as file:
