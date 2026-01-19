9fbf1aba0cfbf166e781540472780ef9864ddae2
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-19 09:46:43 +0100

    introduce main and scopes

--- src/core/function_scratch.py
+++ src/core/function_scratch.py
@@ -2,5 +2,8 @@ def add(number1, number2):
     result = number1 + number2
     return result
 
-add_result = add(22, 20)
-print(add_result)
+def main():
+    add_result = add(22, 20)
+    print(add_result)
+
+main()
