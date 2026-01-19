288ce4316015be61310b395644f51f213cc1ab5c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-19 11:22:24 +0100

    first file access

--- src/applications/file_access/file_reader.py
+++ src/applications/file_access/file_reader.py
@@ -1,7 +1,15 @@
-def main():
+def read_demo():
     path = 'src/applications/file_access/simple.txt'
     file = open(path, 'rt', encoding='utf-8')
     content = file.read()
     print(content)
 
+def write_demo():
+    path = 'src/applications/file_access/result.txt'
+    file = open(path, 'wt', encoding='utf-8')
+    file.write('dqfwghsdicsdabvf ä ül öö')
+
+def main():
+    read_demo()
+    write_demo()
 main()
