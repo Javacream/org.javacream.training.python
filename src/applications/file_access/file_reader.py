a4a496ec3f9aea2962a0569f842ec8ec330eb96b
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-19 11:29:36 +0100

    introduce with

--- src/applications/file_access/file_reader.py
+++ src/applications/file_access/file_reader.py
@@ -1,15 +1,14 @@
 def read_demo():
     path = 'src/applications/file_access/simple.txt'
-    file = open(path, 'rt', encoding='utf-8')
-    content = file.read()
-    file.close()
+    with open(path, 'rt', encoding='utf-8') as file:
+        content = file.read()
     print(content)
 
 def write_demo():
     path = 'src/applications/file_access/result.txt'
-    file = open(path, 'wt', encoding='utf-8')
-    file.write('dqfwghsdicsdabvf ä ül öö')
-    file.close()
+    with open(path, 'wt', encoding='utf-8') as file:
+        file.write('dqfwghsdicsdabvf ä ül öö')
+
 def main():
     read_demo()
     write_demo()
