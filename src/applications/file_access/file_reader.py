29de2d6c9cbc54056c80dc604e1ed12d15852638
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-19 11:27:21 +0100

    file close

--- src/applications/file_access/file_reader.py
+++ src/applications/file_access/file_reader.py
@@ -2,13 +2,14 @@ def read_demo():
     path = 'src/applications/file_access/simple.txt'
     file = open(path, 'rt', encoding='utf-8')
     content = file.read()
+    file.close()
     print(content)
 
 def write_demo():
     path = 'src/applications/file_access/result.txt'
     file = open(path, 'wt', encoding='utf-8')
     file.write('dqfwghsdicsdabvf ä ül öö')
-
+    file.close()
 def main():
     read_demo()
     write_demo()
