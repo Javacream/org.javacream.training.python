fc17885a72201ebbc4b9dfce0c25230da64f32a0
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-20 14:16:48 +0100

    start bmi tests

---
+++ src/applications/bmi/bmi_tests.py
@@ -0,0 +1,9 @@
+from bmi_services import *
+import unittest
+class BmiTests(unittest.TestCase):
+    def test_demo(self):
+        self.assertFalse(False)
+
+    # TODO: Weitere Testmethoden für alle Methoden des bmi_service
+if __name__ == '__main__':
+    unittest.main()
