962845bec286d2a600c96b006c87894c70151ad7
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-20 09:37:41 +0100

    change import to from ...

--- src/applications/bmi/bmi_app.py
+++ src/applications/bmi/bmi_app.py
@@ -1,13 +1,13 @@
-import bmi_services
+from bmi_services import read_people_data, extract, calculate_bmi, calculate_result_text, calculate_bmi_category, write
 def main():
     input_path = 'src/applications/bmi/people.csv'
     result_path = 'src/applications/bmi/bmi.txt'
-    people_data = bmi_services.read_people_data(input_path)
+    people_data = read_people_data(input_path)
     for raw_person_data in people_data:
-        name, height, weight = bmi_services.extract(raw_person_data)
-        bmi = bmi_services.calculate_bmi(height, weight)
-        bmi_category = bmi_services.calculate_bmi_category(bmi)
-        result = bmi_services.calculate_result_text(name, height, weight, bmi, bmi_category)
-        bmi_services.write(result_path, result)
+        name, height, weight = extract(raw_person_data)
+        bmi = calculate_bmi(height, weight)
+        bmi_category = calculate_bmi_category(bmi)
+        result = calculate_result_text(name, height, weight, bmi, bmi_category)
+        write(result_path, result)
 
 main()
